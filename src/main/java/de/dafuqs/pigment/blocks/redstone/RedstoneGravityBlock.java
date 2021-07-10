e4aba09ea86e04778b6f2e22c1131ca3bc22b105
DaFuqs <dominik.m.schmidt@googlemail.com>
2021-07-10 13:37:12 +0200

    finished redstone gravity block

--- src/main/java/de/dafuqs/pigment/blocks/redstone/RedstoneGravityBlock.java
+++ src/main/java/de/dafuqs/pigment/blocks/redstone/RedstoneGravityBlock.java
@@ -1,8 +1,13 @@
 package de.dafuqs.pigment.blocks.redstone;
 
+import de.dafuqs.pigment.blocks.altar.AltarBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.WorldAccess;
@@ -11,30 +16,34 @@ import java.util.Random;
 
 public class RedstoneGravityBlock extends FallingBlock {
 
+    public static final BooleanProperty UNSTABLE = BooleanProperty.of("unstable");
+
     public RedstoneGravityBlock(Settings settings) {
         super(settings);
+        setDefaultState(getStateManager().getDefaultState().with(UNSTABLE, false));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+        stateManager.add(UNSTABLE);
     }
 
     /**
-     * Only trigger fall if redstone applied
+     * Only trigger fall if redstone applied or unstable
+     * if restone: set neighboring block to unstable
      */
-    // TODO: pass on fall trigger to neighboring blocks
     @Override
     public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        if(world.getReceivedRedstonePower(pos) > 0) {
-            super.scheduledTick(state, world, pos, random);
-        }
-    }
-
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-        if (!world.isClient()) {
+        if(state.get(UNSTABLE) || world.getReceivedRedstonePower(pos) > 0) {
             for(Direction dir : Direction.values()) {
-                if (world.getBlockState(pos.offset(dir)).isOf(this)) {
-                    world.getBlockTickScheduler().schedule(pos.offset(dir), this, 1);
+                BlockPos offsetPos = pos.offset(dir);
+                if (world.getBlockState(offsetPos).isOf(this)) {
+                    world.setBlockState(offsetPos, world.getBlockState(offsetPos).with(UNSTABLE, true));
+                    world.getBlockTickScheduler().schedule(offsetPos, this, 1);
                 }
             }
+            super.scheduledTick(state, world, pos, random);
         }
-        return state;
     }
 
 }

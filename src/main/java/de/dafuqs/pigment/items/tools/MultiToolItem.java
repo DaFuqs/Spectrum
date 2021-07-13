6c9d4935fe87a958f1fed99f9f12c84f8e454da8
DaFuqs <dominik.m.schmidt@googlemail.com>
2021-07-13 19:28:57 +0200

    Multitool is not multifunctional

--- src/main/java/de/dafuqs/pigment/items/tools/MultiToolItem.java
+++ src/main/java/de/dafuqs/pigment/items/tools/MultiToolItem.java
@@ -1,9 +1,15 @@
 package de.dafuqs.pigment.items.tools;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.PickaxeItem;
-import net.minecraft.item.ToolMaterial;
+import net.minecraft.block.CampfireBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.*;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 
 public class MultiToolItem extends PickaxeItem {
 
@@ -20,5 +26,22 @@ public class MultiToolItem extends PickaxeItem {
     public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
         return miningSpeed;
     }
+
+    /**
+     * Invoke shovel, axe and hoe right click actions (in this order)
+     * Like stripping logs, tilling grass paths etc.
+     * To get tilled earth it has to converted to path and then tilled again
+     */
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        ActionResult actionResult = Items.IRON_SHOVEL.useOnBlock(context);
+        if (actionResult == ActionResult.PASS) {
+            actionResult = Items.IRON_AXE.useOnBlock(context);
+            if(actionResult == ActionResult.PASS) {
+                actionResult = Items.IRON_HOE.useOnBlock(context);
+            }
+        }
+        return actionResult;
+    }
+
 
 }

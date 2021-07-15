8504b78f45cd65bb8321e739d9924b74b208585a
DaFuqs <dominik.m.schmidt@googlemail.com>
2021-07-15 21:31:53 +0200

    added missing crafting resipes and loot tables

---
+++ raw/colored_lamp_recipes.ps1
@@ -0,0 +1,35 @@
+﻿$c = @("black", "blue", "brown", "cyan", "gray", "green", "light_blue", "light_gray", "lime", "magenta", "orange", "pink", "purple", "red", "white", "yellow")
+
+$c | Foreach-Object {
+New-Item -Name "$_`_lamp.json" -ItemType File -Value @"
+{
+  "type": "pigment_altar",
+  "time": 40,
+  "tier": 1,
+  "magenta": 0,
+  "yellow": 0,
+  "cyan": 0,
+  "black": 0,
+  "white": 0,
+  "experience": 0.25,
+  "pattern": [
+    " P ",
+    "PGP",
+    " P "
+  ],
+  "key": {
+  	"P": {
+      "item": "pigment:$_`_pigment"
+    },
+	"G": {
+      "item": "minecraft:glowstone"
+    }
+  },
+  "result" : {
+    "item": "pigment:$_`_lamp",
+    "count": 1
+  }
+}
+"@
+
+}
---
+++ raw/gem_ore_loot_tables.ps1
@@ -0,0 +1,75 @@
+﻿$gems = @("amethyst", "citrine", "topaz", "onyx", "moonstone")
+
+$gems | ForEach-Object {
+
+    if($_ -eq "amethyst") {
+        $ident = "minecraft"
+    } else {
+        $ident = "pigment"
+    }
+
+    foreach($ore in @("", "deepslate_")) {
+
+    New-Item -Name "$ore$_`_ore.json" -ItemType File -Value @"
+{
+  "type": "minecraft:block",
+  "pools": [
+    {
+      "rolls": 1.0,
+      "bonus_rolls": 0.0,
+      "entries": [
+        {
+          "type": "minecraft:alternatives",
+          "children": [
+            {
+              "type": "minecraft:item",
+		      "name": "pigment:$ore$_`_ore",
+              "conditions": [
+                {
+                  "condition": "minecraft:match_tool",
+                  "predicate": {
+                    "enchantments": [
+                      {
+                        "enchantment": "minecraft:silk_touch",
+                        "levels": {
+                          "min": 1
+                        }
+                      }
+                    ]
+                  }
+                }
+              ]
+            },
+            {
+              "type": "minecraft:item",
+			  "name": "$ident`:$_`_shard",
+              "functions": [
+                {
+                  "function": "minecraft:set_count",
+                  "count": {
+                    "type": "minecraft:uniform",
+                    "min": 1.0,
+                    "max": 2.0
+                  },
+                  "add": false
+                },
+                {
+                  "function": "minecraft:apply_bonus",
+                  "enchantment": "minecraft:fortune",
+                  "formula": "minecraft:ore_drops"
+                },
+                {
+                  "function": "minecraft:explosion_decay"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+"@
+    }
+
+}

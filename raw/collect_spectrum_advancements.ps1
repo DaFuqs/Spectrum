685ed88aec8161172d92e7568bbe1e50474ca8b5
DaFuqs <dominik.m.schmidt@googlemail.com>
2021-08-02 16:47:00 +0200

    fixed command output

---
+++ raw/collect_spectrum_advancements.ps1
@@ -0,0 +1,15 @@
+﻿$c = @("black", "blue", "brown", "cyan", "gray", "green", "light_blue", "light_gray", "lime", "magenta", "orange", "pink", "purple", "red", "white", "yellow")
+
+$c | Foreach-Object {
+New-Item -Name "collect_$_`_spectrum.json" -ItemType File -Value @"
+{
+  "criteria": {
+    "has_black_spectrum": {
+      "trigger": "minecraft:inventory_changed",
+      "conditions": { "items": [{ "items": [ "spectrum:$_`_spectrum" ]}] }
+    }
+  }
+}
+"@
+
+}

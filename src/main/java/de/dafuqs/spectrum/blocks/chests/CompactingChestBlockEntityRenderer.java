55815a3bc849e39baef3ee77ac6c29ad404e2744
DaFuqs <dominik.m.schmidt@googlemail.com>
2021-09-09 15:18:10 +0200

    added custom renderer for compacting chest

---
+++ src/main/java/de/dafuqs/spectrum/blocks/chests/CompactingChestBlockEntityRenderer.java
@@ -0,0 +1,87 @@
+package de.dafuqs.spectrum.blocks.chests;
+
+import de.dafuqs.spectrum.SpectrumCommon;
+import de.dafuqs.spectrum.registries.SpectrumBlocks;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ChestBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.client.model.*;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3f;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
+
+@Environment(EnvType.CLIENT)
+public class CompactingChestBlockEntityRenderer<CompactingChestBlockEntity extends SpectrumChestBlockEntity> implements BlockEntityRenderer<CompactingChestBlockEntity> {
+
+    private final SpriteIdentifier spriteIdentifier;
+    private final ModelPart root;
+    private final ModelPart lid;
+    private final ModelPart column;
+
+    public CompactingChestBlockEntityRenderer(BlockEntityRendererFactory.Context ctx) {
+        spriteIdentifier = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier(SpectrumCommon.MOD_ID, "entity/compacting_chest"));
+
+        TexturedModelData texturedModelData = getTexturedModelData();
+        root = texturedModelData.createModel();
+        //root.setPivot(8.0F, 8.0F, 8.0F);
+        lid = root.getChild("lid");
+        column = root.getChild("column");
+    }
+
+
+    @Override
+    public void render(CompactingChestBlockEntity entity, float tickDelta, MatrixStack matrixStack, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        World world = entity.getWorld();
+        boolean bl = world != null;
+        BlockState blockState = bl ? entity.getCachedState() : SpectrumBlocks.COMPACTING_CHEST.getDefaultState().with(ChestBlock.FACING, Direction.SOUTH);
+
+        Block block = blockState.getBlock();
+        if (block instanceof CompactingChestBlock) {
+            matrixStack.push();
+            float f = (blockState.get(ChestBlock.FACING)).asRotation();
+            matrixStack.translate(0.5D, 0.5D, 0.5D);
+            matrixStack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(-f));
+            matrixStack.translate(-0.5D, -0.5D, -0.5D);
+
+            float openFactor = entity.getAnimationProgress(tickDelta);
+            openFactor = 1.0F - openFactor;
+            openFactor = 1.0F - openFactor * openFactor * openFactor;
+
+            lid.pivotY = 11 + openFactor * 6;
+            column.pivotY = 11 + openFactor * 6;
+
+            SpriteIdentifier spriteIdentifier = ((SpectrumChestBlock) block).getTexture();
+            VertexConsumer vertexConsumer = spriteIdentifier.getVertexConsumer(vertexConsumers, RenderLayer::getEntityCutout);
+
+            root.render(matrixStack, vertexConsumer, light, overlay);
+
+            matrixStack.pop();
+        }
+    }
+
+    public static @NotNull TexturedModelData getTexturedModelData(){
+        ModelData modelData = new ModelData();
+        ModelPartData modelPartData = modelData.getRoot();
+
+        modelPartData.addChild("bottom", ModelPartBuilder.create().uv(0, 17).cuboid(1.0F, 0.0F, 1.0F, 14.0F, 11.0F, 14.0F), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        modelPartData.addChild("lid", ModelPartBuilder.create().uv(0, 0).cuboid(1.0F, 0.0F, 1.0F, 14.0F, 3.0F, 14.0F), ModelTransform.pivot(0.0F, 13.0F, 0.0F));
+        modelPartData.addChild("column", ModelPartBuilder.create().uv(18, 32).cuboid(7F, -7.0F, 7.0F, 2.0F, 7.0F, 2.0F), ModelTransform.pivot(0.0F, 13.0F, 0.0F));
+
+        return TexturedModelData.of(modelData, 64, 64);
+    }
+
+}

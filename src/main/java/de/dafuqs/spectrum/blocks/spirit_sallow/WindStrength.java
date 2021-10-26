46ca80b7c2ac4b71157fce78eaa78fde67c73ba6
DaFuqs <dominik.m.schmidt@googlemail.com>
2021-10-26 21:40:50 +0200

    wind!

--- src/main/java/de/dafuqs/spectrum/blocks/spirit_sallow/WindStrength.java
+++ src/main/java/de/dafuqs/spectrum/blocks/spirit_sallow/WindStrength.java
@@ -1,26 +1,35 @@
 package de.dafuqs.spectrum.blocks.spirit_sallow;
 
-import net.minecraft.util.math.noise.PerlinNoiseSampler;
-import net.minecraft.world.World;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.noise.SimplexNoiseSampler;
 import net.minecraft.world.gen.ChunkRandom;
 
 public class WindStrength {
 	
-	private static PerlinNoiseSampler sampler;
+	private static SimplexNoiseSampler samplerX;
+	private static SimplexNoiseSampler samplerY;
+	private static SimplexNoiseSampler samplerZ;
 	
 	private static long cachedTick;
-	private static World cachedWorld;
-	private static double cachedValue;
 	
-	public static double getStrength(World world, long tick) {
-		if(sampler == null) {
-			sampler = new PerlinNoiseSampler(new ChunkRandom());
+	public Vec3d cachedValue;
+	
+	public Vec3d getWindStrength(Vec3d pos, long tick) {
+		if(samplerX == null) {
+			samplerX = new SimplexNoiseSampler(new ChunkRandom());
+			samplerY = new SimplexNoiseSampler(new ChunkRandom());
+			samplerZ = new SimplexNoiseSampler(new ChunkRandom());
 		}
 		
-		if(tick != cachedTick || world != cachedWorld) {
-			cachedValue = sampler.sample(0, 0, 0);
+		if(tick != cachedTick) {
+			cachedValue = new Vec3d(
+					samplerX.sample(0, 0, (tick + MinecraftClient.getInstance().getTickDelta()) / 512D),
+					samplerY.sample(0, 0, (tick + MinecraftClient.getInstance().getTickDelta()) / 512D),
+					samplerZ.sample(0, 0, (tick + MinecraftClient.getInstance().getTickDelta()) / 512D)
+			);
+			
 			cachedTick = tick;
-			cachedWorld = world;
 		}
 		
 		return cachedValue;
---
+++ src/main/java/de/dafuqs/spectrum/particle/client/WindParticle.java
@@ -0,0 +1,70 @@
+package de.dafuqs.spectrum.particle.client;
+
+import de.dafuqs.spectrum.blocks.spirit_sallow.WindStrength;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.block.Block;
+import net.minecraft.client.particle.*;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.Random;
+
+@Environment(EnvType.CLIENT)
+public class WindParticle extends SpriteBillboardParticle {
+	
+	private static final WindStrength wind = new WindStrength();
+	
+	private final Vec3d spawnPos;
+	
+	@Override
+	public void tick() {
+		super.tick();
+		
+		Vec3d windStrength = wind.getWindStrength(spawnPos, world.getTime());
+		this.velocityX += windStrength.getX() * 0.004;
+		this.velocityY += windStrength.getY() * 0.001;
+		this.velocityZ += windStrength.getZ() * 0.004;
+	}
+	
+	protected WindParticle(ClientWorld clientWorld, double x, double y, double z, double velocityX, double velocityY, double velocityZ) {
+		super(clientWorld, x, y - 0.125D, z, velocityX, velocityY, velocityZ);
+		
+		this.spawnPos = new Vec3d(x, y, z);
+		this.collidesWithWorld = true;
+
+		this.setBoundingBoxSpacing(0.01F, 0.01F);
+		this.scale *= this.random.nextFloat() * 0.4F + 0.7F;
+		this.maxAge = 120;
+	}
+
+	@Override
+	public ParticleTextureSheet getType() {
+		return ParticleTextureSheet.PARTICLE_SHEET_OPAQUE;
+	}
+
+	public static class Factory implements ParticleFactory<DefaultParticleType> {
+		private final SpriteProvider spriteProvider;
+
+		public Factory(SpriteProvider spriteProvider) {
+			this.spriteProvider = spriteProvider;
+		}
+
+		@Override
+		public Particle createParticle(DefaultParticleType parameters, ClientWorld clientWorld, double x, double y, double z, double velocityX, double velocityY, double velocityZ) {
+			Random random = clientWorld.getRandom();
+
+			WindParticle particle = new WindParticle(clientWorld, x, y, z, velocityX * random.nextDouble(), (random.nextDouble() - 0.5) * 0.05, velocityZ * random.nextDouble());
+			particle.setSprite(this.spriteProvider);
+			return particle;
+		}
+	}
+
+	@Override
+	protected int getBrightness(float tint) {
+		return 15728880;
+	}
+
+}

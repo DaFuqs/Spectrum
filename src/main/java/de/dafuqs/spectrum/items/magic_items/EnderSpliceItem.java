782b0bf9ad9717d7a838c41060eb0cdadcf0e0d2
DaFuqs <dominik.m.schmidt@googlemail.com>
2021-11-10 21:59:03 +0100

    Allow Ender Splice to  bind to players

--- src/main/java/de/dafuqs/spectrum/items/magic_items/EnderSpliceItem.java
+++ src/main/java/de/dafuqs/spectrum/items/magic_items/EnderSpliceItem.java
@@ -2,11 +2,13 @@ package de.dafuqs.spectrum.items.magic_items;
 
 import de.dafuqs.spectrum.SpectrumClient;
 import de.dafuqs.spectrum.Support;
+import de.dafuqs.spectrum.interfaces.PlayerOwned;
 import de.dafuqs.spectrum.sound.EnderSpliceChargingSoundInstance;
 import de.dafuqs.spectrum.sound.SpectrumSoundEvents;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -21,82 +23,109 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.UseAction;
+import net.minecraft.util.*;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
+import org.apache.logging.log4j.core.jmx.Server;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public class EnderSpliceItem extends Item {
-
+	
 	public EnderSpliceItem(Settings settings) {
 		super(settings);
 	}
 
 	@Override
 	public ItemStack finishUsing(ItemStack itemStack, World world, LivingEntity user) {
-		PlayerEntity playerEntity = user instanceof PlayerEntity ? (PlayerEntity)user : null;
-		if (playerEntity instanceof ServerPlayerEntity) {
-			Criteria.CONSUME_ITEM.trigger((ServerPlayerEntity)playerEntity, itemStack);
-		}
-
-		NbtCompound nbtCompound = itemStack.getTag();
-		if (playerEntity != null && nbtCompound != null && nbtCompound.contains("PosX") && nbtCompound.contains("PosY") && nbtCompound.contains("PosZ") && nbtCompound.contains("Dimension")) {
-			String dimensionKeyString = nbtCompound.getString("Dimension");
-			if (user.getEntityWorld().getRegistryKey().getValue().toString().equals(dimensionKeyString)) {
-
-				double x = nbtCompound.getDouble("PosX");
-				double y = nbtCompound.getDouble("PosY");
-				double z = nbtCompound.getDouble("PosZ");
-				if(!world.isClient) {
-					Vec3d pos = playerEntity.getPos();
-
-					world.playSound(playerEntity, pos.getX(), pos.getY(), pos.getZ(), SpectrumSoundEvents.PLAYER_TELEPORTS, SoundCategory.PLAYERS, 1.0F, 1.0F);
-					user.requestTeleport(x, y + 0.25, z); // +0.25 makes it look way more lively
-					world.playSound(playerEntity, x, y, z, SpectrumSoundEvents.PLAYER_TELEPORTS, SoundCategory.PLAYERS, 1.0F, 1.0F);
-
-					// make sure the sound plays even when the player currently teleports
-					if(playerEntity instanceof ServerPlayerEntity) {
-						((ServerPlayerEntity) playerEntity).networkHandler.sendPacket(new PlaySoundIdS2CPacket(SpectrumSoundEvents.PLAYER_TELEPORTS.getId(), SoundCategory.PLAYERS, playerEntity.getPos(), 1.0F, 1.0F));
-						((ServerPlayerEntity) playerEntity).networkHandler.sendPacket(new PlaySoundIdS2CPacket(SoundEvents.BLOCK_GLASS_BREAK.getId(), SoundCategory.PLAYERS, playerEntity.getPos(), 1.0F, 1.0F));
-					}
+		if(world.isClient) {
+			if(getTeleportTargetPos(itemStack).isEmpty() && getTeleportTargetPlayerUUID(itemStack).isEmpty()) {
+				// If aiming at an entity: trigger entity interaction
+				HitResult hitResult = MinecraftClient.getInstance().crosshairTarget;
+				if(hitResult.getType() == HitResult.Type.ENTITY) {
+					EntityHitResult entityHitResult = (EntityHitResult) hitResult;
+					MinecraftClient.getInstance().interactionManager.interactEntity(MinecraftClient.getInstance().player, entityHitResult.getEntity(), user.getActiveHand());
 				}
 			}
-
+		} else {
+			PlayerEntity playerEntity = user instanceof PlayerEntity ? (PlayerEntity) user : null;
+			if (playerEntity instanceof ServerPlayerEntity) {
+				Criteria.CONSUME_ITEM.trigger((ServerPlayerEntity) playerEntity, itemStack);
+			}
+			
+			// If Dimension & Pos stored => Teleport to that position
+			Optional<Pair<String, Vec3d>> teleportTargetPos = getTeleportTargetPos(itemStack);
+			if (teleportTargetPos.isPresent()) {
+				RegistryKey<World> targetWorldKey = RegistryKey.of(Registry.WORLD_KEY, new Identifier(teleportTargetPos.get().getLeft()));
+				World targetWorld = world.getServer().getWorld(targetWorldKey);
+				teleportPlayerToPos(world, user, playerEntity, targetWorld, teleportTargetPos.get().getRight());
+			} else {
+				// If UUID stored => Teleport to player, if online
+				Optional<UUID> teleportTargetPlayerUUID = getTeleportTargetPlayerUUID(itemStack);
+				if (teleportTargetPlayerUUID.isPresent()) {
+					teleportPlayerToPlayerWithUUID(world, user, playerEntity, teleportTargetPlayerUUID.get());
+				} else {
+					// Nothing stored => Store current position
+					setTeleportTargetPos(itemStack, playerEntity.getEntityWorld(), playerEntity.getPos());
+					((ServerPlayerEntity) playerEntity).networkHandler.sendPacket(new PlaySoundIdS2CPacket(SpectrumSoundEvents.ENDER_SPLICE_BOUND.getId(), SoundCategory.PLAYERS, playerEntity.getPos(), 1.0F, 1.0F));
+				}
+			}
+			
 			playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
 			if (!playerEntity.getAbilities().creativeMode) {
 				itemStack.decrement(1);
 			}
-
-			return itemStack;
-		} else {
-			if(!world.isClient) {
-				Vec3d pos = user.getPos();
-
-				if (nbtCompound == null) {
-					nbtCompound = new NbtCompound();
-				}
-
-				nbtCompound.putDouble("PosX", pos.getX());
-				nbtCompound.putDouble("PosY", pos.getY());
-				nbtCompound.putDouble("PosZ", pos.getZ());
-				nbtCompound.putString("Dimension", user.getEntityWorld().getRegistryKey().getValue().toString());
-				itemStack.setTag(nbtCompound);
-
-				((ServerPlayerEntity) playerEntity).networkHandler.sendPacket(new PlaySoundIdS2CPacket(SpectrumSoundEvents.ENDER_SPLICE_BOUND.getId(), SoundCategory.PLAYERS, playerEntity.getPos(), 1.0F, 1.0F));
+		}
+		
+		return itemStack;
+	}
+	
+	private void teleportPlayerToPlayerWithUUID(World world, LivingEntity user, PlayerEntity playerEntity, UUID targetPlayerUUID) {
+		PlayerEntity targetPlayer = PlayerOwned.getPlayerEntityIfOnline(world, targetPlayerUUID);
+		if(targetPlayer != null) {
+			teleportPlayerToPos(targetPlayer.getEntityWorld(), user, playerEntity, targetPlayer.getEntityWorld(), targetPlayer.getPos());
+		}
+	}
+	
+	private void teleportPlayerToPos(World world, LivingEntity user, PlayerEntity playerEntity, World targetWorld, Vec3d targetPos) {
+		if (user.getEntityWorld().getRegistryKey().getValue().toString().equals(targetWorld.getRegistryKey().getValue().toString())) {
+			Vec3d currentPos = playerEntity.getPos();
+			world.playSound(playerEntity, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SpectrumSoundEvents.PLAYER_TELEPORTS, SoundCategory.PLAYERS, 1.0F, 1.0F);
+			
+			user.requestTeleport(targetPos.getX(), targetPos.y + 0.25, targetPos.z); // +0.25 makes it look way more lively
+			world.playSound(playerEntity, targetPos.getX(), targetPos.y, targetPos.z, SpectrumSoundEvents.PLAYER_TELEPORTS, SoundCategory.PLAYERS, 1.0F, 1.0F);
+			
+			// make sure the sound plays even when the player currently teleports
+			if(playerEntity instanceof ServerPlayerEntity) {
+				((ServerPlayerEntity) playerEntity).networkHandler.sendPacket(new PlaySoundIdS2CPacket(SpectrumSoundEvents.PLAYER_TELEPORTS.getId(), SoundCategory.PLAYERS, playerEntity.getPos(), 1.0F, 1.0F));
+				((ServerPlayerEntity) playerEntity).networkHandler.sendPacket(new PlaySoundIdS2CPacket(SoundEvents.BLOCK_GLASS_BREAK.getId(), SoundCategory.PLAYERS, playerEntity.getPos(), 1.0F, 1.0F));
 			}
 		}
-
-		return itemStack;
 	}
 	
 	@Override
 	public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
-		return super.useOnEntity(stack, user, entity, hand);
+		if(user.getItemUseTime() >= getMaxUseTime(stack)) { // only true when triggered via onFinishUsing()
+			if (entity instanceof PlayerEntity && getTeleportTargetPlayerUUID(stack).isEmpty()) {
+				if (entity instanceof ServerPlayerEntity serverPlayerEntity) {
+					setTeleportTargetPlayer(stack, serverPlayerEntity);
+					((ServerPlayerEntity) user).networkHandler.sendPacket(new PlaySoundIdS2CPacket(SpectrumSoundEvents.ENDER_SPLICE_BOUND.getId(), SoundCategory.PLAYERS, user.getPos(), 1.0F, 1.0F));
+					serverPlayerEntity.networkHandler.sendPacket(new PlaySoundIdS2CPacket(SpectrumSoundEvents.ENDER_SPLICE_BOUND.getId(), SoundCategory.PLAYERS, user.getPos(), 1.0F, 1.0F));
+				}
+				user.stopUsingItem();
+			}
+		}
+		
+		return ActionResult.SUCCESS;
 	}
 	
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
@@ -120,20 +149,89 @@ public class EnderSpliceItem extends Item {
 	}
 
 	@Environment(EnvType.CLIENT)
-	public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-		NbtCompound nbtCompound = stack.getTag();
-		if(nbtCompound != null && nbtCompound.contains("PosX") && nbtCompound.contains("PosY") && nbtCompound.contains("PosZ") && nbtCompound.contains("Dimension")) {
-			String dimensionKeyString = nbtCompound.getString("Dimension");
-			String dimensionDisplayString = Support.getReadableDimensionString(dimensionKeyString);
-			int x = (int) nbtCompound.getDouble("PosX");
-			int y = (int) nbtCompound.getDouble("PosY");
-			int z = (int) nbtCompound.getDouble("PosZ");
-
-			tooltip.add(new TranslatableText("item.spectrum.ender_splice.tooltip.bound", x, y, z, dimensionDisplayString));
+	public void appendTooltip(ItemStack itemStack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+		// If Dimension & Pos stored => Teleport to that position
+		Optional<Pair<String, Vec3d>> teleportTargetPos = getTeleportTargetPos(itemStack);
+		if (teleportTargetPos.isPresent()) {
+			String dimensionDisplayString = Support.getReadableDimensionString(teleportTargetPos.get().getLeft());
+			Vec3d pos = teleportTargetPos.get().getRight();
+			tooltip.add(new TranslatableText("item.spectrum.ender_splice.tooltip.bound_pos", (int) pos.x, (int) pos.y, (int) pos.z, dimensionDisplayString));
+			return;
 		} else {
-			tooltip.add(new TranslatableText("item.spectrum.ender_splice.tooltip.unbound"));
+			// If UUID stored => Teleport to player, if online
+			Optional<UUID> teleportTargetPlayerUUID = getTeleportTargetPlayerUUID(itemStack);
+			if(teleportTargetPlayerUUID.isPresent()) {
+				Optional<String> teleportTargetPlayerName = getTeleportTargetPlayerName(itemStack);
+				if(teleportTargetPlayerName.isPresent()) {
+					tooltip.add(new TranslatableText("item.spectrum.ender_splice.tooltip.bound_player", teleportTargetPlayerName.get()));
+				} else {
+					tooltip.add(new TranslatableText("item.spectrum.ender_splice.tooltip.bound_player", "???"));
+				}
+				return;
+			}
 		}
+		
+		tooltip.add(new TranslatableText("item.spectrum.ender_splice.tooltip.unbound"));
+	}
+	
+	public void setTeleportTargetPos(@NotNull ItemStack itemStack, World world, Vec3d pos) {
+		NbtCompound nbtCompound = itemStack.getOrCreateTag();
+		
+		// Remove player tags, if present
+		if(nbtCompound.contains("TargetPlayerName")) { nbtCompound.remove("TargetPlayerName"); }
+		if(nbtCompound.contains("TargetPlayerUUID")) { nbtCompound.remove("TargetPlayerUUID"); }
+		
+		// Add pos
+		nbtCompound.putDouble("PosX", pos.getX());
+		nbtCompound.putDouble("PosY", pos.getY());
+		nbtCompound.putDouble("PosZ", pos.getZ());
+		nbtCompound.putString("Dimension", world.getRegistryKey().getValue().toString());
+		itemStack.setTag(nbtCompound);
+	}
+	
+	public void setTeleportTargetPlayer(@NotNull ItemStack itemStack, ServerPlayerEntity player) {
+		NbtCompound nbtCompound = itemStack.getOrCreateTag();
+		
+		// Override target pos, if present
+		if(nbtCompound.contains("PosX")) { nbtCompound.remove("PosX"); }
+		if(nbtCompound.contains("PosY")) { nbtCompound.remove("PosY"); }
+		if(nbtCompound.contains("PosZ")) { nbtCompound.remove("PosZ"); }
+		if(nbtCompound.contains("Dimension")) { nbtCompound.remove("Dimension"); }
+		
+		// Add player
+		nbtCompound.putString("TargetPlayerName", player.getName().asString());
+		nbtCompound.putUuid("TargetPlayerUUID", player.getUuid());
+		itemStack.setTag(nbtCompound);
 	}
 
+	public Optional<Pair<String, Vec3d>> getTeleportTargetPos(@NotNull ItemStack itemStack) {
+		NbtCompound nbtCompound = itemStack.getTag();
+		if(nbtCompound != null && nbtCompound.contains("PosX") && nbtCompound.contains("PosY") && nbtCompound.contains("PosZ") && nbtCompound.contains("Dimension")) {
+			String dimensionKeyString = nbtCompound.getString("Dimension");
+			double x = nbtCompound.getDouble("PosX");
+			double y = nbtCompound.getDouble("PosY");
+			double z = nbtCompound.getDouble("PosZ");
+			Vec3d pos = new Vec3d(x, y, z);
+			
+			return Optional.of(new Pair<>(dimensionKeyString, pos));
+		}
+		return Optional.empty();
+	}
+	
+	public Optional<UUID> getTeleportTargetPlayerUUID(@NotNull ItemStack itemStack) {
+		NbtCompound nbtCompound = itemStack.getTag();
+		if(nbtCompound != null && nbtCompound.contains("TargetPlayerUUID")) {
+			return Optional.of(nbtCompound.getUuid("TargetPlayerUUID"));
+		}
+		return Optional.empty();
+	}
+	
+	public Optional<String> getTeleportTargetPlayerName(@NotNull ItemStack itemStack) {
+		NbtCompound nbtCompound = itemStack.getTag();
+		if(nbtCompound != null && nbtCompound.contains("TargetPlayerName")) {
+			return Optional.of(nbtCompound.getString("TargetPlayerName"));
+		}
+		return Optional.empty();
+	}
 
 }

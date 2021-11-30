635dfbf7ac66f5b78bd539612c6651daed0c06a0
DaFuqs <dominik.m.schmidt@googlemail.com>
2021-11-30 09:52:18 +0100

    updated Patchouli to 1.18 & fixed mixin crash

--- src/main/java/de/dafuqs/spectrum/compat/REI/REIServerIntegration.java
+++ src/main/java/de/dafuqs/spectrum/compat/REI/REIServerIntegration.java
@@ -1,24 +1,10 @@
 package de.dafuqs.spectrum.compat.REI;
 
-import de.dafuqs.spectrum.compat.REI.info.PedestalGridMenuInfo;
-import de.dafuqs.spectrum.inventories.PedestalScreenHandler;
 import me.shedaniel.rei.api.common.display.DisplaySerializerRegistry;
-import me.shedaniel.rei.api.common.display.SimpleGridMenuDisplay;
 import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
-import me.shedaniel.rei.api.common.transfer.info.MenuInfoContext;
 import me.shedaniel.rei.api.common.transfer.info.MenuInfoRegistry;
-import me.shedaniel.rei.api.common.transfer.info.simple.SimpleGridMenuInfo;
-import me.shedaniel.rei.api.common.transfer.info.simple.SimpleMenuInfoProvider;
-import me.shedaniel.rei.plugin.common.BuiltinPlugin;
-
-import java.util.stream.IntStream;
 
 public class REIServerIntegration implements REIServerPlugin {
-    @Override
-    public void registerMenuInfo(MenuInfoRegistry registry) {
-        registry.register(BuiltinPlugin.CRAFTING, PedestalScreenHandler.class,
-        registry.register(PedestalCraftingCategory.ID, PedestalScreenHandler.class, SimpleMenuInfoProvider.of(PedestalGridMenuInfo::new)));
-    }
     
     @Override
     public void registerDisplaySerializer(DisplaySerializerRegistry registry) {
@@ -26,4 +12,11 @@ public class REIServerIntegration implements REIServerPlugin {
         REIServerPlugin.super.registerDisplaySerializer(registry);
         //registry.register(PedestalCraftingCategory.ID, PedestalCraftingRecipeDisplay.serializer());
     }
+    
+    @Override
+    public void registerMenuInfo(MenuInfoRegistry registry) {
+        //registry.register(BuiltinPlugin.CRAFTING, PedestalScreenHandler.class);
+        //registry.register(PedestalCraftingCategory.ID, PedestalScreenHandler.class, SimpleMenuInfoProvider.of(PedestalGridMenuInfo::new));
+    }
+    
 }

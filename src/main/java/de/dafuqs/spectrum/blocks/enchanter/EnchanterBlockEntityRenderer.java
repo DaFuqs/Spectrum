5920f54756b4744388d1b236f4f9cfb3a464c8f9
DaFuqs <dominik.m.schmidt@googlemail.com>
2021-12-29 17:57:57 +0100

    unlock private chest only after placing pedestal

--- src/main/java/de/dafuqs/spectrum/blocks/enchanter/EnchanterBlockEntityRenderer.java
+++ src/main/java/de/dafuqs/spectrum/blocks/enchanter/EnchanterBlockEntityRenderer.java
@@ -15,7 +15,6 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.*;
-import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 
 public class EnchanterBlockEntityRenderer implements BlockEntityRenderer<de.dafuqs.spectrum.blocks.enchanter.EnchanterBlockEntity> {
@@ -72,18 +71,20 @@ public class EnchanterBlockEntityRenderer implements BlockEntityRenderer<de.dafu
 		// The Experience Item rendered in the air
 		ItemStack experienceItemStack = blockEntity.inventory.getStack(1);
 		if(!experienceItemStack.isEmpty() && experienceItemStack.getItem() instanceof ExperienceStorageItem) {
-			render(blockEntity.getWorld(), ExperienceHelper.getExperienceOrbSizeForExperience(ExperienceStorageItem.getStoredExperience(experienceItemStack)), tickDelta, matrixStack, vertexConsumerProvider, experienceSpriteBrightness);
+			renderExperienceOrb((float) blockEntity.getWorld().getTime() + tickDelta, ExperienceHelper.getExperienceOrbSizeForExperience(ExperienceStorageItem.getStoredExperience(experienceItemStack)), matrixStack, vertexConsumerProvider, experienceSpriteBrightness);
 		}
+		
+		//renderLight(blockEntity, matrixStack, vertexConsumerProvider, 3, 0, null, null, null, null);
 	}
 	
-	public void render(World world, int orbSize, float tickDelta, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+	public void renderExperienceOrb(float timeWithTickDelta, int orbSize, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
 		matrixStack.push();
 		
 		float h = (float)(orbSize % 4 * 16) / 64.0F;
 		float k = (float)(orbSize % 4 * 16 + 16) / 64.0F;
 		float l = (float)(orbSize / 4 * 16) / 64.0F;
 		float m = (float)(orbSize / 4 * 16 + 16) / 64.0F;
-		float r = ((float) world.getTime() + tickDelta) / 2.0F;
+		float r = timeWithTickDelta / 2.0F;
 		int s = (int)((MathHelper.sin(r + 0.0F) + 1.0F) * 0.5F * 255.0F);
 		int u = (int)((MathHelper.sin(r + 4.1887903F) + 1.0F) * 0.1F * 255.0F);
 		
@@ -91,7 +92,7 @@ public class EnchanterBlockEntityRenderer implements BlockEntityRenderer<de.dafu
 		matrixStack.multiply(dispatcher.camera.getRotation());
 		matrixStack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(180.0F));
 		
-		float scale = 0.5F + (float) (Math.sin((world.getTime() + tickDelta) / 8.0) / 8.0);
+		float scale = 0.5F + (float) (Math.sin(timeWithTickDelta / 8.0) / 8.0);
 		matrixStack.scale(scale, scale, scale);
 		
 		VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(LAYER);
@@ -110,5 +111,137 @@ public class EnchanterBlockEntityRenderer implements BlockEntityRenderer<de.dafu
 	private static void vertex(@NotNull VertexConsumer vertexConsumer, Matrix4f positionMatrix, Matrix3f normalMatrix, float x, float y, int red, int green, int blue, float u, float v, int light) {
 		vertexConsumer.vertex(positionMatrix, x, y, 0.0F).color(red, green, blue, 128).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
 	}
+	
+	/*
+	public void renderUpgradeLines(EnchanterBlockEntity enchanterBlockEntity, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider,
+	                               int xOffsets, int yOffsets,
+	                               @Nullable Upgradeable.UpgradeType upgradeType1, @Nullable Upgradeable.UpgradeType upgradeType2, @Nullable Upgradeable.UpgradeType upgradeType3, @Nullable Upgradeable.UpgradeType upgradeType4) {
+		
+		BlockPos sourcePos = new BlockPos(0, 0, 0);
+		Vec3i sizeVector = new Vec3i(3, 3, 3);
+		
+		double sourceX = sourcePos.getX() + 0.5;
+		double sourceY = sourcePos.getY() + 0.5;
+		double sourceZ = sourcePos.getZ() + 0.5;
+		
+		double y2 = sourceY + sizeVector.getY();
+		double k = sizeVector.getX();
+		double l = sizeVector.getZ();
+		
+		double x1 = k < 0.0D ? sourceX + 1.0D : sourceX;
+		double z1 = l < 0.0D ? sourceZ + 1.0D : sourceZ;
+		double x2 = x1 + k;
+		double z2 = z1 + l;
+		
+		float alpha = 1.0F;
+		float red = 0.9F;
+		float xAxisRed = 0.5F;
+		
+		VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(RenderLayer.getLines());
+		drawLines(matrixStack, vertexConsumer, (float) x1, (float) sourceY, (float) z1, (float) x2, (float) y2, (float) z2, red, red, red, alpha, xAxisRed, xAxisRed, xAxisRed);
+	}
+	
+	public static void drawLines(MatrixStack matrices, VertexConsumer vertexConsumer, float x1, float y1, float z1, float x2, float y2, float z2, float red, float green, float blue, float alpha, float xAxisRed, float yAxisGreen, float zAxisBlue) {
+		Matrix4f matrix4f = matrices.peek().getPositionMatrix();
+		Matrix3f matrix3f = matrices.peek().getNormalMatrix();
+		
+		matrices.push();
+		matrices.scale(50.0F, 50.0F, 50.0F);
+		
+		vertexConsumer.vertex(matrix4f, x1, y1, z1).color(red, red, red, alpha).normal(matrix3f, -1.0F, 0.0F, 0.0F).next();
+		vertexConsumer.vertex(matrix4f, x1-3, y1 + 0.5F, z1-3).color(red, red, red, alpha).normal(matrix3f, -1.0F, 0.0F, 0.0F).next();
+		
+		vertexConsumer.vertex(matrix4f, x1, y1, z1).color(red, red, red, alpha).normal(matrix3f, 0.0F, -1.0F, 0.0F).next();
+		vertexConsumer.vertex(matrix4f, x1-3, y1 + 0.5F, z1+3).color(red, red, red, alpha).normal(matrix3f, 0.0F, -1.0F, 0.0F).next();
+		
+		vertexConsumer.vertex(matrix4f, x1, y1, z1).color(red, red, red, alpha).normal(matrix3f, 0.0F, 0.0F, -1.0F).next();
+		vertexConsumer.vertex(matrix4f, x1+3, y1 + 0.5F, z1-3).color(red, red, red, alpha).normal(matrix3f, 0.0F, 0.0F, -1.0F).next();
+		
+		vertexConsumer.vertex(matrix4f, x1, y1, z1).color(red, red, red, alpha).normal(matrix3f, 0.0F, -1.0F, 0.0F).next();
+		vertexConsumer.vertex(matrix4f, x1+3, y1 + 0.5F, z1+3).color(red, red, red, alpha).normal(matrix3f, 0.0F, -1.0F, 0.0F).next();
+		
+		matrices.pop();
+		
+		/*
+		vertexConsumer.vertex(matrix4f, i, j, h).color(red, green, blue, alpha).normal(matrix3f, -1.0F, 0.0F, 0.0F).next();
+		vertexConsumer.vertex(matrix4f, f, j, h).color(red, green, blue, alpha).normal(matrix3f, -1.0F, 0.0F, 0.0F).next();
+		vertexConsumer.vertex(matrix4f, f, j, h).color(red, green, blue, alpha).normal(matrix3f, 0.0F, 0.0F, 1.0F).next();
+		vertexConsumer.vertex(matrix4f, f, j, k).color(red, green, blue, alpha).normal(matrix3f, 0.0F, 0.0F, 1.0F).next();
+		vertexConsumer.vertex(matrix4f, f, j, k).color(red, green, blue, alpha).normal(matrix3f, 0.0F, -1.0F, 0.0F).next();
+		vertexConsumer.vertex(matrix4f, f, g, k).color(red, green, blue, alpha).normal(matrix3f, 0.0F, -1.0F, 0.0F).next();
+		vertexConsumer.vertex(matrix4f, f, g, k).color(red, green, blue, alpha).normal(matrix3f, 1.0F, 0.0F, 0.0F).next();
+		vertexConsumer.vertex(matrix4f, i, g, k).color(red, green, blue, alpha).normal(matrix3f, 1.0F, 0.0F, 0.0F).next();
+		vertexConsumer.vertex(matrix4f, i, g, k).color(red, green, blue, alpha).normal(matrix3f, 0.0F, 0.0F, -1.0F).next();
+		vertexConsumer.vertex(matrix4f, i, g, h).color(red, green, blue, alpha).normal(matrix3f, 0.0F, 0.0F, -1.0F).next();
+		vertexConsumer.vertex(matrix4f, f, j, k).color(red, green, blue, alpha).normal(matrix3f, 1.0F, 0.0F, 0.0F).next();
+		vertexConsumer.vertex(matrix4f, i, j, k).color(red, green, blue, alpha).normal(matrix3f, 1.0F, 0.0F, 0.0F).next();
+		vertexConsumer.vertex(matrix4f, i, g, k).color(red, green, blue, alpha).normal(matrix3f, 0.0F, 1.0F, 0.0F).next();
+		vertexConsumer.vertex(matrix4f, i, j, k).color(red, green, blue, alpha).normal(matrix3f, 0.0F, 1.0F, 0.0F).next();
+		vertexConsumer.vertex(matrix4f, i, j, h).color(red, green, blue, alpha).normal(matrix3f, 0.0F, 0.0F, 1.0F).next();
+		vertexConsumer.vertex(matrix4f, i, j, k).color(red, green, blue, alpha).normal(matrix3f, 0.0F, 0.0F, 1.0F).next();
+		
+	}*/
+	/*
+	public static void renderLight(MatrixStack matrixStack, VertexConsumerProvider buffer, Color color, long seed, int minScale, float scale, int count, int light) {
+		Random rand = new Random();
+		rand.setSeed(seed);
+		
+		int orbSize = 2;
+		float u = (float)(orbSize % 4 * 16) / 64.0F;
+		float v = (float)(orbSize % 4 * 16 + 16) / 64.0F;
+		
+		float f1 = MinecraftClient.getInstance().world.getTime() / 400.0F;
+		float f2 = 0.0F;
+		int alpha = 128;
+		//int alpha = (int) (255.0F * (1.0F - f2));
+		
+		VertexConsumer vb = buffer.getBuffer(LAYER);
+		MatrixStack.Entry entry = matrixStack.peek();
+		Matrix3f normalMatrix = entry.getNormalMatrix();
+		Matrix4f positionMatrix = entry.getPositionMatrix();
+		
+		matrixStack.push();
+		for (int i = 0; i < count; i++) {
+			matrixStack.push();
+			matrixStack.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(rand.nextFloat() * 360.0F));
+			matrixStack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(rand.nextFloat() * 360.0F));
+			matrixStack.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(rand.nextFloat() * 360.0F));
+			matrixStack.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(rand.nextFloat() * 360.0F));
+			matrixStack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(rand.nextFloat() * 360.0F));
+			matrixStack.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(rand.nextFloat() * 360.0F + f1 * 360.0F));
+
+			float fa = rand.nextFloat() * 20.0F + 5.0F + f2 * 10.0F;
+			float f4 = rand.nextFloat() * 2.0F + 1.0F + f2 * 2.0F;
+			float fa1 = 30.0F / (Math.min(minScale, 10 * scale) / 10.0F);
+			fa /= fa1;
+			f4 /= fa1;
+			
+			
+			vb.vertex(positionMatrix, 0F,      0F, 0F)        .color(color.getRed(), color.getGreen(), color.getBlue(), alpha).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
+			vb.vertex(positionMatrix, 0F,      0F, 0F)        .color(color.getRed(), color.getGreen(), color.getBlue(), alpha).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
+			vb.vertex(positionMatrix, -0.7F * f4, fa, -0.5F * f4).color(color.getRed(), color.getGreen(), color.getBlue(), 0).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
+			vb.vertex(positionMatrix,  0.7F * f4, fa, -0.5F * f4).color(color.getRed(), color.getGreen(), color.getBlue(), 0).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
+			vb.vertex(positionMatrix, 0F,     0F, 0F)        .color(color.getRed(), color.getGreen(), color.getBlue(), alpha).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
+			vb.vertex(positionMatrix, 0F,     0F, 0F)        .color(color.getRed(), color.getGreen(), color.getBlue(), alpha).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
+			vb.vertex(positionMatrix, 0.7F * f4, fa, -0.5F * f4).color(color.getRed(), color.getGreen(), color.getBlue(), 0).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
+			vb.vertex(positionMatrix, 0F,        fa,    1F * f4).color(color.getRed(), color.getGreen(), color.getBlue(), 0).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
+			vb.vertex(positionMatrix, 0F,      0F, 0F)        .color(color.getRed(), color.getGreen(), color.getBlue(), alpha).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
+			vb.vertex(positionMatrix, 0F,      0F, 0F)        .color(color.getRed(), color.getGreen(), color.getBlue(), alpha).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
+			vb.vertex(positionMatrix, 0F,         fa,    1F * f4).color(color.getRed(), color.getGreen(), color.getBlue(), 0).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
+			vb.vertex(positionMatrix, -0.7F * f4, fa, -0.5F * f4).color(color.getRed(), color.getGreen(), color.getBlue(), 0).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0.0F, 1.0F, 0.0F).next();
+			
+			matrixStack.pop();
+		}
+		matrixStack.pop();
+		
+		refreshDrawing(buffer, RenderLayer.getTranslucent());
+	}
+	
+	public static void refreshDrawing(VertexConsumerProvider vb, RenderLayer type) {
+		if (vb instanceof BufferBuilder) {
+			type.draw((BufferBuilder) vb, 0, 0, 0);
+			((BufferBuilder) vb).begin(type.getDrawMode(), type.getVertexFormat());
+		}
+	}*/
 	
 }

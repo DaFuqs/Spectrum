f0fa0408795d28c336c5b26ac007b4740dea36da
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-01-19 21:52:44 +0100

    PageHint work

--- src/main/java/de/dafuqs/spectrum/compat/patchouli/PageHint.java
+++ src/main/java/de/dafuqs/spectrum/compat/patchouli/PageHint.java
@@ -3,29 +3,32 @@ package de.dafuqs.spectrum.compat.patchouli;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.resource.language.I18n;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Identifier;
-import vazkii.patchouli.client.base.ClientAdvancements;
+import vazkii.patchouli.api.IVariable;
 import vazkii.patchouli.client.base.PersistentData;
 import vazkii.patchouli.client.book.BookContentsBuilder;
 import vazkii.patchouli.client.book.BookEntry;
+import vazkii.patchouli.client.book.BookPage;
+import vazkii.patchouli.client.book.gui.BookTextRenderer;
 import vazkii.patchouli.client.book.gui.GuiBook;
 import vazkii.patchouli.client.book.gui.GuiBookEntry;
-import vazkii.patchouli.client.book.page.abstr.PageWithText;
+import vazkii.patchouli.client.book.page.PageSpotlight;
 import vazkii.patchouli.common.book.Book;
 
-public class PageHint extends PageWithText {
+public class PageHint extends BookPage {
 	
-	Identifier trigger;
+	IVariable cost;
+	IVariable text;
+	transient BookTextRenderer textRender;
+	transient Ingredient ingredient;
+	transient boolean revealed;
+	
 	String title;
 	
-	public PageHint() {
-		super();
-	}
-	
-	@Override
 	public int getTextHeight() {
 		return 22;
 	}
@@ -33,26 +36,34 @@ public class PageHint extends PageWithText {
 	@Override
 	public void build(BookEntry entry, BookContentsBuilder builder, int pageNum) {
 		super.build(entry, builder, pageNum);
-		trigger = null;
+		ingredient = cost.as(Ingredient.class);
 	}
 	
-	public boolean isCompleted(Book book) {
+	public boolean isRevealed(Book book) {
 		return PersistentData.data.getBookData(book).completedManualQuests.contains(entry.getId().toString());
 	}
 	
 	@Override
 	public void onDisplayed(GuiBookEntry parent, int left, int top) {
 		super.onDisplayed(parent, left, top);
-		
-		boolean completed = isCompleted(parent.book);
-		if(!completed) {
-			ButtonWidget button = new ButtonWidget(GuiBook.PAGE_WIDTH / 2 - 50, GuiBook.PAGE_HEIGHT - 35, 100, 20, LiteralText.EMPTY, this::questButtonClicked);
+		Text displayedText = text.as(Text.class);
+
+		revealed = isRevealed(parent.book);
+		if(!revealed) {
+			ButtonWidget button = new ButtonWidget(GuiBook.PAGE_WIDTH / 2 - 50, GuiBook.PAGE_HEIGHT - 35, 100, 20, LiteralText.EMPTY, this::revealHintButtonClicked);
 			addButton(button);
 			button.setMessage(new TranslatableText("spectrum.gui.lexicon.reveal_hint"));
+			displayedText = new LiteralText("$(obf)" + displayedText.getString());
 		}
+		textRender = new BookTextRenderer(parent, displayedText, 0, getTextHeight());
 	}
 	
-	protected void questButtonClicked(ButtonWidget button) {
+	@Override
+	public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
+		return textRender.click(mouseX, mouseY, mouseButton);
+	}
+	
+	protected void revealHintButtonClicked(ButtonWidget button) {
 		String res = entry.getId().toString();
 		PersistentData.DataHolder.BookData data = PersistentData.data.getBookData(parent.book);
 		
@@ -61,6 +72,14 @@ public class PageHint extends PageWithText {
 		} else {
 			data.completedManualQuests.add(res);
 		}
+		
+		Text displayedText = text.as(Text.class);
+		textRender = new BookTextRenderer(parent, displayedText, 0, getTextHeight());
+		
+		// TODO: pay cost
+		// TODO: reveal cloaked entry
+		// TODO: give player "used hint" advancement
+		
 		PersistentData.save();
 		entry.markReadStateDirty();
 	}
@@ -68,6 +87,11 @@ public class PageHint extends PageWithText {
 	@Override
 	public void render(MatrixStack ms, int mouseX, int mouseY, float pticks) {
 		super.render(ms, mouseX, mouseY, pticks);
+		
+		textRender.render(ms, mouseX, mouseY);
+		if(!revealed) {
+			parent.renderIngredient(ms, GuiBook.PAGE_WIDTH / 2 + 23, GuiBook.PAGE_HEIGHT - 34, mouseX, mouseY, ingredient);
+		}
 		
 		parent.drawCenteredStringNoShadow(ms, title == null || title.isEmpty() ? I18n.translate("patchouli.gui.lexicon.objective") : i18n(title), GuiBook.PAGE_WIDTH / 2, 0, book.headerColor);
 		GuiBook.drawSeparator(ms, book, 0, 12);

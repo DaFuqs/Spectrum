899cd454c822094db5daa6c3ee29d62b1e11fe68
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-02-26 21:26:40 +0100

    base blocks for creature spawn and spitit instiller

---
+++ src/main/java/de/dafuqs/spectrum/blocks/creature_spawn/CreatureSpawnBlock.java
@@ -0,0 +1,41 @@
+package de.dafuqs.spectrum.blocks.creature_spawn;
+
+import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class CreatureSpawnBlock extends BlockWithEntity {
+	
+	protected static final VoxelShape SHAPE = Block.createCuboidShape(3.0D, 0.0D, 3.0D, 13.0D, 13.0D, 13.0D);
+	
+	public CreatureSpawnBlock(Settings settings) {
+		super(settings);
+	}
+	
+	@Override
+	public BlockRenderType getRenderType(BlockState state) {
+		return BlockRenderType.MODEL;
+	}
+	
+	
+	@Nullable
+	@Override
+	public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+		return new CreatureSpawnBlockEntity(pos, state);
+	}
+	
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		return SHAPE;
+	}
+	
+}
--- src/main/java/de/dafuqs/spectrum/blocks/spirit_instiller/SpiritInstillerBlock.java
+++ src/main/java/de/dafuqs/spectrum/blocks/spirit_instiller/SpiritInstillerBlock.java
@@ -1,30 +1,44 @@
 package de.dafuqs.spectrum.blocks.spirit_instiller;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class SpiritInstillerBlock extends BlockWithEntity {
 	
-	protected SpiritInstillerBlock(Settings settings) {
+	protected static final VoxelShape SHAPE = Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 15.0D, 16.0D);
+	
+	public SpiritInstillerBlock(Settings settings) {
 		super(settings);
 	}
+	
+	@Override
+	public BlockRenderType getRenderType(BlockState state) {
+		return BlockRenderType.MODEL;
+	}
+	
 	
 	@Nullable
 	@Override
 	public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-		return null;
+		return new SpiritInstillerBlockEntity(pos, state);
 	}
 	
 	@Nullable
 	@Override
 	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
 		return super.getTicker(world, state, type);
+	}
+	
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		return SHAPE;
 	}
 	
 }

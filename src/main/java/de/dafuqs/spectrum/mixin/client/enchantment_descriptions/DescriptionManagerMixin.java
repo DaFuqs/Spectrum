228800b6cf932bc08b68c59a3243cc5b78c166b8
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-03-05 14:54:17 +0100

    removed palette slot for now (not needed yet)

--- src/main/java/de/dafuqs/spectrum/mixin/client/enchantment_descriptions/DescriptionManagerMixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/client/enchantment_descriptions/DescriptionManagerMixin.java
@@ -1,26 +1,38 @@
 package de.dafuqs.spectrum.mixin.client.enchantment_descriptions;
 
+import de.dafuqs.spectrum.enchantments.SpectrumEnchantment;
 import net.darkhax.enchdesc.DescriptionManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Language;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * If Enchantment descriptions is installed:
  * Obfuscate descriptions for Enchantments that the user does not have discovered yet
  */
-@Mixin(DescriptionManager.class)
+/*@Pseudo
+@Mixin(value = DescriptionManager.class, remap = false)
 public class DescriptionManagerMixin {
 	
-	// TODO
-	// breaks in prod
-	/*private static final Language language = Language.getInstance();
+	private static final Language spectrum$language = Language.getInstance();
 	
-	@Inject(at = @At("RETURN"), method= "Lnet/darkhax/enchdesc/DescriptionManager;getDescription(L;)L;", cancellable = true, remap = false, locals = LocalCapture.CAPTURE_FAILSOFT)
-	private static void getDescription(Enchantment ench, CallbackInfoReturnable<Text> cir) {
+	@Inject(at = @At("RETURN"), method= "Lnet/darkhax/enchdesc/DescriptionManager;getDescription(L;)L;", cancellable = true)
+	private static void spectrum$obfuscateEnchDescDescription(Enchantment ench, CallbackInfoReturnable<Text> cir) {
 		if(ench instanceof SpectrumEnchantment spectrumEnchantment) {
 			if(!spectrumEnchantment.canEntityUse(MinecraftClient.getInstance().player)) {
-				cir.setReturnValue(new LiteralText(language.get(((TranslatableText) cir.getReturnValue()).getKey())).formatted(Formatting.OBFUSCATED).formatted(Formatting.DARK_GRAY));
+				cir.setReturnValue(new LiteralText(spectrum$language.get(((TranslatableText) cir.getReturnValue()).getKey())).formatted(Formatting.OBFUSCATED).formatted(Formatting.DARK_GRAY));
 			}
 		}
-	}*/
+	}
 
 }
+*/

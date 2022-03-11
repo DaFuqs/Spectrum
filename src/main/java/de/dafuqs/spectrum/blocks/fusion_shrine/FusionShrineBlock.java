cd6852e764e8334b9203e74a7673dba3c86da6ee
Isegrim <dominik.m.schmidt@googlemail.com>
2022-03-11 09:34:28 +0100

    if recipe required advancements are not set: fallback to the one used to craft the block itself

--- src/main/java/de/dafuqs/spectrum/blocks/fusion_shrine/FusionShrineBlock.java
+++ src/main/java/de/dafuqs/spectrum/blocks/fusion_shrine/FusionShrineBlock.java
@@ -45,7 +45,7 @@ import java.util.Optional;
 
 public class FusionShrineBlock extends BlockWithEntity {
 	
-	public static final Identifier UNLOCK_IDENTIFIER = new Identifier(SpectrumCommon.MOD_ID, "build_fusion_shrine");
+	public static final Identifier UNLOCK_IDENTIFIER = new Identifier(SpectrumCommon.MOD_ID, "collect_all_basic_pigments_besides_brown");
 	
 	public static final IntProperty LIGHT_LEVEL = IntProperty.of("light_level", 0, 15);
 	protected static final VoxelShape SHAPE = Block.createCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 15.0D, 14.0D);

fee5a32269d936e90427bed69da200017db9dea0
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-04-19 19:12:50 +0200

    backport Fusion Shrine recipe optimizations. Fixed crash with cosmetic armor. Resolves #53

--- src/main/java/de/dafuqs/spectrum/mixin/client/HeadFeatureRendererMixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/client/HeadFeatureRendererMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.client.render.entity.feature.HeadFeatureRenderer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.render.entity.model.ModelWithHead;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -18,19 +17,25 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(HeadFeatureRenderer.class)
 public class HeadFeatureRendererMixin<T extends LivingEntity, M extends EntityModel<T> & ModelWithHead> {
 
 	@Inject(method= "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/block/AbstractSkullBlock;getSkullType()Lnet/minecraft/block/SkullBlock$SkullType;"),
-			cancellable = true)
-	private void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int light, T livingEntity, float animationProgress, float g, float h, float j, float k, float l, CallbackInfo ci) {
-		ItemStack itemStack = livingEntity.getEquippedStack(EquipmentSlot.HEAD);
-		Item item = itemStack.getItem();
-		
-		if (item instanceof BlockItem && ((BlockItem)item).getBlock() instanceof SpectrumSkullBlock) {
-			SkullBlock.SkullType skullType = ((SpectrumSkullBlock)((BlockItem)item).getBlock()).getSkullType();
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPart;rotate(Lnet/minecraft/client/util/math/MatrixStack;)V", shift = At.Shift.AFTER),
+			cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
+	private void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int light, T livingEntity, float animationProgress, float h, float j, float k, float l, float m, CallbackInfo ci, ItemStack itemStack, Item item, boolean bl) {
+		if (item instanceof BlockItem && ((BlockItem)item).getBlock() instanceof SpectrumSkullBlock spectrumSkullBlock) {
+			m = 1.1875F;
+			matrixStack.scale(m, -m, -m);
+			if (bl) {
+				matrixStack.translate(0.0D, 0.0625D, 0.0D);
+			}
+			
+			matrixStack.translate(-0.5D, 0.0D, -0.5D);
+			
+			SkullBlock.SkullType skullType = spectrumSkullBlock.getSkullType();
 			RenderLayer renderLayer = SpectrumSkullBlockEntityRenderer.getRenderLayer(skullType);
 			SpectrumSkullBlockEntityRenderer.renderSkull(null, 180.0F, animationProgress, matrixStack, vertexConsumerProvider, light, renderLayer);
 			matrixStack.pop();

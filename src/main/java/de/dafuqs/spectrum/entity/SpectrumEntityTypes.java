f248b820c1664b331fed2ce6e079bd4e2a95f66d
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-04-25 09:10:10 +0200

    fixed phantom frames not rendering from afar

--- src/main/java/de/dafuqs/spectrum/entity/SpectrumEntityTypes.java
+++ src/main/java/de/dafuqs/spectrum/entity/SpectrumEntityTypes.java
@@ -20,15 +20,15 @@ public class SpectrumEntityTypes {
 	public static EntityType<? extends ThrownItemEntity> BLOCK_FLOODER_PROJECTILE;
 
 	public static void register() {
-		SHOOTING_STAR = register("shooting_star", 240, 20, true, EntityDimensions.changing(0.8F, 0.8F), true, ShootingStarEntity::new);
-		GRAVITY_BLOCK = register("gravity_block", 160, 20, true, EntityDimensions.changing(0.98F, 0.98F), true, GravityBlockEntity::new);
+		SHOOTING_STAR = register("shooting_star", 15, 20, true, EntityDimensions.changing(0.8F, 0.8F), true, ShootingStarEntity::new);
+		GRAVITY_BLOCK = register("gravity_block", 10, 20, true, EntityDimensions.changing(0.98F, 0.98F), true, GravityBlockEntity::new);
 		INVISIBLE_ITEM_FRAME = register("invisible_item_frame", 10, 2147483647, false, EntityDimensions.changing(0.5F, 0.5F), false, InvisibleItemFrameEntity::new);
 		INVISIBLE_GLOW_ITEM_FRAME = register("invisible_glow_item_frame", 10, 2147483647, false, EntityDimensions.changing(0.5F, 0.5F), false, InvisibleGlowItemFrameEntity::new);
 		BLOCK_FLOODER_PROJECTILE = register("block_flooder_projectile", 4, 10, true, EntityDimensions.changing(0.25F, 0.25F), true, BlockFlooderProjectile::new);
 	}
 
 	public static <X extends Entity> EntityType<X> register(String name, int trackingDistance, int updateIntervalTicks, boolean alwaysUpdateVelocity, EntityDimensions size, boolean fireImmune, EntityType.EntityFactory<X> factory) {
-		FabricEntityTypeBuilder<X> builder = FabricEntityTypeBuilder.create(SpawnGroup.MISC, factory).trackable(trackingDistance, updateIntervalTicks, alwaysUpdateVelocity).dimensions(size);
+		FabricEntityTypeBuilder<X> builder = FabricEntityTypeBuilder.create(SpawnGroup.MISC, factory).trackRangeChunks(trackingDistance).trackedUpdateRate(updateIntervalTicks).forceTrackedVelocityUpdates(alwaysUpdateVelocity).dimensions(size);
 		if(fireImmune) {
 			builder.fireImmune();
 		}
--- src/main/java/de/dafuqs/spectrum/mixin/EntityTypeMixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/EntityTypeMixin.java
@@ -1,9 +1,20 @@
 package de.dafuqs.spectrum.mixin;
 
+import de.dafuqs.spectrum.entity.SpectrumEntityTypes;
 import net.minecraft.entity.EntityType;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityType.class)
 public class EntityTypeMixin {
-
+	
+	@Inject(at = @At("RETURN"), method = "alwaysUpdateVelocity()Z", cancellable = true)
+	public void alwaysUpdateVelocity(CallbackInfoReturnable<Boolean> cir) {
+		if(cir.getReturnValue() && ((Object) this != SpectrumEntityTypes.INVISIBLE_ITEM_FRAME || (Object) this != SpectrumEntityTypes.INVISIBLE_GLOW_ITEM_FRAME)) {
+			cir.setReturnValue(false);
+		}
+	}
+	
 }

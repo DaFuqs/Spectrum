e7a9e2b92184bffe37bbe742a721a8048c4f2595
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-04-30 11:21:27 +0200

    improved FirestarterMobBlock

--- src/main/java/de/dafuqs/spectrum/blocks/mob_blocks/FirestarterMobBlock.java
+++ src/main/java/de/dafuqs/spectrum/blocks/mob_blocks/FirestarterMobBlock.java
@@ -8,12 +8,14 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.property.Properties;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.event.GameEvent;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -26,33 +28,31 @@ public class FirestarterMobBlock extends MobBlock {
 	
 	@Override
 	public boolean trigger(ServerWorld world, BlockPos blockPos, BlockState state, @Nullable Entity entity, Direction side) {
-		if(entity != null) {
-			entity.setOnFireFor(40);
-		}
 		for(Direction direction : Direction.values()) {
 			causeFire(world, blockPos.offset(direction), direction);
 		}
 		return true;
 	}
 	
-	public static boolean causeFire(ServerWorld world, BlockPos blockPos, Direction side) {
+	public static void causeFire(@NotNull ServerWorld world, BlockPos blockPos, Direction side) {
 		BlockState blockState = world.getBlockState(blockPos);
-		if (!CampfireBlock.canBeLit(blockState) && !CandleBlock.canBeLit(blockState) && !CandleCakeBlock.canBeLit(blockState)) {
-			BlockPos blockPos2 = blockPos.offset(side);
-			if (AbstractFireBlock.canPlaceAt(world, blockPos2, side)) {
-				world.playSound(null, blockPos2, SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.BLOCKS, 1.0F, world.getRandom().nextFloat() * 0.4F + 0.8F);
-				BlockState blockState2 = AbstractFireBlock.getState(world, blockPos2);
-				world.setBlockState(blockPos2, blockState2, 11);
-				world.emitGameEvent(null, GameEvent.BLOCK_PLACE, blockPos);
-				return true;
-			} else {
-				return false;
-			}
-		} else {
+		if (CampfireBlock.canBeLit(blockState) || CandleBlock.canBeLit(blockState) || CandleCakeBlock.canBeLit(blockState)) {
+			// light lightable blocks
 			world.playSound(null, blockPos, SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.BLOCKS, 1.0F, world.getRandom().nextFloat() * 0.4F + 0.8F);
 			world.setBlockState(blockPos, blockState.with(Properties.LIT, true), 11);
 			world.emitGameEvent(null, GameEvent.BLOCK_PLACE, blockPos);
-			return true;
+		} else if(blockState.isIn(BlockTags.ICE)) {
+			// smelt ice
+			world.playSound(null, blockPos, SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.BLOCKS, 1.0F, world.getRandom().nextFloat() * 0.4F + 0.8F);
+			world.setBlockState(blockPos, Blocks.WATER.getDefaultState());
+		} else {
+			// place fire
+			if (AbstractFireBlock.canPlaceAt(world, blockPos, side)) {
+				world.playSound(null, blockPos, SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.BLOCKS, 1.0F, world.getRandom().nextFloat() * 0.4F + 0.8F);
+				BlockState blockState2 = AbstractFireBlock.getState(world, blockPos);
+				world.setBlockState(blockPos, blockState2, 11);
+				world.emitGameEvent(null, GameEvent.BLOCK_PLACE, blockPos);
+			}
 		}
 	}
 	

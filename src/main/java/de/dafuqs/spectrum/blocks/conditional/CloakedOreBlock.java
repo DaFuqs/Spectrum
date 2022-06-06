9f4758a14bfb5293b212355bb70316f051448cb4
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-06-06 17:42:41 +0200

    cloaked ore revamp. Hack to decide weather to drop or not drop XP

--- src/main/java/de/dafuqs/spectrum/blocks/conditional/CloakedOreBlock.java
+++ src/main/java/de/dafuqs/spectrum/blocks/conditional/CloakedOreBlock.java
@@ -1,50 +1,74 @@
 package de.dafuqs.spectrum.blocks.conditional;
 
 import de.dafuqs.spectrum.interfaces.Cloakable;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.OreBlock;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.context.LootContext;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.Pair;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 import java.util.Hashtable;
 import java.util.List;
 
-public abstract class CloakedOreBlock extends OreBlock implements Cloakable {
+public class CloakedOreBlock extends OreBlock implements Cloakable {
 	
-	private final boolean deepSlateOre;
-	
-	public CloakedOreBlock(Settings settings, UniformIntProvider uniformIntProvider, boolean deepSlateOre) {
+	protected final Identifier cloakAdvancementIdentifier;
+	protected final BlockState cloakBlockState;
+	protected static boolean dropXP;
+
+	public CloakedOreBlock(Settings settings, UniformIntProvider uniformIntProvider, Identifier cloakAdvancementIdentifier, BlockState cloakBlockState) {
 		super(settings, uniformIntProvider);
-		this.deepSlateOre = deepSlateOre;
+		this.cloakAdvancementIdentifier = cloakAdvancementIdentifier;
+		this.cloakBlockState = cloakBlockState;
+		registerCloak();
 	}
-	
+
 	@Override
 	public Hashtable<BlockState, BlockState> getBlockStateCloaks() {
 		Hashtable<BlockState, BlockState> hashtable = new Hashtable<>();
-		if (deepSlateOre) {
-			hashtable.put(this.getDefaultState(), Blocks.DEEPSLATE.getDefaultState());
-		} else {
-			hashtable.put(this.getDefaultState(), Blocks.STONE.getDefaultState());
-		}
+		hashtable.put(this.getDefaultState(), cloakBlockState);
 		return hashtable;
 	}
 	
 	@Override
-	public Pair<Item, Item> getItemCloak() {
-		if (deepSlateOre) {
-			return new Pair<>(this.asItem(), Blocks.DEEPSLATE.asItem());
-		} else {
-			return new Pair<>(this.asItem(), Blocks.STONE.asItem());
-		}
+	public Identifier getCloakAdvancementIdentifier() {
+		return cloakAdvancementIdentifier;
 	}
 	
+	@Override
+	public Pair<Item, Item> getItemCloak() {
+		return new Pair<>(this.asItem(), cloakBlockState.getBlock().asItem());
+	}
+
 	@Deprecated
 	public List<ItemStack> getDroppedStacks(BlockState state, LootContext.Builder builder) {
+		// workaround: since onStacksDropped() has no way of checking if it was
+		// triggered by a player we have to cache that information here
+		PlayerEntity lootPlayerEntity = getLootPlayerEntity(builder);
+		if(lootPlayerEntity != null && isVisibleTo(lootPlayerEntity)) {
+			dropXP = true;
+		} else {
+			dropXP = false;
+		}
+		
 		return getCloakedDroppedStacks(state, builder);
 	}
 	
+	@Override
+	public void onStacksDropped(BlockState state, ServerWorld world, BlockPos pos, ItemStack stack) {
+		if(dropXP) {
+			super.onStacksDropped(state, world, pos, stack);
+		}
+	}
+
 }
--- src/main/java/de/dafuqs/spectrum/blocks/conditional/GemstoneOreBlock.java
+++ src/main/java/de/dafuqs/spectrum/blocks/conditional/GemstoneOreBlock.java
@@ -1,24 +1,21 @@
 package de.dafuqs.spectrum.blocks.conditional;
 
 import de.dafuqs.spectrum.enums.GemstoneColor;
+import net.minecraft.block.BlockState;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class GemstoneOreBlock extends CloakedOreBlock {
-	
-	private final Identifier cloakAdvancementIdentifier;
+
 	private final GemstoneColor gemstoneColor;
-	
-	public GemstoneOreBlock(Settings settings, UniformIntProvider experienceDropped, GemstoneColor gemstoneColor, boolean deepSlateOre, Identifier cloakAdvancementIdentifier) {
-		super(settings, experienceDropped, deepSlateOre);
+
+	public GemstoneOreBlock(Settings settings, UniformIntProvider experienceDropped, GemstoneColor gemstoneColor, Identifier cloakAdvancementIdentifier, BlockState cloakBlockState) {
+		super(settings, experienceDropped, cloakAdvancementIdentifier, cloakBlockState);
 		this.gemstoneColor = gemstoneColor;
-		this.cloakAdvancementIdentifier = cloakAdvancementIdentifier;
-		registerCloak();
 	}
 	
-	@Override
-	public Identifier getCloakAdvancementIdentifier() {
-		return cloakAdvancementIdentifier;
+	public GemstoneColor getGemstoneColor() {
+		return gemstoneColor;
 	}
 	
 }

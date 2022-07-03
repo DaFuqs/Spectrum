20917c43a46d10ab76607587ce1bf65c2142f579
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-07-03 14:17:50 +0200

    make Dike Gates have a outline when holding one to make it easier to build with them in creative

--- src/main/java/de/dafuqs/spectrum/blocks/structure/DikeGateBlock.java
+++ src/main/java/de/dafuqs/spectrum/blocks/structure/DikeGateBlock.java
@@ -45,7 +45,11 @@ public class DikeGateBlock extends AbstractGlassBlock {
 	}
 	
 	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-		return getCollisionShape(state, world, pos, context);
+		if(context.isHolding(this.asItem())) {
+			return VoxelShapes.fullCube();
+		} else {
+			return getCollisionShape(state, world, pos, context);
+		}
 	}
 	
 	@Override

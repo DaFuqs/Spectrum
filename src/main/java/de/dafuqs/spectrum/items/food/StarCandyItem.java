9f086fcef7bd6394e02dc99bc880710234bbfe4b
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-10-11 20:47:25 +0200

    infused beverage recipes

--- src/main/java/de/dafuqs/spectrum/items/food/StarCandyItem.java
+++ src/main/java/de/dafuqs/spectrum/items/food/StarCandyItem.java
@@ -1,9 +1,15 @@
 package de.dafuqs.spectrum.items.food;
 
+import de.dafuqs.spectrum.registries.SpectrumItems;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class StarCandyItem extends Item {
+	
+	public static final float ENCHANTED_STAR_CANDY_CHANCE = 0.5F;
 	
 	public static final FoodComponent FOOD_COMPONENT = new FoodComponent.Builder()
 			.hunger(2).saturationModifier(0.25F)
@@ -11,6 +17,19 @@ public class StarCandyItem extends Item {
 	
 	public StarCandyItem(Settings settings) {
 		super(settings);
+	}
+	
+	@Override
+	public void onCraft(ItemStack stack, World world, PlayerEntity player) {
+		super.onCraft(stack, world, player);
+		if(world.random.nextFloat() < ENCHANTED_STAR_CANDY_CHANCE) {
+			int count = stack.getCount();
+			stack.setCount(0);
+			
+			ItemStack enchantedStack = SpectrumItems.ENCHANTED_STAR_CANDY.getDefaultStack();
+			enchantedStack.setCount(count);
+			player.giveItemStack(enchantedStack);
+		}
 	}
 	
 }

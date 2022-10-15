49f7ae3d2fbfd39275934f11fd914f9894ad7736
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-10-15 15:14:04 +0200

    finished titration barrel advancements

--- src/main/java/de/dafuqs/spectrum/progression/advancement/TitrationBarrelTappingCriterion.java
+++ src/main/java/de/dafuqs/spectrum/progression/advancement/TitrationBarrelTappingCriterion.java
@@ -20,8 +20,8 @@ public class TitrationBarrelTappingCriterion extends AbstractCriterion<Titration
 	
 	static final Identifier ID = SpectrumCommon.locate("titration_barrel_tapping");
 	
-	public static TitrationBarrelTappingCriterion.Conditions create(ItemPredicate[] item, NumberRange.IntRange ingameDaysAgeRange) {
-		return new TitrationBarrelTappingCriterion.Conditions(EntityPredicate.Extended.EMPTY, item, ingameDaysAgeRange);
+	public static TitrationBarrelTappingCriterion.Conditions create(ItemPredicate[] item, NumberRange.IntRange ingameDaysAgeRange, NumberRange.IntRange ingredientCountRange) {
+		return new TitrationBarrelTappingCriterion.Conditions(EntityPredicate.Extended.EMPTY, item, ingameDaysAgeRange, ingredientCountRange);
 	}
 	
 	public Identifier getId() {
@@ -31,34 +31,38 @@ public class TitrationBarrelTappingCriterion extends AbstractCriterion<Titration
 	public TitrationBarrelTappingCriterion.Conditions conditionsFromJson(JsonObject jsonObject, EntityPredicate.Extended extended, AdvancementEntityPredicateDeserializer advancementEntityPredicateDeserializer) {
 		ItemPredicate[] tappedItemPredicates = ItemPredicate.deserializeAll(jsonObject.get("items"));
 		NumberRange.IntRange ingameDaysAgeRange = NumberRange.IntRange.fromJson(jsonObject.get("age_ingame_days"));
-		return new TitrationBarrelTappingCriterion.Conditions(extended, tappedItemPredicates, ingameDaysAgeRange);
+		NumberRange.IntRange ingredientCountRange = NumberRange.IntRange.fromJson(jsonObject.get("ingredient_count"));
+		return new TitrationBarrelTappingCriterion.Conditions(extended, tappedItemPredicates, ingameDaysAgeRange, ingredientCountRange);
 	}
 	
-	public void trigger(ServerPlayerEntity player, ItemStack itemStack, int ingameDaysAge) {
+	public void trigger(ServerPlayerEntity player, ItemStack itemStack, int ingameDaysAge, int ingredientCount) {
 		this.trigger(player, (conditions) -> {
-			return conditions.matches(itemStack, ingameDaysAge);
+			return conditions.matches(itemStack, ingameDaysAge, ingredientCount);
 		});
 	}
 	
 	public static class Conditions extends AbstractCriterionConditions {
 		private final ItemPredicate[] tappedItemPredicates;
 		private final NumberRange.IntRange ingameDaysAgeRange;
+		private final NumberRange.IntRange ingredientCountRange;
 		
-		public Conditions(EntityPredicate.Extended player, ItemPredicate[] tappedItemPredicates, NumberRange.IntRange ingameDaysAgeRange) {
+		public Conditions(EntityPredicate.Extended player, ItemPredicate[] tappedItemPredicates, NumberRange.IntRange ingameDaysAgeRange, NumberRange.IntRange ingredientCountRange) {
 			super(ID, player);
 			this.tappedItemPredicates = tappedItemPredicates;
 			this.ingameDaysAgeRange = ingameDaysAgeRange;
+			this.ingredientCountRange = ingredientCountRange;
 		}
 		
 		public JsonObject toJson(AdvancementEntityPredicateSerializer predicateSerializer) {
 			JsonObject jsonObject = super.toJson(predicateSerializer);
 			jsonObject.addProperty("items", this.tappedItemPredicates.toString());
 			jsonObject.add("age_ingame_days", this.ingameDaysAgeRange.toJson());
+			jsonObject.add("ingredient_count", this.ingredientCountRange.toJson());
 			return jsonObject;
 		}
 		
-		public boolean matches(ItemStack itemStack, int experience) {
-			if (this.ingameDaysAgeRange.test(experience)) {
+		public boolean matches(ItemStack itemStack, int experience, int ingredientCount) {
+			if (this.ingameDaysAgeRange.test(experience) && this.ingredientCountRange.test(ingredientCount)) {
 				List<ItemPredicate> list = new ObjectArrayList(this.tappedItemPredicates);
 				if (list.isEmpty()) {
 					return true;

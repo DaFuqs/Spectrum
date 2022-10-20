b8fc40a25da785c8125e6ca734407e9a32acdb72
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-10-20 22:00:26 +0200

    amaranth bushels can only be planted on dirt & co.

--- src/main/java/de/dafuqs/spectrum/blocks/conditional/amaranth/AmaranthBushelBlock.java
+++ src/main/java/de/dafuqs/spectrum/blocks/conditional/amaranth/AmaranthBushelBlock.java
@@ -1,16 +1,10 @@
 package de.dafuqs.spectrum.blocks.conditional.amaranth;
 
 import de.dafuqs.revelationary.api.revelations.RevelationAware;
-import de.dafuqs.spectrum.SpectrumCommon;
-import de.dafuqs.spectrum.registries.SpectrumItems;
-import net.id.incubus_core.block.TallCropBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.TallPlantBlock;
-import net.minecraft.block.enums.DoubleBlockHalf;
+import net.minecraft.block.PlantBlock;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemConvertible;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Pair;
 import org.jetbrains.annotations.Nullable;
@@ -18,7 +12,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Hashtable;
 import java.util.Map;
 
-public class AmaranthBushelBlock extends Block implements RevelationAware {
+public class AmaranthBushelBlock extends PlantBlock implements RevelationAware {
 	
 	public AmaranthBushelBlock(Settings settings) {
 		super(settings);

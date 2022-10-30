948ac5fe5bc150991f2dc665160574bb4267c1bf
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-10-30 19:25:55 +0100

    cloak blood orchids as red tulips, until petals found. Make them have no offset

--- src/main/java/de/dafuqs/spectrum/blocks/conditional/BloodOrchidBlock.java
+++ src/main/java/de/dafuqs/spectrum/blocks/conditional/BloodOrchidBlock.java
@@ -1,13 +1,17 @@
-package de.dafuqs.spectrum.blocks;
+package de.dafuqs.spectrum.blocks.conditional;
 
+import de.dafuqs.revelationary.api.revelations.RevelationAware;
+import de.dafuqs.spectrum.SpectrumCommon;
 import de.dafuqs.spectrum.helpers.Support;
 import de.dafuqs.spectrum.progression.SpectrumAdvancementCriteria;
 import de.dafuqs.spectrum.registries.SpectrumItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.FlowerBlock;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
@@ -17,19 +21,24 @@ import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Pair;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.Random;
 
-public class BloodOrchidBlock extends FlowerBlock {
+public class BloodOrchidBlock extends FlowerBlock implements RevelationAware {
 	
 	public static final IntProperty AGE = Properties.AGE_5;
 	
 	public BloodOrchidBlock(StatusEffect suspiciousStewEffect, int effectDuration, Settings settings) {
 		super(suspiciousStewEffect, effectDuration, settings);
 		this.setDefaultState(this.stateManager.getDefaultState().with(AGE, 0));
+		RevelationAware.register(this);
 	}
 	
 	@Override
@@ -63,6 +72,30 @@ public class BloodOrchidBlock extends FlowerBlock {
 			}
 		}
 		return ActionResult.PASS;
+	}
+	
+	@Override
+	public OffsetType getOffsetType() {
+		return OffsetType.NONE;
+	}
+	
+	@Override
+	public Identifier getCloakAdvancementIdentifier() {
+		return SpectrumCommon.locate("midgame/collect_blood_orchid_petal");
+	}
+	
+	@Override
+	public Map<BlockState, BlockState> getBlockStateCloaks() {
+		Hashtable<BlockState, BlockState> hashtable = new Hashtable<>();
+		for(int i = 0; i <= Properties.AGE_5_MAX; i++) {
+			hashtable.put(this.getDefaultState().with(AGE, i), Blocks.RED_TULIP.getDefaultState());
+		}
+		return hashtable;
+	}
+	
+	@Override
+	public Pair<Item, Item> getItemCloak() {
+		return new Pair<>(this.asItem(), Blocks.RED_TULIP.asItem());
 	}
 	
 }

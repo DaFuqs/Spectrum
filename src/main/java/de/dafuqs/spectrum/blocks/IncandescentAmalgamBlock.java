8896c614057c7423c8a3cb662ec3da78fd6aba9b
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-11-10 21:13:44 +0100

    amalgam can be waterlogged

--- src/main/java/de/dafuqs/spectrum/blocks/IncandescentAmalgamBlock.java
+++ src/main/java/de/dafuqs/spectrum/blocks/IncandescentAmalgamBlock.java
@@ -5,32 +5,65 @@ import de.dafuqs.spectrum.registries.SpectrumEnchantments;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.block.Waterloggable;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import net.minecraft.world.explosion.Explosion;
 import net.minecraft.world.explosion.ExplosionBehavior;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Random;
 
-public class IncandescentAmalgamBlock extends Block {
+public class IncandescentAmalgamBlock extends Block implements Waterloggable {
+	
+	public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 	
 	protected static final VoxelShape SHAPE = Block.createCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 9.0D, 12.0D);
 	
 	public IncandescentAmalgamBlock(Settings settings) {
 		super(settings);
+		this.setDefaultState(this.stateManager.getDefaultState().with(WATERLOGGED, false));
+	}
+	
+	protected void appendProperties(StateManager.@NotNull Builder<Block, BlockState> builder) {
+		builder.add(WATERLOGGED);
+	}
+	
+	public FluidState getFluidState(@NotNull BlockState state) {
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+	}
+	
+	public BlockState getStateForNeighborUpdate(@NotNull BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+		if (state.get(WATERLOGGED)) {
+			world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+		}
+		return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+	}
+	
+	public BlockState getPlacementState(@NotNull ItemPlacementContext ctx) {
+		FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+		return this.getDefaultState().with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
 	}
 	
 	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
@@ -40,18 +73,20 @@ public class IncandescentAmalgamBlock extends Block {
 	@Override
 	public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
 		super.randomDisplayTick(state, world, pos, random);
-		int r = random.nextInt(50);
-		if (r < 10) {
-			double posX = (double)pos.getX() + 0.25D + random.nextDouble() * 0.5D;
-			double posY = (double)pos.getY() + random.nextDouble() * 0.5D;
-			double posZ = (double)pos.getZ() + 0.25D + random.nextDouble() * 0.5D;
-			world.addParticle(ParticleTypes.LAVA, posX, posY, posZ, 0.0D, 0.0D, 0.0D);
-			if(r == 0) {
-				world.playSound(posX, posY, posZ, SoundEvents.BLOCK_LAVA_POP, SoundCategory.BLOCKS, 0.2F + random.nextFloat() * 0.2F, 0.9F + random.nextFloat() * 0.15F, false);
+		if (!state.get(WATERLOGGED)) {
+			int r = random.nextInt(50);
+			if (r < 10) {
+				double posX = (double) pos.getX() + 0.25D + random.nextDouble() * 0.5D;
+				double posY = (double) pos.getY() + random.nextDouble() * 0.5D;
+				double posZ = (double) pos.getZ() + 0.25D + random.nextDouble() * 0.5D;
+				world.addParticle(ParticleTypes.LAVA, posX, posY, posZ, 0.0D, 0.0D, 0.0D);
+				if (r == 0) {
+					world.playSound(posX, posY, posZ, SoundEvents.BLOCK_LAVA_POP, SoundCategory.BLOCKS, 0.2F + random.nextFloat() * 0.2F, 0.9F + random.nextFloat() * 0.15F, false);
+				}
 			}
-		}
-		if (random.nextInt(100) == 0) {
-			world.playSound(pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 0.2F + random.nextFloat() * 0.2F, 0.9F + random.nextFloat() * 0.15F, false);
+			if (random.nextInt(100) == 0) {
+				world.playSound(pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 0.2F + random.nextFloat() * 0.2F, 0.9F + random.nextFloat() * 0.15F, false);
+			}
 		}
 	}
 	
@@ -69,25 +104,31 @@ public class IncandescentAmalgamBlock extends Block {
 	@Override
 	public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
 		super.onSteppedOn(world, pos, state, entity);
-		explode(world, pos);
+		if (!state.get(WATERLOGGED)) {
+			explode(world, pos);
+		}
 	}
 	
 	@Override
 	public void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
 		super.onLandedUpon(world, state, pos, entity, fallDistance);
-		explode(world, pos);
+		if (!state.get(WATERLOGGED)) {
+			explode(world, pos);
+		}
 	}
 	
 	@Override
 	public void onProjectileHit(World world, BlockState state, BlockHitResult hit, ProjectileEntity projectile) {
 		super.onProjectileHit(world, state, hit, projectile);
-		explode(world, hit.getBlockPos());
+		if (!state.get(WATERLOGGED)) {
+			explode(world, hit.getBlockPos());
+		}
 	}
 	
 	@Override
 	public void neighborUpdate(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean notify) {
 		super.neighborUpdate(state, world, pos, block, fromPos, notify);
-		if(world.random.nextInt(10) == 0) {
+		if (!state.get(WATERLOGGED) && world.random.nextInt(10) == 0) {
 			explode(world, pos);
 		}
 	}
@@ -96,7 +137,7 @@ public class IncandescentAmalgamBlock extends Block {
 	// => creative players can easily break it without causing an explosion
 	@Override
 	public void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack) {
-		if(EnchantmentHelper.getLevel(SpectrumEnchantments.RESONANCE, stack) > 0) {
+		if (state.get(WATERLOGGED) || EnchantmentHelper.getLevel(SpectrumEnchantments.RESONANCE, stack) > 0) {
 			super.afterBreak(world, player, pos, state, blockEntity, stack);
 		} else {
 			explode(world, pos);
@@ -104,7 +145,7 @@ public class IncandescentAmalgamBlock extends Block {
 	}
 	
 	protected static void explode(World world, BlockPos pos) {
-		if(!world.isClient) {
+		if (!world.isClient) {
 			world.createExplosion(null, SpectrumDamageSources.INCANDESCENCE, new ExplosionBehavior(), pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 7.0F, true, Explosion.DestructionType.DESTROY);
 		}
 	}

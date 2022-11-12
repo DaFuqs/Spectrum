13a40f0a86ef21116d8405d9e8e8f1b5028c3a31
azzy <danielferrerreyes@protonmail.com>
2022-11-12 04:13:21 -0600

    sag my black materia

--- src/main/java/de/dafuqs/spectrum/registries/SpectrumBlockTags.java
+++ src/main/java/de/dafuqs/spectrum/registries/SpectrumBlockTags.java
@@ -10,6 +10,7 @@ public class SpectrumBlockTags {
 	// PLANTS
 	public static TagKey<Block> MERMAIDS_BRUSH_PLANTABLE = getReference("mermaids_brush_plantable");
 	public static TagKey<Block> QUITOXIC_REEDS_PLANTABLE = getReference("quitoxic_reeds_plantable");
+	public static TagKey<Block> BLACK_SLUDGE_BLOCKS = getReference("black_sludge_blocks");
 	
 	public static final TagKey<Block> BLACKSLAG_ORE_REPLACEABLES = getReference("blackslag_ore_replaceables");
 	

bb27dc101108c4f7c74e2f059b9500a807b5f877
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-11-26 11:14:51 +0100

    fixed shooting stars immediately despawning

--- src/main/java/de/dafuqs/spectrum/entity/entity/ShootingStarEntity.java
+++ src/main/java/de/dafuqs/spectrum/entity/entity/ShootingStarEntity.java
@@ -256,7 +256,7 @@ public class ShootingStarEntity extends Entity {
 		} else {
 			// despawning
 			this.age++;
-			if (this.age < 6000 && !playerPlaced && !hardened) {
+			if (this.age > 6000 && !playerPlaced && !hardened) {
 				this.discard();
 			}
 			

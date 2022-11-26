6bd10e238a19fcdd76aba39101e03887b7fdf2e5
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-11-26 11:31:09 +0100

    fixed star candy being free. Bumped chances

--- src/main/java/de/dafuqs/spectrum/recipe/pedestal/dynamic/StarCandyRecipe.java
+++ src/main/java/de/dafuqs/spectrum/recipe/pedestal/dynamic/StarCandyRecipe.java
@@ -1,11 +1,13 @@
 package de.dafuqs.spectrum.recipe.pedestal.dynamic;
 
 import de.dafuqs.spectrum.SpectrumCommon;
+import de.dafuqs.spectrum.blocks.pedestal.PedestalBlockEntity;
 import de.dafuqs.spectrum.enums.BuiltinGemstoneColor;
 import de.dafuqs.spectrum.enums.PedestalRecipeTier;
 import de.dafuqs.spectrum.recipe.pedestal.PedestalCraftingRecipe;
 import de.dafuqs.spectrum.registries.SpectrumItems;
 import net.id.incubus_core.recipe.IngredientStack;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -14,6 +16,7 @@ import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.SpecialRecipeSerializer;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
+import oshi.util.tuples.Triplet;
 
 import java.util.HashMap;
 import java.util.Random;
@@ -24,7 +27,7 @@ public class StarCandyRecipe extends PedestalCraftingRecipe {
 	public static final Random RANDOM = new Random();
 	
 	public static final Identifier UNLOCK_IDENTIFIER = SpectrumCommon.locate("progression/unlock_star_candy");
-	public static final float PURPLE_STAR_CANDY_CHANCE = 0.001F;
+	public static final float PURPLE_STAR_CANDY_CHANCE = 0.0025F;
 	public static final HashMap<BuiltinGemstoneColor, Integer> GEMSTONE_POWDER_INPUTS = new HashMap<>() {{
 		put(BuiltinGemstoneColor.YELLOW, 1);
 	}};
@@ -35,11 +38,27 @@ public class StarCandyRecipe extends PedestalCraftingRecipe {
 	
 	@Override
 	public ItemStack craft(Inventory inv) {
-		if(RANDOM.nextFloat() < PURPLE_STAR_CANDY_CHANCE) {
-			return SpectrumItems.PURPLE_STAR_CANDY.getDefaultStack();
-		} else {
-			return this.output.copy();
+		if (inv instanceof PedestalBlockEntity pedestal) {
+			Triplet<Integer, Integer, Boolean> orientation = getRecipeOrientation(inv);
+			if(orientation == null) {
+				return ItemStack.EMPTY;
+			}
+			decrementIngredientStacks(pedestal, orientation);
+			
+			ItemStack recipeOutput;
+			if(RANDOM.nextFloat() < PURPLE_STAR_CANDY_CHANCE) {
+				recipeOutput = SpectrumItems.PURPLE_STAR_CANDY.getDefaultStack();
+			} else {
+				recipeOutput = this.output.copy();
+			}
+			
+			PlayerEntity player = pedestal.getOwnerIfOnline();
+			if (player != null) {
+				recipeOutput.onCraft(pedestal.getWorld(), player, recipeOutput.getCount());
+			}
+			return recipeOutput;
 		}
+		return ItemStack.EMPTY;
 	}
 	
 	private static DefaultedList<IngredientStack> generateInputs() {

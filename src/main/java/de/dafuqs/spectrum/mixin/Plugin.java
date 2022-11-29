2d34c4131731b9df7613fe287d747b54730de58f
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-11-29 21:41:41 +0100

    init mixin extras earlier

--- src/main/java/de/dafuqs/spectrum/mixin/Plugin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/Plugin.java
@@ -1,6 +1,5 @@
 package de.dafuqs.spectrum.mixin;
 
-import com.llamalad7.mixinextras.MixinExtrasBootstrap;
 import net.fabricmc.loader.api.FabricLoader;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
@@ -13,7 +12,7 @@ public final class Plugin implements IMixinConfigPlugin {
 	
 	@Override
 	public void onLoad(String mixinPackage) {
-		MixinExtrasBootstrap.init();
+
 	}
 	
 	@Override

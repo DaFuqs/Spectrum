d05a657fc0bea645bf524f9e634f3cd0e4dcc803
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-12-10 19:09:46 +0100

    aea

--- src/main/java/de/dafuqs/spectrum/mixin/RandomChanceLootConditionMixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/RandomChanceLootConditionMixin.java
@@ -19,7 +19,7 @@ public abstract class RandomChanceLootConditionMixin {
 	float chance;
 	
 	@Inject(at = @At("RETURN"), method = "test(Lnet/minecraft/loot/context/LootContext;)Z", cancellable = true)
-	public void applyRareLootEnchantment(LootContext lootContext, CallbackInfoReturnable<Boolean> cir) {
+	public void spectrum$applyRareLootEnchantment(LootContext lootContext, CallbackInfoReturnable<Boolean> cir) {
 		// if the result was to not drop a drop before reroll
 		// gets more probable with each additional level of Clovers Favor
 		if (!cir.getReturnValue() && this.chance < 1.0F) {

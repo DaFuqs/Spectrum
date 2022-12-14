8f6308c7df81dd39676bd672651d7f97334629ee
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-12-14 09:37:23 +0100

    ore veins are back

--- src/main/java/de/dafuqs/spectrum/mixin/ChunkNoiseSamplerMixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/ChunkNoiseSamplerMixin.java
@@ -1,25 +1,29 @@
 package de.dafuqs.spectrum.mixin;
 
-import net.minecraft.world.gen.chunk.ChunkNoiseSampler;
+import com.google.common.collect.ImmutableList;
+import de.dafuqs.spectrum.deeper_down.DDOreVeinSampler;
+import net.minecraft.block.Blocks;
+import net.minecraft.world.gen.chunk.*;
 import net.minecraft.world.gen.densityfunction.DensityFunction;
+import net.minecraft.world.gen.densityfunction.DensityFunctionTypes;
+import net.minecraft.world.gen.noise.NoiseConfig;
+import net.minecraft.world.gen.noise.NoiseRouter;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ChunkNoiseSampler.class)
 public abstract class ChunkNoiseSamplerMixin {
 	
-	// TODO: readd
-	@Shadow
-	protected abstract DensityFunction getActualDensityFunction(DensityFunction densityFunction);
-	
-	/*@Inject(method = "<init>(ILnet/minecraft/world/gen/noise/NoiseConfig;IILnet/minecraft/world/gen/chunk/GenerationShapeConfig;Lnet/minecraft/world/gen/densityfunction/DensityFunctionTypes$Beardifying;Lnet/minecraft/world/gen/chunk/ChunkGeneratorSettings;Lnet/minecraft/world/gen/chunk/AquiferSampler$FluidLevelSampler;Lnet/minecraft/world/gen/chunk/Blender;)V",
+	@Inject(method = "<init>(ILnet/minecraft/world/gen/noise/NoiseConfig;IILnet/minecraft/world/gen/chunk/GenerationShapeConfig;Lnet/minecraft/world/gen/densityfunction/DensityFunctionTypes$Beardifying;Lnet/minecraft/world/gen/chunk/ChunkGeneratorSettings;Lnet/minecraft/world/gen/chunk/AquiferSampler$FluidLevelSampler;Lnet/minecraft/world/gen/chunk/Blender;)V",
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/chunk/ChunkGeneratorSettings;oreVeins()Z"),
 			locals = LocalCapture.CAPTURE_FAILHARD)
-	public void spectrum$init(int horizontalCellCount, NoiseConfig noiseConfig, int startX, int startZ, GenerationShapeConfig generationShapeConfig, DensityFunctionTypes.Beardifying beardifying, ChunkGeneratorSettings chunkGeneratorSettings, AquiferSampler.FluidLevelSampler fluidLevelSampler, Blender blender, CallbackInfo ci, ImmutableList.Builder<ChunkNoiseSampler.BlockStateSampler> builder) {
-		if (chunkGeneratorSettings.defaultBlock() == Blocks.DEEPSLATE.getDefaultState()) {
-			NoiseRouter noiseRouter = noiseConfig.getNoiseRouter();
+	public void spectrum$init(int horizontalCellCount, NoiseConfig noiseConfig, int startX, int startZ, GenerationShapeConfig generationShapeConfig, DensityFunctionTypes.Beardifying beardifying, ChunkGeneratorSettings chunkGeneratorSettings, AquiferSampler.FluidLevelSampler fluidLevelSampler, Blender blender, CallbackInfo ci, NoiseRouter noiseRouter, NoiseRouter noiseRouter2, ImmutableList.Builder builder, DensityFunction densityFunction) {
+		if (chunkGeneratorSettings.defaultBlock() == Blocks.DEEPSLATE.getDefaultState() && chunkGeneratorSettings.generationShapeConfig().minimumY() == -448) {
 			builder.add(DDOreVeinSampler.create(noiseRouter.veinToggle(), noiseRouter.veinRidged(), noiseRouter.veinGap(), noiseConfig.getOreRandomDeriver()));
 		}
-	}*/
+	}
 	
 }

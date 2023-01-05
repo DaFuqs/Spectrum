664511da637ad07272a3345cabe6c4b08e0c9eb2
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-01-05 21:49:13 +0100

    neat ring always has glint

--- src/main/java/de/dafuqs/spectrum/items/trinkets/NeatRingItem.java
+++ src/main/java/de/dafuqs/spectrum/items/trinkets/NeatRingItem.java
@@ -12,16 +12,21 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class NeatRingItem extends SpectrumTrinketItem {
-	
+
 	public NeatRingItem(Settings settings) {
 		super(settings, SpectrumCommon.locate("progression/unlock_neat_ring"));
 	}
-	
+
 	@Environment(EnvType.CLIENT)
 	@Override
 	public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
 		super.appendTooltip(stack, world, tooltip, context);
 		tooltip.add(Text.translatable("item.spectrum.neat_ring.tooltip"));
 	}
-	
+
+	@Override
+	public boolean hasGlint(ItemStack stack) {
+		return true;
+	}
+
 }

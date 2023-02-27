faa3bb51c672363802391e5d3f5ab8b2ad8a82f5
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-02-27 17:58:14 +0100

    buffed reprise teleportation distance

--- src/main/java/de/dafuqs/spectrum/items/food/beverages/RepriseItem.java
+++ src/main/java/de/dafuqs/spectrum/items/food/beverages/RepriseItem.java
@@ -1,21 +1,19 @@
 package de.dafuqs.spectrum.items.food.beverages;
 
-import de.dafuqs.spectrum.items.food.beverages.properties.BeverageProperties;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.passive.FoxEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.world.World;
+import de.dafuqs.spectrum.items.food.beverages.properties.*;
+import net.minecraft.entity.*;
+import net.minecraft.entity.passive.*;
+import net.minecraft.entity.player.*;
+import net.minecraft.item.*;
+import net.minecraft.nbt.*;
+import net.minecraft.server.world.*;
+import net.minecraft.sound.*;
+import net.minecraft.text.*;
+import net.minecraft.util.*;
+import net.minecraft.util.math.*;
+import net.minecraft.world.*;
 
-import java.util.List;
+import java.util.*;
 
 public class RepriseItem extends BeverageItem {
 	
@@ -35,7 +33,7 @@ public class RepriseItem extends BeverageItem {
 		
 		public void addTooltip(ItemStack itemStack, List<Text> tooltip) {
 			super.addTooltip(itemStack, tooltip);
-			int teleportRange = getTeleportRange(itemStack);
+			long teleportRange = getTeleportRange(itemStack);
 			tooltip.add(Text.translatable("item.spectrum.reprise.tooltip.teleport", teleportRange).formatted(Formatting.GRAY));
 		}
 		
@@ -46,27 +44,27 @@ public class RepriseItem extends BeverageItem {
 		randomTeleport(world, user, getTeleportRange(stack));
 		return super.finishUsing(stack, world, user);
 	}
-	
-	public static int getTeleportRange(ItemStack itemStack) {
+
+	public static long getTeleportRange(ItemStack itemStack) {
 		BeverageProperties properties = BeverageProperties.getFromStack(itemStack);
-		return (int) Math.ceil(Math.pow(1.4, properties.alcPercent));
+		return (long) Math.ceil(Math.pow(2, properties.alcPercent));
 	}
-	
-	public void randomTeleport(World world, LivingEntity user, int maxRange) {
+
+	public void randomTeleport(World world, LivingEntity user, long maxRange) {
 		if (!world.isClient) {
 			double d = user.getX();
 			double e = user.getY();
 			double f = user.getZ();
-			
+
 			for (int i = 0; i < 16; ++i) {
-				double g = user.getX() + (user.getRandom().nextDouble() - 0.5D) * maxRange;
-				double h = MathHelper.clamp(user.getY() + (double) (user.getRandom().nextInt(maxRange) - maxRange / 2), world.getBottomY(), (world.getBottomY() + ((ServerWorld) world).getLogicalHeight() - 1));
-				double j = user.getZ() + (user.getRandom().nextDouble() - 0.5D) * maxRange;
+				double newX = user.getX() + (user.getRandom().nextDouble() - 0.5D) * maxRange;
+				double newY = MathHelper.clamp(user.getY() + (double) (user.getRandom().nextFloat() * 2 * maxRange) - maxRange, world.getBottomY(), (world.getBottomY() + ((ServerWorld) world).getLogicalHeight() - 1));
+				double newZ = user.getZ() + (user.getRandom().nextDouble() - 0.5D) * maxRange;
 				if (user.hasVehicle()) {
 					user.stopRiding();
 				}
-				
-				if (user.teleport(g, h, j, true)) {
+
+				if (user.teleport(newX, newY, newZ, true)) {
 					SoundEvent soundEvent = user instanceof FoxEntity ? SoundEvents.ENTITY_FOX_TELEPORT : SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT;
 					world.playSound(null, d, e, f, soundEvent, SoundCategory.PLAYERS, 1.0F, 1.0F);
 					user.playSound(soundEvent, 1.0F, 1.0F);

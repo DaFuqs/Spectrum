42b2fd5e435fcc1d8c48531edc8f476b856b1c88
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-04-01 16:26:04 +0200

    prevent biome ground taking up the whole DD with time

--- src/main/java/de/dafuqs/spectrum/registries/SpectrumBlocks.java
+++ src/main/java/de/dafuqs/spectrum/registries/SpectrumBlocks.java
@@ -516,9 +516,9 @@ public class SpectrumBlocks {
 	public static final ToIntFunction<BlockState> LIT_PROVIDER = (state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0);
 	
 	//DD FLORA
-	public static final Block SAWBLADE_GRASS = new SpectrumSpreadableBlock(AbstractBlock.Settings.copy(Blocks.PODZOL).sounds(BlockSoundGroup.AZALEA_LEAVES).ticksRandomly(), BLACKSLAG, BLACKSLAG.getDefaultState());
-	public static final Block SHIMMEL = new SpectrumSpreadableBlock(AbstractBlock.Settings.copy(Blocks.MYCELIUM).sounds(BlockSoundGroup.WART_BLOCK).ticksRandomly(), BLACKSLAG, BLACKSLAG.getDefaultState());
-	public static final Block OVERGROWN_BLACKSLAG = new SpectrumSpreadableBlock(AbstractBlock.Settings.copy(Blocks.PODZOL).sounds(BlockSoundGroup.VINE).velocityMultiplier(0.925F).ticksRandomly(), BLACKSLAG, BLACKSLAG.getDefaultState());
+	public static final Block SAWBLADE_GRASS = new SpectrumSpreadableBlock(AbstractBlock.Settings.copy(Blocks.PODZOL).sounds(BlockSoundGroup.AZALEA_LEAVES).ticksRandomly(), null, BLACKSLAG.getDefaultState());
+	public static final Block SHIMMEL = new SpectrumSpreadableBlock(AbstractBlock.Settings.copy(Blocks.MYCELIUM).sounds(BlockSoundGroup.WART_BLOCK).ticksRandomly(), null, BLACKSLAG.getDefaultState());
+	public static final Block OVERGROWN_BLACKSLAG = new SpectrumSpreadableBlock(AbstractBlock.Settings.copy(Blocks.PODZOL).sounds(BlockSoundGroup.VINE).velocityMultiplier(0.925F).ticksRandomly(), null, BLACKSLAG.getDefaultState());
 	public static final Block ROTTEN_GROUND = new RottenGroundBlock(AbstractBlock.Settings.copy(Blocks.MUD).sounds(BlockSoundGroup.HONEY).velocityMultiplier(0.775F).jumpVelocityMultiplier(0.9F));
 	
 	public static final Block SLATE_NOXSHROOM = new GilledFungusBlock(fungus(MapColor.GRAY), () -> getConfiguredFeatureRegistryEntry("slate_noxfungus"));

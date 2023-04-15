c3fb599f6bd8dd9fa5e832ffc6199c351b096c7c
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-04-15 06:57:32 +0200

    dike gates emit particles

--- src/main/java/de/dafuqs/spectrum/blocks/structure/DikeGateBlock.java
+++ src/main/java/de/dafuqs/spectrum/blocks/structure/DikeGateBlock.java
@@ -1,28 +1,23 @@
 package de.dafuqs.spectrum.blocks.structure;
 
-import de.dafuqs.spectrum.cca.azure_dike.AzureDikeProvider;
-import de.dafuqs.spectrum.networking.SpectrumS2CPacketSender;
-import de.dafuqs.spectrum.particle.SpectrumParticleTypes;
-import de.dafuqs.spectrum.registries.SpectrumDamageSources;
-import de.dafuqs.spectrum.registries.SpectrumSoundEvents;
-import net.minecraft.block.AbstractGlassBlock;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.EntityShapeContext;
-import net.minecraft.block.ShapeContext;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
-import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
+import de.dafuqs.spectrum.cca.azure_dike.*;
+import de.dafuqs.spectrum.networking.*;
+import de.dafuqs.spectrum.particle.*;
+import de.dafuqs.spectrum.registries.*;
+import net.minecraft.block.*;
+import net.minecraft.entity.*;
+import net.minecraft.entity.player.*;
+import net.minecraft.server.network.*;
+import net.minecraft.server.world.*;
+import net.minecraft.sound.*;
+import net.minecraft.util.*;
+import net.minecraft.util.hit.*;
+import net.minecraft.util.math.*;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.shape.*;
+import net.minecraft.world.*;
+
+import java.util.*;
 
 public class DikeGateBlock extends AbstractGlassBlock {
 	
@@ -44,6 +39,7 @@ public class DikeGateBlock extends AbstractGlassBlock {
 		return VoxelShapes.fullCube();
 	}
 	
+	@Override
 	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
 		if (context.isHolding(this.asItem())) {
 			return VoxelShapes.fullCube();
@@ -70,12 +66,28 @@ public class DikeGateBlock extends AbstractGlassBlock {
 		super.onEntityCollision(state, world, pos, entity);
 	}
 	
+	@Override
+	public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+		Iterator<Direction> directions = Util.copyShuffled(Direction.values(), random).iterator();
+		for (int i = 0; i < 2; i++) {
+			Direction direction = directions.next();
+			BlockPos blockPos = pos.offset(direction);
+			BlockState blockState = world.getBlockState(blockPos);
+			if (!state.isOpaque() || !blockState.isSideSolidFullSquare(world, blockPos, direction.getOpposite())) {
+				double d = direction.getOffsetX() == 0 ? random.nextDouble() : 0.5 + direction.getOffsetX() * 0.6;
+				double e = direction.getOffsetY() == 0 ? random.nextDouble() : 0.5 + direction.getOffsetY() * 0.6;
+				double f = direction.getOffsetZ() == 0 ? random.nextDouble() : 0.5 + direction.getOffsetZ() * 0.6;
+				world.addParticle(SpectrumParticleTypes.AZURE_DIKE, pos.getX() + d, pos.getY() + e, pos.getZ() + f, 0.0, 0.025, 0.0);
+			}
+		}
+	}
+	
 	public void punishEntityWithoutAzureDike(BlockView world, BlockPos pos, Entity entity, boolean decreasedSounds) {
 		if (world instanceof ServerWorld serverWorld && entity instanceof LivingEntity livingEntity) {
 			int charges = AzureDikeProvider.getAzureDikeCharges(livingEntity);
 			if (charges == 0) {
 				entity.damage(SpectrumDamageSources.DIKE_GATE, 1);
-				SpectrumS2CPacketSender.playParticles(serverWorld, pos, SpectrumParticleTypes.BLUE_CRAFTING, 10);
+				SpectrumS2CPacketSender.playParticles(serverWorld, pos, SpectrumParticleTypes.AZURE_DIKE, 10);
 				if (entity instanceof ServerPlayerEntity serverPlayerEntity && (!decreasedSounds || ((ServerWorld) world).getTime() % 10 == 0)) {
 					serverPlayerEntity.playSound(SpectrumSoundEvents.USE_FAIL, SoundCategory.PLAYERS, 0.75F, 1.0F);
 				}

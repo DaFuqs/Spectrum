0930057e5dae23e5015401ba4bcb6b48d98aabbb
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-05-02 19:33:15 +0200

    common protection api

---
+++ src/main/java/de/dafuqs/spectrum/compat/claims/GenericClaimModsCompat.java
@@ -0,0 +1,31 @@
+package de.dafuqs.spectrum.compat.claims;
+
+import net.fabricmc.loader.api.*;
+import net.minecraft.entity.player.*;
+import net.minecraft.util.math.*;
+import net.minecraft.world.*;
+import org.jetbrains.annotations.*;
+
+public class GenericClaimModsCompat {
+	
+	public static boolean IS_COMMON_PROTECTION_API_PRESENT = FabricLoader.getInstance().isModLoaded("common-protection-api");
+	
+	/**
+	 * Call this for all kinds of world modifications
+	 * For each supported protection mod add a single check here
+	 * instead of spreading individual protection mods over the whole codebase
+	 * <p>
+	 * This also means we do not need any kind of hard compat
+	 *
+	 * @param world the world that should get modified
+	 * @param pos   the pos that should get modified
+	 * @return if modification is allowed
+	 */
+	public static boolean isProtected(World world, BlockPos pos, @Nullable PlayerEntity player) {
+		if (IS_COMMON_PROTECTION_API_PRESENT) {
+			return !CommonProtectionApiCompat.isProtected(world, pos, player);
+		}
+		return true;
+	}
+	
+}

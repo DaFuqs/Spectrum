06b445ac4b56c8dc01019d73df691c040fa26b38
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-05-13 00:07:01 +0200

    everpromise ribbon particles

--- src/main/java/de/dafuqs/spectrum/items/magic_items/EverpromiseRibbonItem.java
+++ src/main/java/de/dafuqs/spectrum/items/magic_items/EverpromiseRibbonItem.java
@@ -7,8 +7,10 @@ import net.minecraft.entity.*;
 import net.minecraft.entity.mob.*;
 import net.minecraft.entity.player.*;
 import net.minecraft.item.*;
+import net.minecraft.particle.*;
 import net.minecraft.text.*;
 import net.minecraft.util.*;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.*;
 
 import java.util.*;
@@ -21,17 +23,26 @@ public class EverpromiseRibbonItem extends Item {
 	
 	public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
 		if (stack.hasCustomName() && !(entity instanceof PlayerEntity)) {
-			if (!user.world.isClient && entity.isAlive()) {
-				BondingRibbonComponent.attachBondingRibbon(entity);
-				
-				Text newName = stack.getName();
-				if (newName instanceof MutableText mutableText) {
-					newName = Text.literal(mutableText.getString() + " ❣").setStyle(mutableText.getStyle());
-				}
-				entity.setCustomName(newName);
-				if (entity instanceof MobEntity mobEntity) {
-					mobEntity.setPersistent();
-					EntityHelper.addPlayerTrust(mobEntity, user);
+			if (entity.isAlive()) {
+				if (user.world.isClient) {
+					World world = entity.getWorld();
+					Random random = world.random;
+					for (int i = 0; i < 7; ++i) {
+						world.addParticle(ParticleTypes.HEART, entity.getParticleX(1.0), entity.getRandomBodyY() + 0.5, entity.getParticleZ(1.0),
+								random.nextGaussian() * 0.02, random.nextGaussian() * 0.02, random.nextGaussian() * 0.02);
+					}
+				} else {
+					BondingRibbonComponent.attachBondingRibbon(entity);
+					
+					Text newName = stack.getName();
+					if (newName instanceof MutableText mutableText) {
+						newName = Text.literal(mutableText.getString() + " ❣").setStyle(mutableText.getStyle());
+					}
+					entity.setCustomName(newName);
+					if (entity instanceof MobEntity mobEntity) {
+						mobEntity.setPersistent();
+						EntityHelper.addPlayerTrust(mobEntity, user);
+					}
 				}
 				
 				stack.decrement(1);

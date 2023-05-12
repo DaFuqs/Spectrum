5537ea07f3b4a7e877e5836ffc55e629a93b54d3
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-05-12 14:17:28 +0200

    BlockTargetJigsawStructures

---
+++ src/main/java/de/dafuqs/spectrum/registries/SpectrumStructureTypes.java
@@ -0,0 +1,21 @@
+package de.dafuqs.spectrum.registries;
+
+import com.mojang.serialization.*;
+import de.dafuqs.spectrum.*;
+import de.dafuqs.spectrum.structures.*;
+import net.minecraft.util.registry.*;
+import net.minecraft.world.gen.structure.*;
+
+public class SpectrumStructureTypes {
+	
+	public static StructureType<BlockTargetJigsawStructure> BLOCK_PREDICATE_JIGSAW;
+	
+	public static void register() {
+		BLOCK_PREDICATE_JIGSAW = register("block_target_jigsaw", BlockTargetJigsawStructure.CODEC);
+	}
+	
+	private static <S extends Structure> StructureType<S> register(String id, Codec<S> codec) {
+		return Registry.register(Registry.STRUCTURE_TYPE, SpectrumCommon.locate(id), () -> codec);
+	}
+	
+}

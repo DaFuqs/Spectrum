339785859ce7d96b3ad50c8106fb99594f95d244
Noaaan <noaaan@hotmail.com>
2023-06-24 17:20:05 +0200

    Allow Loot Tables to be used with Item Bowls and Item Roundel
    Technically also applies to any of the crafting stations, as they also extend the InWorldInteractionBlockEntity, which this has been implemented upon.
    
    Did you know that Hoppers can generate with Loot Tables? Crazy stuff.
    
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/de/dafuqs/spectrum/blocks/InWorldInteractionBlockEntity.java
+++ src/main/java/de/dafuqs/spectrum/blocks/InWorldInteractionBlockEntity.java
@@ -1,14 +1,20 @@
 package de.dafuqs.spectrum.blocks;
 
+import net.minecraft.advancement.criterion.*;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.*;
+import net.minecraft.entity.player.*;
 import net.minecraft.inventory.*;
 import net.minecraft.item.*;
+import net.minecraft.loot.*;
+import net.minecraft.loot.context.*;
 import net.minecraft.nbt.*;
 import net.minecraft.network.listener.*;
 import net.minecraft.network.packet.*;
 import net.minecraft.network.packet.s2c.play.*;
+import net.minecraft.server.network.*;
 import net.minecraft.server.world.*;
+import net.minecraft.util.*;
 import net.minecraft.util.collection.*;
 import net.minecraft.util.math.*;
 import org.jetbrains.annotations.*;
@@ -16,7 +22,10 @@ import org.jetbrains.annotations.*;
 public abstract class InWorldInteractionBlockEntity extends BlockEntity implements ImplementedInventory {
 	
 	private final int inventorySize;
-	private DefaultedList<ItemStack> items;
+	protected DefaultedList<ItemStack> items;
+	@Nullable
+	protected Identifier lootTableId;
+	protected long lootTableSeed;
 	
 	public InWorldInteractionBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state, int inventorySize) {
 		super(type, pos, state);
@@ -49,6 +58,50 @@ public abstract class InWorldInteractionBlockEntity extends BlockEntity implemen
 		super.writeNbt(nbt);
 		Inventories.writeNbt(nbt, items);
 	}
+	protected boolean deserializeLootTable(NbtCompound nbt) {
+		if (nbt.contains("LootTable", NbtElement.STRING_TYPE)) {
+			this.lootTableId = new Identifier(nbt.getString("LootTable"));
+			this.lootTableSeed = nbt.getLong("LootTableSeed");
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	protected boolean serializeLootTable(NbtCompound nbt) {
+		if (this.lootTableId == null) {
+			return false;
+		} else {
+			nbt.putString("LootTable", this.lootTableId.toString());
+			if (this.lootTableSeed != 0L) {
+				nbt.putLong("LootTableSeed", this.lootTableSeed);
+			}
+
+			return true;
+		}
+	}
+
+	public void checkLootInteraction(@Nullable PlayerEntity player) {
+		var world = this.getWorld();
+		if (world != null && this.lootTableId != null && world.getServer() != null) {
+			LootTable lootTable = world.getServer().getLootManager().getTable(this.lootTableId);
+			if (player instanceof ServerPlayerEntity) {
+				Criteria.PLAYER_GENERATES_CONTAINER_LOOT.trigger((ServerPlayerEntity)player, this.lootTableId);
+			}
+
+			this.lootTableId = null;
+			LootContext.Builder builder = new LootContext.Builder((ServerWorld) world)
+				.parameter(LootContextParameters.ORIGIN, Vec3d.ofCenter(this.pos))
+				.random(this.lootTableSeed);
+			if (player != null) {
+				builder.luck(player.getLuck()).parameter(LootContextParameters.THIS_ENTITY, player);
+			}
+
+			lootTable.supplyInventory(this, builder.build(LootContextTypes.CHEST));
+		}
+
+	}
+
 	
 	@Nullable
 	@Override

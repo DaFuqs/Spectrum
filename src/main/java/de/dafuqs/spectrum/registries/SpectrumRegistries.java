a0fb81fd54ae992c655355f76c90fc632706a29a
Noaaan <noaaan@hotmail.com>
2023-06-24 17:18:06 +0200

    Make Lizard Variant registries use non-deprecated API
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/de/dafuqs/spectrum/registries/SpectrumRegistries.java
+++ src/main/java/de/dafuqs/spectrum/registries/SpectrumRegistries.java
@@ -8,12 +8,21 @@ import net.minecraft.registry.*;
 
 public class SpectrumRegistries {
 	public static final RegistryKey<Registry<GlassArrowVariant>> GLASS_ARROW_VARIANT_KEY = RegistryKey.ofRegistry(SpectrumCommon.locate("glass_arrow_variant"));
+	public static final RegistryKey<Registry<LizardScaleVariant>>LIZARD_SCALE_VARIANT_KEY = RegistryKey.ofRegistry(SpectrumCommon.locate("lizard_scale_variant"));
+	public static final RegistryKey<Registry<LizardFrillVariant>>LIZARD_FRILL_VARIANT_KEY = RegistryKey.ofRegistry(SpectrumCommon.locate("lizard_frill_variant"));
+	public static final RegistryKey<Registry<LizardHornVariant>>LIZARD_HORN_VARIANT_KEY = RegistryKey.ofRegistry(SpectrumCommon.locate("lizard_horn_variant"));
 	public static final Registry<GlassArrowVariant> GLASS_ARROW_VARIANT = FabricRegistryBuilder.createSimple(GLASS_ARROW_VARIANT_KEY)
 			.attribute(RegistryAttribute.SYNCED)
 			.buildAndRegister();
-	public static final Registry<LizardScaleVariant> LIZARD_SCALE_VARIANT = FabricRegistryBuilder.createSimple(LizardScaleVariant.class, SpectrumCommon.locate("lizard_scale_variant")).attribute(RegistryAttribute.SYNCED).buildAndRegister();
-	public static final Registry<LizardFrillVariant> LIZARD_FRILL_VARIANT = FabricRegistryBuilder.createSimple(LizardFrillVariant.class, SpectrumCommon.locate("lizard_frill_variant")).attribute(RegistryAttribute.SYNCED).buildAndRegister();
-	public static final Registry<LizardHornVariant> LIZARD_HORN_VARIANT = FabricRegistryBuilder.createSimple(LizardHornVariant.class, SpectrumCommon.locate("lizard_horn_variant")).attribute(RegistryAttribute.SYNCED).buildAndRegister();
+	public static final Registry<LizardScaleVariant> LIZARD_SCALE_VARIANT = FabricRegistryBuilder.createSimple(LIZARD_SCALE_VARIANT_KEY)
+		.attribute(RegistryAttribute.SYNCED)
+		.buildAndRegister();
+	public static final Registry<LizardFrillVariant> LIZARD_FRILL_VARIANT = FabricRegistryBuilder.createSimple(LIZARD_FRILL_VARIANT_KEY)
+		.attribute(RegistryAttribute.SYNCED)
+		.buildAndRegister();
+	public static final Registry<LizardHornVariant> LIZARD_HORN_VARIANT = FabricRegistryBuilder.createSimple(LIZARD_HORN_VARIANT_KEY)
+		.attribute(RegistryAttribute.SYNCED)
+		.buildAndRegister();
 
 	public static void register() {
 		GlassArrowVariant.init();
@@ -21,5 +30,5 @@ public class SpectrumRegistries {
 		LizardFrillVariant.init();
 		LizardHornVariant.init();
 	}
-	
+
 }

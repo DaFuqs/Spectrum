6717104e5367693b82bd3d02ad7c95af3342b6ae
Noaaan <noaaan@hotmail.com>
2023-06-24 17:20:05 +0200

    Allow Loot Tables to be used with Item Bowls and Item Roundel
    Technically also applies to any of the crafting stations, as they also extend the InWorldInteractionBlockEntity, which this has been implemented upon.
    
    Did you know that Hoppers can generate with Loot Tables? Crazy stuff.
    
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/de/dafuqs/spectrum/blocks/item_roundel/ItemRoundelBlockEntity.java
+++ src/main/java/de/dafuqs/spectrum/blocks/item_roundel/ItemRoundelBlockEntity.java
@@ -4,6 +4,8 @@ import de.dafuqs.spectrum.blocks.*;
 import de.dafuqs.spectrum.registries.*;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.*;
+import net.minecraft.inventory.*;
+import net.minecraft.nbt.*;
 import net.minecraft.util.math.*;
 
 public class ItemRoundelBlockEntity extends InWorldInteractionBlockEntity {
@@ -21,5 +23,26 @@ public class ItemRoundelBlockEntity extends InWorldInteractionBlockEntity {
 	public boolean renderStacksAsIndividualItems() {
 		return false;
 	}
-	
+
+	@Override
+	public void readNbt(NbtCompound nbt) {
+		super.readNbt(nbt);
+		if (!this.deserializeLootTable(nbt)) {
+			Inventories.readNbt(nbt, this.items);
+		}
+	}
+
+	@Override
+	public void writeNbt(NbtCompound nbt) {
+		super.writeNbt(nbt);
+		if (!this.serializeLootTable(nbt)) {
+			Inventories.writeNbt(nbt, this.items);
+		}
+	}
+
+	@Override
+	public NbtCompound toInitialChunkDataNbt() {
+		this.checkLootInteraction(null);
+		return super.toInitialChunkDataNbt();
+	}
 }

d193240ea558db1ff2c9b3c32b8770ef00105a0f
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-07-05 20:00:33 +0200

    sanity for recipes in guidebook. Simplified biome makeover path

--- raw/Test-PatchouliUnlocks.ps1
+++ raw/Test-PatchouliUnlocks.ps1
@@ -1,6 +1,8 @@
 ﻿$dataFolder = "$PSScriptRoot\..\src\main\resources\data"
 $advancementsFolder = $(Join-Path -Path $dataFolder -ChildPath "spectrum\advancements" -Resolve)
-$advancements = ((Get-ChildItem -Path $advancementsFolder -File -Recurse).FullName -replace ($advancementsFolder -replace "\\", "\\"), "" -replace ".json", "" -replace "\\", "/").Substring(1).Trim()
+$existingAdvancements = ((Get-ChildItem -Path $advancementsFolder -File -Recurse).FullName -replace ($advancementsFolder -replace "\\", "\\"), "" -replace ".json", "" -replace "\\", "/").Substring(1).Trim()
+$recipesFolder = $(Join-Path -Path $dataFolder -ChildPath "spectrum\recipes" -Resolve)
+$existingRecipes = ((Get-ChildItem -Path $recipesFolder -File -Recurse).FullName -replace ($recipesFolder -replace "\\", "\\"), "" -replace ".json", "" -replace "\\", "/").Substring(1).Trim()
 
 $guidebookFolder = $(Join-Path -Path $dataFolder -ChildPath "spectrum\patchouli_books\guidebook\en_us\entries" -Resolve)
 $bookPages = (Get-ChildItem -Path $guidebookFolder -File -Recurse).FullName
@@ -15,9 +17,24 @@ $bookPages | ForEach-Object {
     foreach($ad in $adv) {
         if($ad) {
             $a, $b = $ad.Trim().Split(":")
-            if($advancements -notcontains $b) {
-                Write-Warning "$_ : $b"
+            if($existingAdvancements -notcontains $b) {
+                Write-Warning "Missing Unlock: $_ : $b"
             }
         }
     }
+    
+    
+    $recipes = @()
+    $recipes += $json.pages.recipe
+    $recipes += $json.pages.recipe2
+    foreach($recipe in $recipes) {
+        if($recipe) {
+            $a, $b = $recipe.Trim().Split(":")
+            if($existingRecipes -notcontains $b -and $b -notmatch "_level_2") {
+                Write-Warning "Missing Recipe: $_ : $b"
+            }
+        }
+    }
+
+
 }

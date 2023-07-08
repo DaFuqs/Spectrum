110b51d153599e6b0c41bf46554c60ba33bf2ca2
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-07-08 09:36:40 +0200

    allow placing down signs with text nbt in survival

--- src/main/java/de/dafuqs/spectrum/mixin/AllowCopyBlockEntityDataMixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/AllowCopyBlockEntityDataMixin.java
@@ -5,8 +5,8 @@ import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.*;
 
-@Mixin(MobSpawnerBlockEntity.class)
-public abstract class MobSpawnerBlockEntityMixin {
+@Mixin({MobSpawnerBlockEntity.class, SignBlockEntity.class})
+public abstract class AllowCopyBlockEntityDataMixin {
 	
 	@Inject(method = "copyItemDataRequiresOperator", at = @At("HEAD"), cancellable = true)
 	public void allowPlacingSpawnerWithBlockData(CallbackInfoReturnable<Boolean> callbackInfoReturnable) {

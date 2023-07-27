3cb1cb778d92ff0c821101cf785e2b186a0c401f
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-07-27 21:47:50 +0200

    guardian turret model & animation

--- src/main/java/de/dafuqs/spectrum/entity/models/GuardianTurretEntityModel.java
+++ src/main/java/de/dafuqs/spectrum/entity/models/GuardianTurretEntityModel.java
@@ -26,11 +26,12 @@ public class GuardianTurretEntityModel<T extends GuardianTurretEntity> extends C
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
 		
-		ModelPartData torso = modelPartData.addChild("body", ModelPartBuilder.create().uv(0, 0).cuboid(-8.0F, -8.0F, -8.0F, 16.0F, 8.0F, 16.0F, new Dilation(0.0F))
-				.uv(0, 24).cuboid(-8.0F, -16.0F, -8.0F, 16.0F, 2.0F, 16.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+		ModelPartData body = modelPartData.addChild("body", ModelPartBuilder.create()
+				.uv(0, 0).cuboid(-8.0F, -8.0F, -8.0F, 16.0F, 8.0F, 16.0F, Dilation.NONE)
+				.uv(0, 24).cuboid(-8.0F, -16.0F, -8.0F, 16.0F, 2.0F, 16.0F, Dilation.NONE), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
 		
-		ModelPartData cover = torso.addChild("cover", ModelPartBuilder.create().uv(0, 42).cuboid(-7.0F, -14.0F, -7.0F, 14.0F, 6.0F, 14.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
-		ModelPartData head = torso.addChild("head", ModelPartBuilder.create().uv(48, 0).cuboid(-5.0F, -14.0F, -5.0F, 10.0F, 6.0F, 9.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+		body.addChild("cover", ModelPartBuilder.create().uv(0, 42).cuboid(-7.0F, -14.0F, -7.0F, 14.0F, 6.0F, 14.0F, Dilation.NONE), ModelTransform.NONE);
+		body.addChild("head", ModelPartBuilder.create().uv(48, 0).cuboid(-5.0F, -14.0F, -5.0F, 10.0F, 6.0F, 10.0F, Dilation.NONE), ModelTransform.NONE);
 		
 		return TexturedModelData.of(modelData, 128, 128);
 	}
@@ -39,11 +40,11 @@ public class GuardianTurretEntityModel<T extends GuardianTurretEntity> extends C
 	public void setAngles(T turretEntity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
 		float k = animationProgress - (float) turretEntity.age;
 		float l = (float) ((0.5F + turretEntity.getOpenProgress(k)) * Math.PI);
-		float lidOffset = 0.0F;
+		float coverOffset = 0.0F;
 		if (l > Math.PI) {
-			lidOffset = MathHelper.sin(animationProgress * 0.1F) * 0.7F;
+			coverOffset = MathHelper.sin(animationProgress * 0.1F) * 0.7F;
 		}
-		this.cover.setPivot(0.0F, 16.0F + MathHelper.sin(l) * 8.0F - lidOffset, 0.0F);
+		this.cover.setPivot(0.0F, 8.0F - MathHelper.sin(l) * 8.0F - coverOffset, 0.0F);
 		
 		this.head.pitch = headPitch * 0.017453292F;
 		if (turretEntity.getOpenProgress(k) > 0.3F) {

313435dae0380cb9b68f686424ebb802d72ea2d7
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-07-28 14:31:37 +0200

    Glass Arrows can now be shot by dispensers (fraZ0R), dispensers can spawn Spectrum's spawn eggs

--- src/main/java/de/dafuqs/spectrum/registries/SpectrumDispenserBehaviors.java
+++ src/main/java/de/dafuqs/spectrum/registries/SpectrumDispenserBehaviors.java
@@ -2,33 +2,60 @@ package de.dafuqs.spectrum.registries;
 
 import de.dafuqs.spectrum.blocks.bottomless_bundle.*;
 import de.dafuqs.spectrum.blocks.shooting_star.*;
+import de.dafuqs.spectrum.entity.entity.*;
+import de.dafuqs.spectrum.items.tools.*;
 import net.minecraft.block.*;
 import net.minecraft.block.dispenser.*;
+import net.minecraft.entity.projectile.*;
 import net.minecraft.item.*;
+import net.minecraft.util.math.*;
+import net.minecraft.world.*;
 
 public class SpectrumDispenserBehaviors {
-    
-    public static void register() {
-        DispenserBehavior fluidBucketBehavior = SpectrumBlocks.ENDER_DROPPER.getDefaultBehaviorForItem(Items.WATER_BUCKET.getDefaultStack());
-        DispenserBehavior armorEquipBehavior = SpectrumBlocks.ENDER_DROPPER.getDefaultBehaviorForItem(Items.PLAYER_HEAD.getDefaultStack());
-        
-        DispenserBlock.registerBehavior(SpectrumItems.BOTTOMLESS_BUNDLE, new BottomlessBundleItem.BottomlessBundlePlacementDispenserBehavior());
-        DispenserBlock.registerBehavior(SpectrumBlocks.COLORFUL_SHOOTING_STAR.asItem(), new ShootingStarBlock.ShootingStarBlockDispenserBehavior());
-        DispenserBlock.registerBehavior(SpectrumBlocks.FIERY_SHOOTING_STAR.asItem(), new ShootingStarBlock.ShootingStarBlockDispenserBehavior());
-        DispenserBlock.registerBehavior(SpectrumBlocks.GEMSTONE_SHOOTING_STAR.asItem(), new ShootingStarBlock.ShootingStarBlockDispenserBehavior());
-        DispenserBlock.registerBehavior(SpectrumBlocks.GLISTERING_SHOOTING_STAR.asItem(), new ShootingStarBlock.ShootingStarBlockDispenserBehavior());
-        DispenserBlock.registerBehavior(SpectrumBlocks.PRISTINE_SHOOTING_STAR.asItem(), new ShootingStarBlock.ShootingStarBlockDispenserBehavior());
-        DispenserBlock.registerBehavior(SpectrumItems.BEDROCK_SHEARS.asItem(), new ShearsDispenserBehavior());
-        
-        DispenserBlock.registerBehavior(SpectrumItems.MUD_BUCKET, fluidBucketBehavior);
-        DispenserBlock.registerBehavior(SpectrumItems.LIQUID_CRYSTAL_BUCKET, fluidBucketBehavior);
-        DispenserBlock.registerBehavior(SpectrumItems.MIDNIGHT_SOLUTION_BUCKET, fluidBucketBehavior);
-        
-        for (Block skullBlock : SpectrumBlocks.MOB_HEADS.values()) {
-            DispenserBlock.registerBehavior(skullBlock, armorEquipBehavior);
-        }
-        
-    }
-
-
+	
+	public static void register() {
+		DispenserBlock.registerBehavior(SpectrumItems.BOTTOMLESS_BUNDLE, new BottomlessBundleItem.BottomlessBundlePlacementDispenserBehavior());
+		DispenserBlock.registerBehavior(SpectrumItems.BEDROCK_SHEARS, new ShearsDispenserBehavior());
+		
+		// Shooting Stars
+		DispenserBlock.registerBehavior(SpectrumBlocks.COLORFUL_SHOOTING_STAR, new ShootingStarBlock.ShootingStarBlockDispenserBehavior());
+		DispenserBlock.registerBehavior(SpectrumBlocks.FIERY_SHOOTING_STAR, new ShootingStarBlock.ShootingStarBlockDispenserBehavior());
+		DispenserBlock.registerBehavior(SpectrumBlocks.GEMSTONE_SHOOTING_STAR, new ShootingStarBlock.ShootingStarBlockDispenserBehavior());
+		DispenserBlock.registerBehavior(SpectrumBlocks.GLISTERING_SHOOTING_STAR, new ShootingStarBlock.ShootingStarBlockDispenserBehavior());
+		DispenserBlock.registerBehavior(SpectrumBlocks.PRISTINE_SHOOTING_STAR, new ShootingStarBlock.ShootingStarBlockDispenserBehavior());
+		
+		// Fluid Buckets
+		DispenserBehavior fluidBucketBehavior = SpectrumBlocks.ENDER_DROPPER.getDefaultBehaviorForItem(Items.WATER_BUCKET.getDefaultStack());
+		DispenserBlock.registerBehavior(SpectrumItems.MUD_BUCKET, fluidBucketBehavior);
+		DispenserBlock.registerBehavior(SpectrumItems.LIQUID_CRYSTAL_BUCKET, fluidBucketBehavior);
+		DispenserBlock.registerBehavior(SpectrumItems.MIDNIGHT_SOLUTION_BUCKET, fluidBucketBehavior);
+		DispenserBlock.registerBehavior(SpectrumItems.DRAGONROT_BUCKET, fluidBucketBehavior);
+		
+		// Arrows
+		for (GlassArrowVariant variant : SpectrumRegistries.GLASS_ARROW_VARIANT) {
+			DispenserBlock.registerBehavior(variant.getArrow(), new ProjectileDispenserBehavior() {
+				@Override
+				protected ProjectileEntity createProjectile(World world, Position position, ItemStack stack) {
+					GlassArrowEntity arrow = new GlassArrowEntity(world, position.getX(), position.getY(), position.getZ());
+					arrow.pickupType = PersistentProjectileEntity.PickupPermission.ALLOWED;
+					arrow.setVariant(variant);
+					return arrow;
+				}
+			});
+		}
+		
+		// Spawn Eggs
+		DispenserBehavior spawnEggBehavior = SpectrumBlocks.ENDER_DROPPER.getDefaultBehaviorForItem(Items.SHEEP_SPAWN_EGG.getDefaultStack());
+		DispenserBlock.registerBehavior(SpectrumItems.EGG_LAYING_WOOLY_PIG_SPAWN_EGG, spawnEggBehavior);
+		DispenserBlock.registerBehavior(SpectrumItems.KINDLING_SPAWN_EGG, spawnEggBehavior);
+		DispenserBlock.registerBehavior(SpectrumItems.LIZARD_SPAWN_EGG, spawnEggBehavior);
+		DispenserBlock.registerBehavior(SpectrumItems.GUARDIAN_TURRET_SPAWN_EGG, spawnEggBehavior);
+		
+		// Equipping Mob Heads
+		DispenserBehavior armorEquipBehavior = SpectrumBlocks.ENDER_DROPPER.getDefaultBehaviorForItem(Items.PLAYER_HEAD.getDefaultStack());
+		for (Block skullBlock : SpectrumBlocks.MOB_HEADS.values()) {
+			DispenserBlock.registerBehavior(skullBlock, armorEquipBehavior);
+		}
+	}
+	
 }

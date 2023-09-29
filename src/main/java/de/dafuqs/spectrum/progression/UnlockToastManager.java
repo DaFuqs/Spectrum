3fe5015ee85cac3cdb39d8a1a6ebd2059cb1a013
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-09-29 15:32:53 +0200

    unlocking >50 recipes at the same time will only display one toast

--- src/main/java/de/dafuqs/spectrum/progression/UnlockToastManager.java
+++ src/main/java/de/dafuqs/spectrum/progression/UnlockToastManager.java
@@ -59,6 +59,7 @@ public class UnlockToastManager {
 	
 	@SuppressWarnings("resource")
 	public static void processAdvancements(Set<Identifier> doneAdvancements) {
+		int unlockedRecipeCount = 0;
 		HashMap<RecipeType<?>, List<GatedRecipe>> unlockedRecipesByType = new HashMap<>();
 		List<Pair<ItemStack, String>> specialToasts = new ArrayList<>();
 		
@@ -78,6 +79,7 @@ public class UnlockToastManager {
 						if (unlockedRecipe.canPlayerCraft(MinecraftClient.getInstance().player)) {
 							if (!newRecipes.contains((unlockedRecipe))) {
 								newRecipes.add(unlockedRecipe);
+								unlockedRecipeCount++;
 							}
 						}
 					}
@@ -112,8 +114,21 @@ public class UnlockToastManager {
 			}
 		}
 		
-		for (List<GatedRecipe> unlockedRecipeList : unlockedRecipesByType.values()) {
-			showGroupedRecipeUnlockToasts(unlockedRecipeList);
+		if (unlockedRecipeCount > 50) {
+			// the player unlocked a LOT of recipes at the same time (via command?)
+			// => show a single toast. Nobody's going to remember all that stuff.
+			// At that point it would be overwhelming / annoying
+			List<ItemStack> allStacks = new ArrayList<>();
+			for (List<GatedRecipe> recipes : unlockedRecipesByType.values()) {
+				for (GatedRecipe recipe : recipes) {
+					allStacks.add(recipe.getOutput());
+				}
+			}
+			UnlockedRecipeGroupToast.showLotsOfRecipesToast(MinecraftClient.getInstance(), allStacks);
+		} else {
+			for (List<GatedRecipe> unlockedRecipeList : unlockedRecipesByType.values()) {
+				showGroupedRecipeUnlockToasts(unlockedRecipeList);
+			}
 		}
 		
 		for (Pair<ItemStack, String> messageToast : specialToasts) {
@@ -122,44 +137,51 @@ public class UnlockToastManager {
 	}
 	
 	private static void showGroupedRecipeUnlockToasts(List<GatedRecipe> unlockedRecipes) {
-		if (!unlockedRecipes.isEmpty()) {
-			Text singleText = unlockedRecipes.get(0).getSingleUnlockToastString();
-			Text multipleText = unlockedRecipes.get(0).getMultipleUnlockToastString();
-			
-			HashMap<String, List<ItemStack>> groupedRecipes = new HashMap<>();
-			
-			for (GatedRecipe recipe : unlockedRecipes) {
-				if (!recipe.getOutput().isEmpty()) { // weather recipes
-					if (recipe.getGroup() == null) {
-						SpectrumCommon.logWarning("Found a recipe with null group: " + recipe.getId().toString() + " Please report this. If you are Dafuqs and you are reading this: you messed up big time.");
-					}
-					
-					if (recipe.getGroup().isEmpty()) {
-						ItemStack displayStack = recipe.getOutput().copy();
-						displayStack.setCount(1);
-						UnlockedRecipeGroupToast.showRecipeToast(MinecraftClient.getInstance(), displayStack, singleText);
+		if (unlockedRecipes.isEmpty()) {
+			return;
+		}
+		
+		Text singleText = unlockedRecipes.get(0).getSingleUnlockToastString();
+		Text multipleText = unlockedRecipes.get(0).getMultipleUnlockToastString();
+		
+		List<ItemStack> singleRecipes = new ArrayList<>();
+		HashMap<String, List<ItemStack>> groupedRecipes = new HashMap<>();
+		
+		for (GatedRecipe recipe : unlockedRecipes) {
+			if (!recipe.getOutput().isEmpty()) { // weather recipes
+				if (recipe.getGroup() == null) {
+					SpectrumCommon.logWarning("Found a recipe with null group: " + recipe.getId().toString() + " Please report this. If you are Dafuqs and you are reading this: you messed up big time.");
+				}
+				
+				if (recipe.getGroup().isEmpty()) {
+					singleRecipes.add(recipe.getOutput());
+				} else {
+					if (groupedRecipes.containsKey(recipe.getGroup())) {
+						groupedRecipes.get(recipe.getGroup()).add(recipe.getOutput());
 					} else {
-						if (groupedRecipes.containsKey(recipe.getGroup())) {
-							groupedRecipes.get(recipe.getGroup()).add(recipe.getOutput());
-						} else {
-							List<ItemStack> newList = new ArrayList<>();
-							newList.add(recipe.getOutput());
-							groupedRecipes.put(recipe.getGroup(), newList);
-						}
+						List<ItemStack> newList = new ArrayList<>();
+						newList.add(recipe.getOutput());
+						groupedRecipes.put(recipe.getGroup(), newList);
 					}
 				}
 			}
-			
-			if (!groupedRecipes.isEmpty()) {
-				for (String group : groupedRecipes.keySet()) {
-					List<ItemStack> groupedList = groupedRecipes.get(group);
-					if (groupedList.size() == 1) {
-						UnlockedRecipeGroupToast.showRecipeToast(MinecraftClient.getInstance(), groupedList.get(0), singleText);
-					} else {
-						UnlockedRecipeGroupToast.showRecipeGroupToast(MinecraftClient.getInstance(), group, groupedRecipes.get(group), multipleText);
-					}
+		}
+		
+		// show grouped recipes
+		if (!groupedRecipes.isEmpty()) {
+			for (Map.Entry<String, List<ItemStack>> group : groupedRecipes.entrySet()) {
+				List<ItemStack> groupedList = group.getValue();
+				if (groupedList.size() == 1) {
+					UnlockedRecipeGroupToast.showRecipeToast(MinecraftClient.getInstance(), groupedList.get(0), singleText);
+				} else {
+					UnlockedRecipeGroupToast.showRecipeGroupToast(MinecraftClient.getInstance(), group.getKey(), groupedList, multipleText);
 				}
 			}
+		}
+		
+		// show singular recipes
+		for (ItemStack singleStack : singleRecipes) {
+			UnlockedRecipeGroupToast.showRecipeToast(MinecraftClient.getInstance(), singleStack, singleText);
 		}
 	}
 	

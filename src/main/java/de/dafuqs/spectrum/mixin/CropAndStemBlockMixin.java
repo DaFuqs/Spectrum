d57961fafb12bbdd7346376af3038e34cd56ce5a
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-10-21 13:31:10 +0200

    Fixed stem based plants not being able to be planted on tilled shale_clay/slush (Zenog)

---
+++ src/main/java/de/dafuqs/spectrum/mixin/CropAndStemBlockMixin.java
@@ -0,0 +1,53 @@
+package de.dafuqs.spectrum.mixin;
+
+import com.llamalad7.mixinextras.injector.*;
+import de.dafuqs.spectrum.blocks.*;
+import de.dafuqs.spectrum.blocks.farming.*;
+import de.dafuqs.spectrum.registries.*;
+import net.minecraft.block.*;
+import net.minecraft.server.world.*;
+import net.minecraft.util.math.*;
+import net.minecraft.util.math.random.*;
+import net.minecraft.world.*;
+import org.jetbrains.annotations.*;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.*;
+
+@Mixin({StemBlock.class, CropBlock.class})
+public abstract class CropAndStemBlockMixin {
+	
+	@ModifyReturnValue(method = "canPlantOnTop(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Z", at = @At("RETURN"))
+	public boolean canPlantOnTopOfCustomFarmland(boolean original, @NotNull BlockState floor, BlockView world, BlockPos pos) {
+		if (!original) {
+			Block originalBlock = floor.getBlock();
+			if (originalBlock instanceof ExtraTickFarmlandBlock || originalBlock instanceof ImmutableFarmlandBlock) {
+				return true;
+			}
+		}
+		return original;
+	}
+	
+	@ModifyReturnValue(method = "canGrow", at = @At("RETURN"))
+	public boolean cannotGrowOnShaleClay(boolean original, World world, Random random, BlockPos pos, BlockState state) {
+		if (original && world.getBlockState(pos.down()).isOf(SpectrumBlocks.TILLED_SHALE_CLAY)) {
+			return false;
+		}
+		return original;
+	}
+	
+	@Inject(at = @At("HEAD"), method = "grow", cancellable = true)
+	public void preventGrowthOnShaleClay(ServerWorld world, Random random, BlockPos pos, BlockState state, CallbackInfo ci) {
+		if (world.getBlockState(pos.down()).isOf(SpectrumBlocks.TILLED_SHALE_CLAY)) {
+			ci.cancel();
+		}
+	}
+	
+	@Inject(at = @At("HEAD"), method = "isFertilizable(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Z)Z", cancellable = true)
+	public void isFertilizable(BlockView world, BlockPos pos, BlockState state, boolean isClient, CallbackInfoReturnable<Boolean> cir) {
+		if (world.getBlockState(pos.down()).isOf(SpectrumBlocks.TILLED_SHALE_CLAY)) {
+			cir.cancel();
+		}
+	}
+	
+}

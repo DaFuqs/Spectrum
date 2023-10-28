db4c7ee92e7e79ef963252b3155637e12fd1357f
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-10-28 15:37:25 +0200

    block detector & block placer can be rotated in more directions

--- src/main/java/de/dafuqs/spectrum/blocks/redstone/BlockPlacerBlock.java
+++ src/main/java/de/dafuqs/spectrum/blocks/redstone/BlockPlacerBlock.java
@@ -1,15 +1,21 @@
 package de.dafuqs.spectrum.blocks.redstone;
 
 import net.minecraft.block.*;
-import net.minecraft.block.dispenser.*;
 import net.minecraft.block.entity.*;
+import net.minecraft.entity.*;
+import net.minecraft.entity.player.*;
 import net.minecraft.item.*;
+import net.minecraft.screen.*;
+import net.minecraft.server.world.*;
+import net.minecraft.util.*;
+import net.minecraft.util.hit.*;
 import net.minecraft.util.math.*;
+import net.minecraft.util.math.random.*;
+import net.minecraft.world.*;
+import net.minecraft.world.event.*;
 import org.jetbrains.annotations.*;
 
-public class BlockPlacerBlock extends DispenserBlock {
-	
-	public static DispenserBehavior BLOCK_PLACEMENT_BEHAVIOR = new BlockPlacementDispenserBehavior();
+public class BlockPlacerBlock extends RedstoneInteractionBlock implements BlockEntityProvider {
 	
 	public BlockPlacerBlock(Settings settings) {
 		super(settings);
@@ -21,14 +27,98 @@ public class BlockPlacerBlock extends DispenserBlock {
 	}
 	
 	@Override
-	protected DispenserBehavior getBehaviorForItem(@NotNull ItemStack stack) {
-		if (stack.getItem() instanceof BlockItem) {
-			return BLOCK_PLACEMENT_BEHAVIOR;
+	public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+		if (world.isClient) {
+			return ActionResult.SUCCESS;
+		} else {
+			if (world.getBlockEntity(pos) instanceof BlockPlacerBlockEntity blockPlacerBlockEntity) {
+				player.openHandledScreen(blockPlacerBlockEntity);
+			}
+			return ActionResult.CONSUME;
 		}
-		// If it is not a block item do not handle the selected item at all
-		// this is not to overlap with dispenser / dropper behavior
-		// and not to dispense items into walls
-		return DispenserBehavior.NOOP;
 	}
+	
+	protected void dispense(ServerWorld world, BlockPos pos) {
+		BlockPointerImpl pointer = new BlockPointerImpl(world, pos);
+		BlockPlacerBlockEntity blockEntity = pointer.getBlockEntity();
+		
+		int slot = blockEntity.chooseNonEmptySlot(world.random);
+		if (slot < 0) {
+			world.syncWorldEvent(1001, pos, 0);
+			world.emitGameEvent(null, GameEvent.DISPENSE_FAIL, pos);
+		} else {
+			ItemStack stack = blockEntity.getStack(slot);
+			tryPlace(stack, pointer);
+		}
+	}
+	
+	// We can't reuse the vanilla BlockPlacementDispenserBehavior, since we are using a different orientation for our block:
+	// BlockPlacerBlock.ORIENTATION instead of DispenserBlock.FACING
+	protected void tryPlace(@NotNull ItemStack stack, BlockPointer pointer) {
+		if (stack.getItem() instanceof BlockItem blockItem) {
+			Direction facing = pointer.getBlockState().get(BlockPlacerBlock.ORIENTATION).getFacing();
+			BlockPos placementPos = pointer.getPos().offset(facing);
+			Direction placementDirection = pointer.getWorld().isAir(placementPos.down()) ? facing : Direction.UP;
+			
+			try {
+				blockItem.place(new AutomaticItemPlacementContext(pointer.getWorld(), placementPos, facing, stack, placementDirection));
+				pointer.getWorld().syncWorldEvent(WorldEvents.DISPENSER_DISPENSES, pointer.getPos(), 0);
+				pointer.getWorld().syncWorldEvent(WorldEvents.DISPENSER_ACTIVATED, pointer.getPos(), pointer.getBlockState().get(BlockPlacerBlock.ORIENTATION).getFacing().getId());
+			} catch (Exception ignored) {
+			}
+		} else {
+			pointer.getWorld().emitGameEvent(null, GameEvent.DISPENSE_FAIL, pointer.getPos());
+		}
+	}
+	
+	@Override
+	public void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
+		boolean bl = world.isReceivingRedstonePower(pos) || world.isReceivingRedstonePower(pos.up());
+		boolean bl2 = state.get(TRIGGERED);
+		if (bl && !bl2) {
+			world.createAndScheduleBlockTick(pos, this, 4);
+			world.setBlockState(pos, state.with(TRIGGERED, true), 4);
+		} else if (!bl && bl2) {
+			world.setBlockState(pos, state.with(TRIGGERED, false), 4);
+		}
+	}
+	
+	@Override
+	public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+		this.dispense(world, pos);
+	}
+	
+	@Override
+	public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
+		if (itemStack.hasCustomName()) {
+			if (world.getBlockEntity(pos) instanceof BlockPlacerBlockEntity blockPlacerBlockEntity) {
+				blockPlacerBlockEntity.setCustomName(itemStack.getName());
+			}
+		}
+		
+	}
+	
+	@Override
+	public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+		if (!state.isOf(newState.getBlock())) {
+			if (world.getBlockEntity(pos) instanceof BlockPlacerBlockEntity blockPlacerBlockEntity) {
+				ItemScatterer.spawn(world, pos, blockPlacerBlockEntity);
+				world.updateComparators(pos, this);
+			}
+			
+			super.onStateReplaced(state, world, pos, newState, moved);
+		}
+	}
+	
+	@Override
+	public boolean hasComparatorOutput(BlockState state) {
+		return true;
+	}
+	
+	@Override
+	public int getComparatorOutput(BlockState state, World world, BlockPos pos) {
+		return ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos));
+	}
+	
 	
 }

4cca428f03cb43409b4a9ade92f299530c843695
Isegrim <dominik.m.schmidt@googlemail.com>
2023-11-21 22:30:39 +0100

    that space annoyed me

--- src/main/java/de/dafuqs/spectrum/registries/SpectrumTillableBlocks.java
+++ src/main/java/de/dafuqs/spectrum/registries/SpectrumTillableBlocks.java
@@ -7,7 +7,6 @@ public class SpectrumTillableBlocks {
 	
 	public static void register() {
 		TillableBlockRegistry.register(SpectrumBlocks.SLUSH, HoeItem::canTillFarmland, SpectrumBlocks.TILLED_SLUSH.getDefaultState());
-		
 		TillableBlockRegistry.register(SpectrumBlocks.SHALE_CLAY, HoeItem::canTillFarmland, SpectrumBlocks.TILLED_SHALE_CLAY.getDefaultState());
 	}
 	

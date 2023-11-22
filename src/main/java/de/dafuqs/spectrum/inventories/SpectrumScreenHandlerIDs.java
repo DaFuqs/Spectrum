1bbfc4846107d5deb5c51200adad7f8a72c92567
Isegrim <dominik.m.schmidt@googlemail.com>
2023-11-22 20:56:49 +0100

    made screenhandlerids final

--- src/main/java/de/dafuqs/spectrum/inventories/SpectrumScreenHandlerIDs.java
+++ src/main/java/de/dafuqs/spectrum/inventories/SpectrumScreenHandlerIDs.java
@@ -4,64 +4,33 @@ import de.dafuqs.spectrum.*;
 import net.minecraft.util.*;
 
 public class SpectrumScreenHandlerIDs {
-    // TODO - Make these final?
-	
-	public static Identifier PAINTBRUSH;
-	public static Identifier WORKSTAFF;
 
-    public static Identifier PEDESTAL;
-    public static Identifier CRAFTING_TABLET;
-    public static Identifier RESTOCKING_CHEST;
-    public static Identifier BEDROCK_ANVIL;
-    public static Identifier PARTICLE_SPAWNER;
-    public static Identifier COMPACTING_CHEST;
-    public static Identifier BLACK_HOLE_CHEST;
-    public static Identifier POTION_WORKSHOP;
-    public static Identifier COLOR_PICKER;
-    public static Identifier CINDERHEARTH;
-    public static Identifier FILTERING;
-	public static Identifier BAG_OF_HOLDING;
+	public static final Identifier PAINTBRUSH = SpectrumCommon.locate("paintbrush");
+	public static final Identifier WORKSTAFF = SpectrumCommon.locate("workstaff");
 
-    public static Identifier GENERIC_TIER1_9x3;
-    public static Identifier GENERIC_TIER2_9x3;
-    public static Identifier GENERIC_TIER3_9x3;
+	public static final Identifier PEDESTAL = SpectrumCommon.locate("pedestal");
+	public static final Identifier CRAFTING_TABLET = SpectrumCommon.locate("crafting_tablet");
+	public static final Identifier RESTOCKING_CHEST = SpectrumCommon.locate("restocking_chest");
+	public static final Identifier BEDROCK_ANVIL = SpectrumCommon.locate("bedrock_anvil");
+	public static final Identifier PARTICLE_SPAWNER = SpectrumCommon.locate("particle_spawner");
+	public static final Identifier COMPACTING_CHEST = SpectrumCommon.locate("compacting_chest");
+	public static final Identifier BLACK_HOLE_CHEST = SpectrumCommon.locate("black_hole_chest");
+	public static final Identifier POTION_WORKSHOP = SpectrumCommon.locate("potion_workshop");
+	public static final Identifier COLOR_PICKER = SpectrumCommon.locate("color_picker");
+	public static final Identifier CINDERHEARTH = SpectrumCommon.locate("cinderhearth");
+	public static final Identifier FILTERING = SpectrumCommon.locate("filtering");
+	public static final Identifier BAG_OF_HOLDING = SpectrumCommon.locate("bag_of_holding");
 
-    public static Identifier GENERIC_TIER1_9x6;
-    public static Identifier GENERIC_TIER2_9x6;
-    public static Identifier GENERIC_TIER3_9x6;
+	public static final Identifier GENERIC_TIER1_9x3 = SpectrumCommon.locate("generic_tier1_9x3");
+	public static final Identifier GENERIC_TIER2_9x3 = SpectrumCommon.locate("generic_tier2_9x3");
+	public static final Identifier GENERIC_TIER3_9x3 = SpectrumCommon.locate("generic_tier3_9x3");
 
-    public static Identifier GENERIC_TIER1_3X3;
-	public static Identifier GENERIC_TIER2_3X3;
-	public static Identifier GENERIC_TIER3_3X3;
-	
-	public static void register() {
-		PAINTBRUSH = SpectrumCommon.locate("paintbrush");
-		WORKSTAFF = SpectrumCommon.locate("workstaff");
+	public static final Identifier GENERIC_TIER1_9x6 = SpectrumCommon.locate("generic_tier1_9x6");
+	public static final Identifier GENERIC_TIER2_9x6 = SpectrumCommon.locate("generic_tier2_9x6");
+	public static final Identifier GENERIC_TIER3_9x6 = SpectrumCommon.locate("generic_tier3_9x6");
 
-        PEDESTAL = SpectrumCommon.locate("pedestal");
-        CRAFTING_TABLET = SpectrumCommon.locate("crafting_tablet");
-        RESTOCKING_CHEST = SpectrumCommon.locate("restocking_chest");
-        BEDROCK_ANVIL = SpectrumCommon.locate("bedrock_anvil");
-        PARTICLE_SPAWNER = SpectrumCommon.locate("particle_spawner");
-        COMPACTING_CHEST = SpectrumCommon.locate("compacting_chest");
-        BLACK_HOLE_CHEST = SpectrumCommon.locate("black_hole_chest");
-        POTION_WORKSHOP = SpectrumCommon.locate("potion_workshop");
-        COLOR_PICKER = SpectrumCommon.locate("color_picker");
-        CINDERHEARTH = SpectrumCommon.locate("cinderhearth");
-		FILTERING = SpectrumCommon.locate("filtering");
-		BAG_OF_HOLDING = SpectrumCommon.locate("bag_of_holding");
+	public static final Identifier GENERIC_TIER1_3X3 = SpectrumCommon.locate("generic_tier1_3x3");
+	public static final Identifier GENERIC_TIER2_3X3 = SpectrumCommon.locate("generic_tier2_3x3");
+	public static final Identifier GENERIC_TIER3_3X3 = SpectrumCommon.locate("generic_tier3_3x3");
 
-        GENERIC_TIER1_9x3 = SpectrumCommon.locate("generic_tier1_9x3");
-        GENERIC_TIER2_9x3 = SpectrumCommon.locate("generic_tier2_9x3");
-        GENERIC_TIER3_9x3 = SpectrumCommon.locate("generic_tier3_9x3");
-
-        GENERIC_TIER1_9x6 = SpectrumCommon.locate("generic_tier1_9x6");
-        GENERIC_TIER2_9x6 = SpectrumCommon.locate("generic_tier2_9x6");
-        GENERIC_TIER3_9x6 = SpectrumCommon.locate("generic_tier3_9x6");
-
-        GENERIC_TIER1_3X3 = SpectrumCommon.locate("generic_tier1_3x3");
-		GENERIC_TIER2_3X3 = SpectrumCommon.locate("generic_tier2_3x3");
-		GENERIC_TIER3_3X3 = SpectrumCommon.locate("generic_tier3_3x3");
-	}
-	
 }

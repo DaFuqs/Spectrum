fdcfe6d67e91598215b1e550a9caa56dbc67cdc0
Electro_593 <electro5939@gmail.com>
2023-12-01 19:21:16 -0500

    Fix MC252934Mixin remap error

--- src/main/java/de/dafuqs/spectrum/mixin/MC252934Mixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/MC252934Mixin.java
@@ -1,17 +1,15 @@
 package de.dafuqs.spectrum.mixin;
 
 import net.minecraft.entity.decoration.*;
-import net.minecraft.nbt.*;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.*;
 
 @Mixin(AbstractDecorationEntity.class)
 public abstract class MC252934Mixin {
-	
-	@Inject(method = "readCustomDataFromNbt(Lnet/minecraft/nbt/NbtCompound;)V", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Ljava/lang/String;Ljava/lang/Object;)V"), cancellable = true)
-	private void spectrum$fixMC252934(NbtCompound nbt, CallbackInfo ci) {
-		ci.cancel();
+
+	@Redirect(method = "readCustomDataFromNbt", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Ljava/lang/String;Ljava/lang/Object;)V", remap = false))
+	private void spectrum$fixMC252934(Logger thisLogger, String format, Object arg) {
 	}
-	
+
 }

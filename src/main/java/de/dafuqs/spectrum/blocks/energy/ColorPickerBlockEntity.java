abd1339b24f995edba731fb8492deda19133766c
Electro_593 <electro5939@gmail.com>
2023-12-02 00:46:52 -0500

    Actually fix color picker breaking when full (git broke or something)

--- src/main/java/de/dafuqs/spectrum/blocks/energy/ColorPickerBlockEntity.java
+++ src/main/java/de/dafuqs/spectrum/blocks/energy/ColorPickerBlockEntity.java
@@ -202,7 +202,7 @@ public class ColorPickerBlockEntity extends LootableContainerBlockEntity impleme
 		if (recipe != null) {
 			InkColor color = recipe.getInkColor();
 			long amount = recipe.getInkAmount();
-			if (this.inkStorage.getEnergy(color) + amount <= this.inkStorage.getMaxPerColor()) {
+			if (amount <= this.inkStorage.getRoom(color)) {
 				inventory.get(INPUT_SLOT_ID).decrement(1);
 				this.inkStorage.addEnergy(color, amount);
 				
@@ -255,33 +255,18 @@ public class ColorPickerBlockEntity extends LootableContainerBlockEntity impleme
 		ItemStack stack = inventory.get(OUTPUT_SLOT_ID);
 		if (stack.getItem() instanceof InkStorageItem<?> inkStorageItem) {
 			InkStorage itemStorage = inkStorageItem.getEnergyStorage(stack);
-			
+
+			ServerPlayerEntity owner = null;
+			if (getOwnerIfOnline() instanceof ServerPlayerEntity serverPlayerEntity) {
+				owner = serverPlayerEntity;
+			}
+
 			if (this.selectedColor == null) {
-				boolean searchedOwner = false;
-				ServerPlayerEntity owner = null;
 				for (InkColor color : InkColor.all()) {
-					long amount = InkStorage.transferInk(inkStorage, itemStorage, color);
-					
-					if (amount > 0) {
-						if (!searchedOwner) {
-							owner = (ServerPlayerEntity) getOwnerIfOnline();
-						}
-						if (owner != null) {
-							SpectrumAdvancementCriteria.INK_CONTAINER_INTERACTION.trigger(owner, stack, itemStorage, color, amount);
-						}
-					}
-					
-					transferredAmount += amount;
+					transferredAmount += tryTransferInk(owner, stack, itemStorage, color);
 				}
 			} else {
-				transferredAmount = InkStorage.transferInk(inkStorage, itemStorage, this.selectedColor);
-				
-				if (transferredAmount > 0) {
-					PlayerEntity owner = getOwnerIfOnline();
-					if (owner instanceof ServerPlayerEntity serverPlayerEntity) {
-						SpectrumAdvancementCriteria.INK_CONTAINER_INTERACTION.trigger(serverPlayerEntity, stack, itemStorage, this.selectedColor, transferredAmount);
-					}
-				}
+				transferredAmount = tryTransferInk(owner, stack, itemStorage, this.selectedColor);
 			}
 			
 			if (transferredAmount > 0) {
@@ -291,7 +276,15 @@ public class ColorPickerBlockEntity extends LootableContainerBlockEntity impleme
 		
 		return transferredAmount > 0;
 	}
-	
+
+	private long tryTransferInk(ServerPlayerEntity owner, ItemStack stack, InkStorage itemStorage, InkColor color) {
+		long amount = InkStorage.transferInk(this.inkStorage, itemStorage, color);
+		if (amount > 0 && owner != null) {
+			SpectrumAdvancementCriteria.INK_CONTAINER_INTERACTION.trigger(owner, stack, itemStorage, color, amount);
+		}
+		return amount;
+	}
+
 	public void setSelectedColor(InkColor inkColor) {
 		this.selectedColor = inkColor;
 		this.paused = false;

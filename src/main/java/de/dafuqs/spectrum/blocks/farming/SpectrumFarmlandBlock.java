a6f4627f43fa175126bf693b165916a146806957
Electro_593 <electro5939@gmail.com>
2023-12-01 23:23:24 -0500

    Port tilled slush fix

--- src/main/java/de/dafuqs/spectrum/blocks/farming/SpectrumFarmlandBlock.java
+++ src/main/java/de/dafuqs/spectrum/blocks/farming/SpectrumFarmlandBlock.java
@@ -3,10 +3,12 @@ package de.dafuqs.spectrum.blocks.farming;
 import net.minecraft.block.*;
 import net.minecraft.entity.*;
 import net.minecraft.entity.player.*;
+import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.server.world.*;
 import net.minecraft.util.math.*;
 import net.minecraft.util.math.random.*;
 import net.minecraft.world.*;
+import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.*;
 
 public class SpectrumFarmlandBlock extends FarmlandBlock {
@@ -21,7 +23,21 @@ public class SpectrumFarmlandBlock extends FarmlandBlock {
 	@Override
 	public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
 		if (!state.canPlaceAt(world, pos)) {
-			setBare(state, world, pos);
+			setBare(null, state, world, pos);
+		}
+	}
+
+	@Override
+	public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+		int i = state.get(MOISTURE);
+		if (!isWaterNearby(world, pos) && !world.hasRain(pos.up())) {
+			if (i > 0) {
+				world.setBlockState(pos, state.with(MOISTURE, i - 1), 2);
+			} else if (!hasCrop(world, pos)) {
+				setBare(null, state, world, pos);
+			}
+		} else if (i < 7) {
+			world.setBlockState(pos, state.with(MOISTURE, 7), 2);
 		}
 	}
 	
@@ -32,19 +48,30 @@ public class SpectrumFarmlandBlock extends FarmlandBlock {
 				&& (entity instanceof PlayerEntity || world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING))
 				&& entity.getWidth() * entity.getWidth() * entity.getHeight() > 0.512F) {
 			
-			setBare(state, world, pos);
+			setBare(entity, state, world, pos);
 		}
 		
 		entity.handleFallDamage(fallDistance, 1.0F, world.getDamageSources().fall());
 	}
 	
-	public void setBare(BlockState state, World world, BlockPos pos) {
-		world.setBlockState(pos, pushEntitiesUpBeforeBlockChange(state, bareState, world, pos));
+	public void setBare(@Nullable Entity entity, BlockState state, World world, BlockPos pos) {
+		BlockState blockState = pushEntitiesUpBeforeBlockChange(state, bareState, world, pos);
+		world.setBlockState(pos, blockState);
+		world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(entity, blockState));
 	}
 	
 	public static boolean hasCrop(@NotNull BlockView world, @NotNull BlockPos pos) {
 		Block block = world.getBlockState(pos.up()).getBlock();
 		return block instanceof CropBlock || block instanceof StemBlock || block instanceof AttachedStemBlock;
 	}
-	
+
+	private static boolean isWaterNearby(WorldView world, BlockPos pos) {
+		for (BlockPos testPos : BlockPos.iterate(pos.add(-4, 0, -4), pos.add(4, 1, 4))) {
+			if (world.getFluidState(testPos).isIn(FluidTags.WATER)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 }

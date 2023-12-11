f9e1be886fcad525b6be6b144de55558fba35698
Electro_593 <electro5939@gmail.com>
2023-12-10 22:03:18 -0500

    Copy the map update function and fix variable names

--- src/main/java/de/dafuqs/spectrum/items/magic_items/StructureMapItem.java
+++ src/main/java/de/dafuqs/spectrum/items/magic_items/StructureMapItem.java
@@ -1,7 +1,16 @@
 package de.dafuqs.spectrum.items.magic_items;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.LinkedHashMultiset;
+import com.google.common.collect.Multiset;
+import com.google.common.collect.Multisets;
 import de.dafuqs.spectrum.items.map.StructureMapState;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.item.FilledMapItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
@@ -14,10 +23,10 @@ import net.minecraft.structure.StructureStart;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.*;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.WorldChunk;
 
 import java.util.List;
 
@@ -27,21 +36,25 @@ public class StructureMapItem extends FilledMapItem {
         super(settings);
     }
 
-    private static void createAndSetState(ItemStack stack, ServerWorld world, double x, double z) {
+    private static void createAndSetState(ItemStack stack, ServerWorld world, int x, int z) {
         NbtCompound nbt = stack.getOrCreateNbt();
 
         int id;
         if (nbt.contains("map")) {
             id = nbt.getInt("map");
+            MapState state = getMapState(id, world);
+            if (state != null) {
+                StructureMapState.removeDecorationsNbt(stack, new BlockPos(state.centerX, 0, state.centerZ), "+");
+            }
         } else {
             id = world.getNextMapId();
             nbt.putInt("map", id);
         }
 
-        MapState state = StructureMapState.of(x, z, (byte) 1, true, true, world.getRegistryKey());
+        MapState state = new StructureMapState(x, z, (byte) 1, true, true, false, world.getRegistryKey());
         world.putMapState(getMapName(id), state);
         fillExplorationMap(world, stack);
-        MapState.addDecorationsNbt(stack, new BlockPos((int) x, 0, (int) z), "+", MapIcon.Type.RED_X);
+        MapState.addDecorationsNbt(stack, new BlockPos(x, 0, z), "+", MapIcon.Type.RED_X);
     }
 
     private static void setTarget(ItemStack stack, ServerWorld world, StructureStart start) {
@@ -58,12 +71,141 @@ public class StructureMapItem extends FilledMapItem {
     }
 
     private static void clearTarget(ItemStack stack, ServerWorld world, ServerPlayerEntity player) {
-        createAndSetState(stack, world, player.getX(), player.getZ());
+        createAndSetState(stack, world, (int) player.getX(), (int) player.getZ());
 
         NbtCompound nbt = stack.getNbt();
         if (nbt != null) {
             nbt.remove("mapTarget");
         }
+    }
+
+    @Override
+    public void updateColors(World world, Entity entity, MapState state) {
+        if (world.getRegistryKey() != state.dimension || !(entity instanceof PlayerEntity)) {
+            return;
+        }
+
+        boolean hasCeiling = world.getDimension().hasCeiling();
+
+        int scale = 1 << state.scale;
+        int playerX = MathHelper.floor(entity.getX() - (double) state.centerX) / scale + 64;
+        int playerZ = MathHelper.floor(entity.getZ() - (double) state.centerZ) / scale + 64;
+        int radius = 128 >> (state.scale + (hasCeiling ? 1 : 0));
+
+        MapState.PlayerUpdateTracker playerUpdateTracker = state.getPlayerSyncData((PlayerEntity)entity);
+        playerUpdateTracker.field_131++;
+
+        boolean updated = false;
+        for(int x = playerX - radius + 1; x < playerX + radius; x++) {
+            if ((x & 15) != (playerUpdateTracker.field_131 & 15) && !updated) {
+                continue;
+            }
+
+            updated = false;
+            double previousHeight = 0.0;
+
+            for(int z = playerZ - radius - 1; z < playerZ + radius; z++) {
+                if (x < 0 || z < -1 || x >= 128 || z >= 128) {
+                    continue;
+                }
+
+                int distanceSquared = MathHelper.square(x - playerX) + MathHelper.square(z - playerZ);
+                boolean withinSmallerRadius = distanceSquared > (radius - 2) * (radius - 2);
+
+                int blockX = (state.centerX / scale + x - 64) * scale;
+                int blockZ = (state.centerZ / scale + z - 64) * scale;
+                Multiset<MapColor> multiset = LinkedHashMultiset.create();
+                WorldChunk chunk = world.getChunk(ChunkSectionPos.getSectionCoord(blockX), ChunkSectionPos.getSectionCoord(blockZ));
+                if (chunk.isEmpty()) {
+                    continue;
+                }
+
+                int fluidDepth = 0;
+                double height = 0.0;
+                if (hasCeiling) {
+                    int hash = blockX + blockZ * 231871;
+                    hash = hash * hash * 31287121 + hash * 11;
+                    if ((hash >> 20 & 1) == 0) {
+                        multiset.add(Blocks.DIRT.getDefaultState().getMapColor(world, BlockPos.ORIGIN), 10);
+                    } else {
+                        multiset.add(Blocks.STONE.getDefaultState().getMapColor(world, BlockPos.ORIGIN), 100);
+                    }
+
+                    height = 100.0;
+                } else {
+                    for(int sampleX = 0; sampleX < scale; sampleX++) {
+                        for(int sampleZ = 0; sampleZ < scale; sampleZ++) {
+                            BlockPos.Mutable samplePos = new BlockPos.Mutable(blockX + sampleX, 0, blockZ + sampleZ);
+                            int sampleY = chunk.sampleHeightmap(Heightmap.Type.WORLD_SURFACE, samplePos.getX(), samplePos.getZ()) + 1;
+
+                            BlockState blockState;
+                            if (sampleY <= world.getBottomY() + 1) {
+                                blockState = Blocks.BEDROCK.getDefaultState();
+                            } else {
+                                do {
+                                    sampleY--;
+                                    samplePos.setY(sampleY);
+                                    blockState = chunk.getBlockState(samplePos);
+                                } while(blockState.getMapColor(world, samplePos) == MapColor.CLEAR && sampleY > world.getBottomY());
+
+                                if (sampleY > world.getBottomY() && !blockState.getFluidState().isEmpty()) {
+                                    int fluidY = sampleY - 1;
+                                    BlockPos.Mutable fluidPos = samplePos.mutableCopy();
+
+                                    BlockState fluidBlockState;
+                                    do {
+                                        fluidPos.setY(fluidY--);
+                                        fluidBlockState = chunk.getBlockState(fluidPos);
+                                        fluidDepth++;
+                                    } while(fluidY > world.getBottomY() && !fluidBlockState.getFluidState().isEmpty());
+
+                                    blockState = this.getFluidStateIfVisible(world, blockState, samplePos);
+                                }
+                            }
+
+                            state.removeBanner(world, samplePos.getX(), samplePos.getZ());
+                            height += (double) sampleY / (double) (scale * scale);
+                            multiset.add(blockState.getMapColor(world, samplePos));
+                        }
+                    }
+                }
+
+                fluidDepth /= scale * scale;
+                MapColor color = Iterables.getFirst(Multisets.copyHighestCountFirst(multiset), MapColor.CLEAR);
+                MapColor.Brightness brightness;
+
+                int odd = x + z & 1;
+                if (color == MapColor.WATER_BLUE) {
+                    double depth = (double) fluidDepth * 0.1 + (double) odd * 0.2;
+                    if (depth < 0.5) {
+                        brightness = MapColor.Brightness.HIGH;
+                    } else if (depth > 0.9) {
+                        brightness = MapColor.Brightness.LOW;
+                    } else {
+                        brightness = MapColor.Brightness.NORMAL;
+                    }
+                } else {
+                    double f = (height - previousHeight) * 4.0 / (double) (scale + 4) + ((double) odd - 0.5) * 0.4;
+                    if (f > 0.6) {
+                        brightness = MapColor.Brightness.HIGH;
+                    } else if (f < -0.6) {
+                        brightness = MapColor.Brightness.LOW;
+                    } else {
+                        brightness = MapColor.Brightness.NORMAL;
+                    }
+                }
+
+                previousHeight = height;
+                if (z >= 0 && distanceSquared < radius * radius && (!withinSmallerRadius || odd != 0)) {
+                    updated |= state.putColor(x, z, color.getRenderColorByte(brightness));
+                }
+            }
+        }
+    }
+
+    private BlockState getFluidStateIfVisible(World world, BlockState state, BlockPos pos) {
+        FluidState fluidState = state.getFluidState();
+        return !fluidState.isEmpty() && !state.isSideSolidFullSquare(world, pos, Direction.UP) ? fluidState.getBlockState() : state;
     }
 
     @Override
@@ -99,4 +241,5 @@ public class StructureMapItem extends FilledMapItem {
 
         return TypedActionResult.success(stack, world.isClient());
     }
+
 }
--- src/main/java/de/dafuqs/spectrum/items/map/StructureMapState.java
+++ src/main/java/de/dafuqs/spectrum/items/map/StructureMapState.java
@@ -1,24 +1,25 @@
 package de.dafuqs.spectrum.items.map;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.map.MapIcon;
 import net.minecraft.item.map.MapState;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
 public class StructureMapState extends MapState {
 
-    private static final int MAP_SIZE = 128;
     private final boolean showIcons;
     private final boolean unlimitedTracking;
 
-    private StructureMapState(int centerX, int centerZ, byte scale, boolean showIcons, boolean unlimitedTracking, boolean locked, RegistryKey<World> dimension) {
+    public StructureMapState(int centerX, int centerZ, byte scale, boolean showIcons, boolean unlimitedTracking, boolean locked, RegistryKey<World> dimension) {
         super(centerX, centerZ, scale, showIcons, unlimitedTracking, locked, dimension);
         this.showIcons = showIcons;
         this.unlimitedTracking = unlimitedTracking;
-    }
-
-    public static MapState of(double centerX, double centerZ, byte scale, boolean showIcons, boolean unlimitedTracking, RegistryKey<World> dimension) {
-        return new StructureMapState((int) centerX, (int) centerZ, scale, showIcons, unlimitedTracking, false, dimension);
     }
 
     @Override
@@ -26,7 +27,21 @@ public class StructureMapState extends MapState {
         return of(this.centerX, this.centerZ, (byte) MathHelper.clamp(this.scale + zoomOutScale, 0, 4), this.showIcons, this.unlimitedTracking, this.dimension);
     }
 
-    // update
-    // addIcon
-    // addBanner
+    public static void removeDecorationsNbt(ItemStack stack, BlockPos pos, String id) {
+        NbtCompound nbt = stack.getNbt();
+        if (nbt != null && nbt.contains("Decorations", NbtElement.LIST_TYPE)) {
+            NbtList decorations = nbt.getList("Decorations", NbtElement.COMPOUND_TYPE);
+            for (int i = 0; i < decorations.size(); i++) {
+                NbtCompound decoration = decorations.getCompound(i);
+                String decorationId = decoration.contains("id", NbtElement.STRING_TYPE) ? decoration.getString("id") : null;
+                Double x = decoration.contains("x", NbtElement.DOUBLE_TYPE) ? decoration.getDouble("x") : null;
+                Double z = decoration.contains("z", NbtElement.DOUBLE_TYPE) ? decoration.getDouble("z") : null;
+                if ((decorationId == null || decorationId.equals(id)) && (x == null || x == pos.getX()) && (z == null || z == pos.getZ())) {
+                    decorations.remove(i);
+                    i--;
+                }
+            }
+        }
+    }
+
 }

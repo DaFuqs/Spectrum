79029e4e9cd7862c1c91ed7ea2fc18c6050effd5
Electro_593 <electro5939@gmail.com>
2023-12-21 12:12:03 -0500

    BookCollectionPage

--- src/main/java/de/dafuqs/spectrum/compat/modonomicon/ModonomiconCompat.java
+++ src/main/java/de/dafuqs/spectrum/compat/modonomicon/ModonomiconCompat.java
@@ -1,10 +1,19 @@
 package de.dafuqs.spectrum.compat.modonomicon;
 
+import com.klikli_dev.modonomicon.client.render.page.BookTextPageRenderer;
+import com.klikli_dev.modonomicon.client.render.page.PageRendererRegistry;
+import com.klikli_dev.modonomicon.data.LoaderRegistry;
+import de.dafuqs.spectrum.SpectrumCommon;
 import de.dafuqs.spectrum.compat.*;
+import de.dafuqs.spectrum.compat.modonomicon.client.pages.BookCollectionPageRenderer;
+import de.dafuqs.spectrum.compat.modonomicon.pages.BookCollectionPage;
 import de.dafuqs.spectrum.items.*;
 import de.dafuqs.spectrum.registries.*;
+import net.minecraft.util.Identifier;
 
 public class ModonomiconCompat extends SpectrumIntegrationPacks.ModIntegrationPack {
+
+    public static final Identifier COLLECTION_PAGE = SpectrumCommon.locate("collection");
     
     @Override
     public void register() {
@@ -16,33 +25,33 @@ public class ModonomiconCompat extends SpectrumIntegrationPacks.ModIntegrationPa
     }
     
     public void registerPages() {
-        /*LoaderRegistry.registerPageLoader(SpectrumCommon.locate("anvil_crushing"), PageAnvilCrushing::fromJson, PageAnvilCrushing::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("pedestal_crafting"), PagePedestalCrafting::fromJson, PagePedestalCrafting::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("fusion_shrine_crafting"), PageFusionShrine::fromJson, PageFusionShrine::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("enchanter_crafting"), PageEnchanterRecipe::fromJson, PageEnchanterRecipe::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("enchanter_upgrading"), PageEnchantmentUpgradeRecipe::fromJson, PageEnchantmentUpgradeRecipe::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("potion_workshop_brewing"), PagePotionWorkshopBrewing::fromJson, PagePotionWorkshopBrewing::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("potion_workshop_crafting"), PagePotionWorkshopCrafting::fromJson, PagePotionWorkshopCrafting::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("spirit_instiller_crafting"), PageSpiritInstillerCrafting::fromJson, PageSpiritInstillerCrafting::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("liquid_crystal_converting"), PageLiquidCrystalConverting::fromJson, PageLiquidCrystalConverting::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("midnight_solution_converting"), PageMidnightSolutionConverting::fromJson, PageMidnightSolutionConverting::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("dragonrot_converting"), PageDragonrotConverting::fromJson, PageDragonrotConverting::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("mud_converting"), PageMudConverting::fromJson, PageMudConverting::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("crystallarieum_growing"), PageCrystallarieumGrowing::fromJson, PageCrystallarieumGrowing::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("cinderhearth_smelting"), PageCinderhearthSmelting::fromJson, PageCinderhearthSmelting::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("titration_barrel_fermenting"), PageTitrationBarrelFermenting::fromJson, PageTitrationBarrelFermenting::fromNetwork);
-    
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("status_effect"), PageStatusEffect::fromJson, PageStatusEffect::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("hint"), PageHint::fromJson, PageHint::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("checklist"), PageChecklist::fromJson, PageChecklist::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("confirmation_button"), PageConfirmationButton::fromJson, PageConfirmationButton::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("snippet"), PageSnippet::fromJson, PageSnippet::fromNetwork);
-        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("collection"), PageCollection::fromJson, PageCollection::fromNetwork);*/
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("anvil_crushing"), PageAnvilCrushing::fromJson, PageAnvilCrushing::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("pedestal_crafting"), PagePedestalCrafting::fromJson, PagePedestalCrafting::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("fusion_shrine_crafting"), PageFusionShrine::fromJson, PageFusionShrine::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("enchanter_crafting"), PageEnchanterRecipe::fromJson, PageEnchanterRecipe::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("enchanter_upgrading"), PageEnchantmentUpgradeRecipe::fromJson, PageEnchantmentUpgradeRecipe::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("potion_workshop_brewing"), PagePotionWorkshopBrewing::fromJson, PagePotionWorkshopBrewing::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("potion_workshop_crafting"), PagePotionWorkshopCrafting::fromJson, PagePotionWorkshopCrafting::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("spirit_instiller_crafting"), PageSpiritInstillerCrafting::fromJson, PageSpiritInstillerCrafting::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("liquid_crystal_converting"), PageLiquidCrystalConverting::fromJson, PageLiquidCrystalConverting::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("midnight_solution_converting"), PageMidnightSolutionConverting::fromJson, PageMidnightSolutionConverting::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("dragonrot_converting"), PageDragonrotConverting::fromJson, PageDragonrotConverting::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("mud_converting"), PageMudConverting::fromJson, PageMudConverting::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("crystallarieum_growing"), PageCrystallarieumGrowing::fromJson, PageCrystallarieumGrowing::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("cinderhearth_smelting"), PageCinderhearthSmelting::fromJson, PageCinderhearthSmelting::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("titration_barrel_fermenting"), PageTitrationBarrelFermenting::fromJson, PageTitrationBarrelFermenting::fromNetwork);
+//
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("status_effect"), PageStatusEffect::fromJson, PageStatusEffect::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("hint"), PageHint::fromJson, PageHint::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("checklist"), PageChecklist::fromJson, PageChecklist::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("confirmation_button"), PageConfirmationButton::fromJson, PageConfirmationButton::fromNetwork);
+//        LoaderRegistry.registerPageLoader(SpectrumCommon.locate("snippet"), PageSnippet::fromJson, PageSnippet::fromNetwork);
+        LoaderRegistry.registerPageLoader(COLLECTION_PAGE, BookCollectionPage::fromJson, BookCollectionPage::fromNetwork);
     }
-    
+
     @Override
     public void registerClient() {
-        //PageRendererRegistry.registerPageRenderer(SpectrumCommon.locate("anvil_crushing"),  p -> new BookTextPageRenderer((BookTextPage) p) { });
+        PageRendererRegistry.registerPageRenderer(COLLECTION_PAGE, page -> page instanceof BookCollectionPage bookCollectionPage ? new BookCollectionPageRenderer(bookCollectionPage) : null);
     }
     
 }
---
+++ src/main/java/de/dafuqs/spectrum/compat/modonomicon/pages/BookCollectionPage.java
@@ -0,0 +1,99 @@
+package de.dafuqs.spectrum.compat.modonomicon.pages;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.klikli_dev.modonomicon.Modonomicon;
+import com.klikli_dev.modonomicon.book.BookEntry;
+import com.klikli_dev.modonomicon.book.BookTextHolder;
+import com.klikli_dev.modonomicon.book.page.BookTextPage;
+import com.klikli_dev.modonomicon.util.BookGsonHelper;
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import de.dafuqs.spectrum.compat.modonomicon.ModonomiconCompat;
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.command.argument.ItemStackArgument;
+import net.minecraft.command.argument.ItemStackArgumentType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.JsonHelper;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookCollectionPage extends BookTextPage {
+
+    private final List<String> itemStrings;
+    private final List<ItemStack> items;
+
+    public BookCollectionPage(BookTextHolder title, BookTextHolder text, boolean useMarkdownInTitle, boolean showTitleSeparator, String anchor, List<String> itemStrings) {
+        super(title, text, useMarkdownInTitle, showTitleSeparator, anchor);
+        this.itemStrings = itemStrings;
+        this.items = new ArrayList<>(itemStrings.size());
+    }
+
+    public static BookCollectionPage fromJson(JsonObject json) {
+        var title = BookGsonHelper.getAsBookTextHolder(json, "title", BookTextHolder.EMPTY);
+        var useMarkdownInTitle = JsonHelper.getBoolean(json, "use_markdown_title", false);
+        var showTitleSeparator = JsonHelper.getBoolean(json, "show_title_separator", true);
+        var text = BookGsonHelper.getAsBookTextHolder(json, "text", BookTextHolder.EMPTY);
+        var anchor = JsonHelper.getString(json, "anchor", "");
+        var items = JsonHelper.getArray(json, "items", new JsonArray()).asList().stream().map(JsonElement::toString).toList();
+        return new BookCollectionPage(title, text, useMarkdownInTitle, showTitleSeparator, anchor, items);
+    }
+
+    public static BookCollectionPage fromNetwork(PacketByteBuf buffer) {
+        var title = BookTextHolder.fromNetwork(buffer);
+        var useMarkdownInTitle = buffer.readBoolean();
+        var showTitleSeparator = buffer.readBoolean();
+        var text = BookTextHolder.fromNetwork(buffer);
+        var anchor = buffer.readString();
+        var items = buffer.readList(PacketByteBuf::readString);
+        return new BookCollectionPage(title, text, useMarkdownInTitle, showTitleSeparator, anchor, items);
+    }
+
+    public List<ItemStack> getItems() {
+        return this.items;
+    }
+
+    @Override
+    public Identifier getType() {
+        return ModonomiconCompat.COLLECTION_PAGE;
+    }
+
+    @Override
+    public void build(World world, BookEntry parentEntry, int pageNum) {
+        super.build(world, parentEntry, pageNum);
+
+        for (String itemString : itemStrings) {
+            try {
+                CommandRegistryAccess access = CommandRegistryAccess.of(world.getRegistryManager(), world.getEnabledFeatures());
+                ItemStackArgumentType argumentType = new ItemStackArgumentType(access);
+                ItemStackArgument argument = argumentType.parse(new StringReader(itemString));
+                items.add(argument.createStack(1, false));
+            } catch (CommandSyntaxException e) {
+                Modonomicon.LOG.warn("Unable to parse stack {} in collection page", itemString);
+            }
+        }
+    }
+
+    @Override
+    public void toNetwork(PacketByteBuf buffer) {
+        super.toNetwork(buffer);
+        buffer.writeCollection(this.itemStrings, PacketByteBuf::writeString);
+    }
+
+    @Override
+    public boolean matchesQuery(String query) {
+        return super.matchesQuery(query)
+                || items.stream()
+                .map(ItemStack::getTranslationKey)
+                .map(I18n::translate)
+                .map(String::toLowerCase)
+                .anyMatch(string -> string.contains(query));
+    }
+
+}

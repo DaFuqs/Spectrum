cc9ff8e6a551f983bb28981d523c66804f710e24
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-12-22 16:15:13 +0100

    fix #303

--- src/main/java/de/dafuqs/spectrum/mixin/EnchantmentMixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/EnchantmentMixin.java
@@ -9,12 +9,14 @@ import org.spongepowered.asm.mixin.injection.callback.*;
 
 @Mixin(Enchantment.class)
 public abstract class EnchantmentMixin {
-
+	
 	@Inject(method = "isAcceptableItem(Lnet/minecraft/item/ItemStack;)Z", at = @At("RETURN"), cancellable = true)
 	public void isAcceptableItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-		if (!cir.getReturnValue() || (stack.getItem() instanceof ExtendedEnchantable extendedEnchantable && extendedEnchantable.acceptsEnchantment((Enchantment) (Object) this))) {
-			cir.setReturnValue(true);
+		if (!cir.getReturnValue()) {
+			if ((stack.getItem() instanceof ExtendedEnchantable extendedEnchantable && extendedEnchantable.acceptsEnchantment((Enchantment) (Object) this))) {
+				cir.setReturnValue(true);
+			}
 		}
 	}
-
+	
 }

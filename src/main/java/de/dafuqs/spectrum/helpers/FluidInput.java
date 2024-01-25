71cd3317e7bbbf66c8ff007567d553a1bbb52c0a
imreallybadatnames <pizzagtav@gmail.com>
2024-01-25 13:25:03 +1100

    extras

--- src/main/java/de/dafuqs/spectrum/helpers/FluidInput.java
+++ src/main/java/de/dafuqs/spectrum/helpers/FluidInput.java
@@ -22,20 +22,32 @@ public class FluidInput {
 
     private final @Nullable Fluid fluid;
     private final @Nullable TagKey<Fluid> tag;
-    // compare against EMPTY to check if empty.
-    // in order to represent an empty value, specifically use this field
+    // Compare against EMPTY to check if empty.
+    // In order to represent an empty value, specifically use this field.
     public static FluidInput EMPTY = new FluidInput(null, null);
 
-    // can't be both fluid and tag, so ONLY use the provided of() methods
+    // Can't be both fluid and tag, so ONLY use the provided of() methods
     // NOTE: ALL FluidInput-related code assumes that either:
     // 1. there are always EITHER the fluid OR the fluid tag, NOT both
     // 2. the object is empty AND the object is EQUAL TO FluidInput.EMPTY.
-    // violation of either of those results in either an AssertionError or
+    // Violation of either of those results in either an AssertionError or
     // undefined behavior. As such, don't even allow creation of invalid obj.
     // FluidInput objects with unknown/invalid tag IDs are considered valid.
     private FluidInput(@Nullable Fluid fluid, @Nullable TagKey<Fluid> tag) {
         this.fluid = fluid;
         this.tag = tag;
+    }
+
+    // NOTE: This is for testing. Doesn't explicitly handle invalid FluidInput.
+    @Override
+    public String toString() {
+        if (this == EMPTY)
+            return "FluidInput.EMPTY";
+        if (this.fluid != null)
+            return String.format("FluidInput[fluid=%s]", this.fluid);
+        // must contain either or be FluidInput.EMPTY(as per FluidInput doc)
+        assert this.tag != null;
+        return String.format("FluidInput[tag=%s]", this.tag);
     }
 
     public static FluidInput of(@NotNull Fluid fluid) {
@@ -56,7 +68,8 @@ public class FluidInput {
     }
 
     public Identifier id() {
-        return fluid != null ? Registries.FLUID.getId(fluid) : tag != null ? tag.id() : null;
+        return fluid != null ? Registries.FLUID.getId(fluid)
+                             : tag != null ? tag.id() : null;
     }
 
     // Vanilla-friendly compatibility method.
@@ -67,6 +80,8 @@ public class FluidInput {
             return Ingredient.ofStacks(this.fluid.getBucketItem()
                                                  .getDefaultStack());
         if (this.tag != null) {
+            // Handle custom fluid registries
+            // in the case of FluidInput objects created by other mods.
             Registry<Fluid> registry = RegistryHelper.getRegistryOf(this.tag);
             if(registry == null) return Ingredient.empty();
             Optional<RegistryEntryList.Named<Fluid>> optional =

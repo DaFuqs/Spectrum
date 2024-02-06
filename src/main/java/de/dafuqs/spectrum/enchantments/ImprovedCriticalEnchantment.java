c7a7f850e26775f0f9cfb6c27ae518e2d62ba9b3
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-02-06 18:19:28 +0100

    use statics for temporary attributes

--- src/main/java/de/dafuqs/spectrum/enchantments/ImprovedCriticalEnchantment.java
+++ src/main/java/de/dafuqs/spectrum/enchantments/ImprovedCriticalEnchantment.java
@@ -11,6 +11,7 @@ import java.util.*;
 public class ImprovedCriticalEnchantment extends SpectrumEnchantment {
 	
 	public static final UUID EXTRA_CRIT_DAMAGE_MULTIPLIER_ATTRIBUTE_UUID = UUID.fromString("e9bca8d4-9dcb-4e9e-8a7b-48b129c7ec5a");
+	public static final String EXTRA_CRIT_DAMAGE_MULTIPLIER_ATTRIBUTE_NAME = "spectrum:improved_critical";
 	
 	public ImprovedCriticalEnchantment(Rarity weight, Identifier unlockAdvancementIdentifier, EquipmentSlot... slotTypes) {
 		super(weight, EnchantmentTarget.WEAPON, slotTypes, unlockAdvancementIdentifier);

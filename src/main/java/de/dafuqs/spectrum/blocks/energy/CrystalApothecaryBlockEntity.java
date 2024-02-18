e3bdc03e751ff30776d84f7bc4170b9f4208a952
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-02-18 14:50:21 +0100

    Rename Title of Crystal Apothecary to "XXX's Crystal Apothecary" (Eset)

--- src/main/java/de/dafuqs/spectrum/blocks/energy/CrystalApothecaryBlockEntity.java
+++ src/main/java/de/dafuqs/spectrum/blocks/energy/CrystalApothecaryBlockEntity.java
@@ -126,7 +126,7 @@ public class CrystalApothecaryBlockEntity extends LootableContainerBlockEntity i
 	@Override
 	protected Text getContainerName() {
 		if (hasOwner()) {
-			return Text.translatable("block.spectrum.crystal_apothecary").append(Text.translatable("container.spectrum.owned_by_player", this.ownerName));
+			return Text.translatable("block.spectrum.crystal_apothecary.owner", this.ownerName);
 		} else {
 			return Text.translatable("block.spectrum.crystal_apothecary");
 		}

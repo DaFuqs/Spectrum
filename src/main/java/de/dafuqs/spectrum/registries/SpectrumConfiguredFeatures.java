64d5362c322bb0dfabad219f2a1593b16951f422
Isegrim <dominik.m.schmidt@googlemail.com>
2024-03-12 22:14:17 +0100

    Fixed Clover bonemealing not always working (unilock)

--- src/main/java/de/dafuqs/spectrum/registries/SpectrumConfiguredFeatures.java
+++ src/main/java/de/dafuqs/spectrum/registries/SpectrumConfiguredFeatures.java
@@ -6,6 +6,7 @@ import net.minecraft.world.gen.feature.*;
 
 public class SpectrumConfiguredFeatures {
 
+	public static final RegistryKey<ConfiguredFeature<?, ?>> CLOVER_PATCH = of("clover_patch");
 	public static final RegistryKey<ConfiguredFeature<?,?>> SNAPPING_IVY_PATCH = of("snapping_ivy_patch");
 	public static final RegistryKey<ConfiguredFeature<?,?>> JADEITE_LOTUS = of("jadeite_lotus");
 	public static final RegistryKey<ConfiguredFeature<?,?>> NEPHRITE_BLOSSOM_BULB = of("nephrite_blossom");

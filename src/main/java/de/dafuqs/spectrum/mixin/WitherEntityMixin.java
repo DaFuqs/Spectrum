9586bcc91711988bcbc2b06445c10af9df05babe
Isegrim <dominik.m.schmidt@googlemail.com>
2024-03-13 22:19:46 +0100

    Fix #360: crash when Wither is killed by a non-entity

--- src/main/java/de/dafuqs/spectrum/mixin/WitherEntityMixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/WitherEntityMixin.java
@@ -5,6 +5,7 @@ import de.dafuqs.spectrum.registries.*;
 import net.minecraft.entity.*;
 import net.minecraft.entity.boss.*;
 import net.minecraft.entity.damage.*;
+import net.minecraft.world.*;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.*;
@@ -18,9 +19,10 @@ public abstract class WitherEntityMixin {
 	private void spawnEntity(DamageSource source, int lootingMultiplier, boolean allowDrops, CallbackInfo ci, ItemEntity itemEntity) {
 		Entity attackerEntity = source.getAttacker();
 		if (attackerEntity instanceof LivingEntity livingAttacker) {
+			World world = attackerEntity.getWorld();
 			int cloversFavorLevel = SpectrumEnchantmentHelper.getUsableLevel(SpectrumEnchantments.CLOVERS_FAVOR, livingAttacker.getMainHandStack(), livingAttacker);
 			if (cloversFavorLevel > 0) {
-				int additionalCount = (int) (cloversFavorLevel / 2.0F + ((WitherEntity) (Object) this).getWorld().random.nextFloat() * cloversFavorLevel);
+				int additionalCount = (int) (cloversFavorLevel / 2.0F + world.random.nextFloat() * cloversFavorLevel);
 				itemEntity.getStack().setCount(itemEntity.getStack().getCount() + additionalCount);
 			}
 		}

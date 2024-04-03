b63fe18acab1f31719af6b748b3a429200b9ad57
Isegrim <dominik.m.schmidt@googlemail.com>
2024-04-03 17:35:05 +0200

    added kindling variants

---
+++ src/main/java/de/dafuqs/spectrum/entity/variants/KindlingVariant.java
@@ -0,0 +1,20 @@
+package de.dafuqs.spectrum.entity.variants;
+
+import de.dafuqs.spectrum.*;
+import de.dafuqs.spectrum.registries.*;
+import net.minecraft.registry.*;
+import net.minecraft.util.*;
+
+public record KindlingVariant(Identifier defaultTexture, Identifier blinkingTexture, Identifier angryTexture,
+							  Identifier clippedTexture, Identifier blinkingClippedTexture,
+							  Identifier angryClippedTexture) {
+
+	public static final KindlingVariant DEFAULT = register("default", "textures/entity/kindling/kindling.png", "textures/entity/kindling/kindling_blink.png", "textures/entity/kindling/kindling_angry.png", "textures/entity/kindling/kindling_clipped.png", "textures/entity/kindling/kindling_blink_clipped.png", "textures/entity/kindling/kindling_angry_clipped.png");
+
+	private static KindlingVariant register(String name, String defaultTexture, String blinkingTexture, String angryTexture, String clippedTexture, String blinkingClippedTexture, String angryClippedTexture) {
+		return Registry.register(SpectrumRegistries.KINDLING_VARIANT, SpectrumCommon.locate(name), new KindlingVariant(SpectrumCommon.locate(defaultTexture), SpectrumCommon.locate(blinkingTexture), SpectrumCommon.locate(angryTexture), SpectrumCommon.locate(clippedTexture), SpectrumCommon.locate(blinkingClippedTexture), SpectrumCommon.locate(angryClippedTexture)));
+	}
+
+	public static void init() {
+	}
+}
--- src/main/java/de/dafuqs/spectrum/registries/SpectrumRegistries.java
+++ src/main/java/de/dafuqs/spectrum/registries/SpectrumRegistries.java
@@ -6,10 +6,11 @@ import de.dafuqs.spectrum.entity.variants.*;
 import de.dafuqs.spectrum.explosion.*;
 import de.dafuqs.spectrum.items.tools.*;
 import net.fabricmc.fabric.api.event.registry.*;
-import net.minecraft.tag.*;
+import net.minecraft.registry.*;
+import net.minecraft.registry.entry.*;
+import net.minecraft.registry.tag.*;
 import net.minecraft.util.*;
 import net.minecraft.util.math.random.Random;
-import net.minecraft.util.registry.*;
 
 import java.util.*;
 
@@ -18,20 +19,30 @@ public class SpectrumRegistries {
 	private static final Identifier INK_COLORS_ID = SpectrumCommon.locate("ink_color");
 	public static final RegistryKey<Registry<InkColor>> INK_COLORS_KEY = RegistryKey.ofRegistry(INK_COLORS_ID);
 	public static final Registry<InkColor> INK_COLORS = FabricRegistryBuilder.createSimple(InkColor.class, INK_COLORS_ID).attribute(RegistryAttribute.SYNCED).buildAndRegister();
-	
+
 	private static final Identifier LIZARD_FRILL_VARIANT_ID = SpectrumCommon.locate("lizard_frill_variant");
 	private static final Identifier LIZARD_HORN_VARIANT_ID = SpectrumCommon.locate("lizard_horn_variant");
-	
+
 	public static final RegistryKey<Registry<LizardFrillVariant>> LIZARD_FRILL_VARIANT_KEY = RegistryKey.ofRegistry(LIZARD_FRILL_VARIANT_ID);
 	public static final RegistryKey<Registry<LizardHornVariant>> LIZARD_HORN_VARIANT_KEY = RegistryKey.ofRegistry(LIZARD_HORN_VARIANT_ID);
-	
-	public static final Registry<LizardFrillVariant> LIZARD_FRILL_VARIANT = FabricRegistryBuilder.createSimple(LizardFrillVariant.class, LIZARD_FRILL_VARIANT_ID).attribute(RegistryAttribute.SYNCED).buildAndRegister();
-	public static final Registry<LizardHornVariant> LIZARD_HORN_VARIANT = FabricRegistryBuilder.createSimple(LizardHornVariant.class, LIZARD_HORN_VARIANT_ID).attribute(RegistryAttribute.SYNCED).buildAndRegister();
-	
-	public static final Registry<GlassArrowVariant> GLASS_ARROW_VARIANT = FabricRegistryBuilder.createSimple(GlassArrowVariant.class, SpectrumCommon.locate("glass_arrow_variant")).attribute(RegistryAttribute.SYNCED).buildAndRegister();
-	public static final Registry<ExplosionModifierType> EXPLOSION_MODIFIER_TYPES = FabricRegistryBuilder.createSimple(ExplosionModifierType.class, SpectrumCommon.locate("explosion_effect_family")).attribute(RegistryAttribute.SYNCED).buildAndRegister();
-	public static final Registry<ExplosionModifier> EXPLOSION_MODIFIERS = FabricRegistryBuilder.createSimple(ExplosionModifier.class, SpectrumCommon.locate("explosion_effect_modifier")).attribute(RegistryAttribute.SYNCED).buildAndRegister();
-	
+	public static final Registry<LizardHornVariant> LIZARD_HORN_VARIANT = FabricRegistryBuilder.createSimple(LIZARD_HORN_VARIANT_KEY).attribute(RegistryAttribute.SYNCED).buildAndRegister();
+
+	private static final Identifier KINDLING_VARIANT_ID = SpectrumCommon.locate("kindling_variant");
+	public static final RegistryKey<Registry<KindlingVariant>> KINDLING_VARIANT_KEY = RegistryKey.ofRegistry(KINDLING_VARIANT_ID);
+	public static final Registry<KindlingVariant> KINDLING_VARIANT = FabricRegistryBuilder.createSimple(KINDLING_VARIANT_KEY).attribute(RegistryAttribute.SYNCED).buildAndRegister();
+
+	private static final Identifier GLASS_ARROW_VARIANT_ID = SpectrumCommon.locate("glass_arrow_variant");
+	public static final RegistryKey<Registry<GlassArrowVariant>> GLASS_ARROW_VARIANT_KEY = RegistryKey.ofRegistry(GLASS_ARROW_VARIANT_ID);
+	public static final Registry<GlassArrowVariant> GLASS_ARROW_VARIANT = FabricRegistryBuilder.createSimple(GLASS_ARROW_VARIANT_KEY).attribute(RegistryAttribute.SYNCED).buildAndRegister();
+
+	private static final Identifier EXPLOSION_MODIFIER_TYPES_ID = SpectrumCommon.locate("explosion_effect_family");
+	public static final RegistryKey<Registry<ExplosionModifierType>> EXPLOSION_MODIFIER_TYPES_KEY = RegistryKey.ofRegistry(EXPLOSION_MODIFIER_TYPES_ID);
+	public static final Registry<ExplosionModifierType> EXPLOSION_MODIFIER_TYPES = FabricRegistryBuilder.createSimple(EXPLOSION_MODIFIER_TYPES_KEY).attribute(RegistryAttribute.SYNCED).buildAndRegister();
+
+	private static final Identifier EXPLOSION_MODIFIERS_ID = SpectrumCommon.locate("explosion_effect_modifier");
+	public static final RegistryKey<Registry<ExplosionModifier>> EXPLOSION_MODIFIERS_KEY = RegistryKey.ofRegistry(EXPLOSION_MODIFIERS_ID);
+	public static final Registry<ExplosionModifier> EXPLOSION_MODIFIERS = FabricRegistryBuilder.createSimple(EXPLOSION_MODIFIERS_KEY).attribute(RegistryAttribute.SYNCED).buildAndRegister();
+
 	public static <T> T getRandomTagEntry(Registry<T> registry, TagKey<T> tag, Random random, T fallback) {
 		Optional<RegistryEntryList.Named<T>> tagEntries = registry.getEntryList(tag);
 		if (tagEntries.isPresent()) {
@@ -45,11 +56,11 @@ public class SpectrumRegistries {
 		Optional<RegistryEntryList.Named<T>> tagEntries = registry.getEntryList(tag);
 		return tagEntries.map(registryEntries -> registryEntries.stream().toList()).orElseGet(List::of);
 	}
-	
+
 	public static void register() {
 		LizardFrillVariant.init();
 		LizardHornVariant.init();
-		
+		KindlingVariant.init();
 		GlassArrowVariant.init();
 	}
 	

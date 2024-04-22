f923526f6014c08a2f4905650a8412c388a4011c
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-04-22 20:31:26 +0200

    update colors, including wiki

--- src/main/java/de/dafuqs/spectrum/api/energy/color/InkColors.java
+++ src/main/java/de/dafuqs/spectrum/api/energy/color/InkColors.java
@@ -1,11 +1,10 @@
 package de.dafuqs.spectrum.api.energy.color;
 
 import de.dafuqs.spectrum.*;
-import de.dafuqs.spectrum.helpers.ColorHelper;
+import de.dafuqs.spectrum.helpers.*;
 import de.dafuqs.spectrum.registries.*;
 import net.minecraft.registry.*;
 import net.minecraft.util.*;
-import org.joml.*;
 
 import java.util.*;
 
@@ -22,9 +21,9 @@ public class InkColors {
 	 */
 	public static final int ALT_BLACK = 0x302951;
 	
-	public static final ElementalColor CYAN = registerElemental("cyan", new ElementalColor(DyeColor.CYAN, ColorHelper.colorIntToVec(0x45ecff), BASE_ADVANCEMENT_ID));
+	public static final ElementalColor CYAN = registerElemental("cyan", new ElementalColor(DyeColor.CYAN, ColorHelper.colorIntToVec(0x5bffed), BASE_ADVANCEMENT_ID));
 	public static final ElementalColor MAGENTA = registerElemental("magenta", new ElementalColor(DyeColor.MAGENTA, ColorHelper.colorIntToVec(0xff4ff6), BASE_ADVANCEMENT_ID));
-	public static final ElementalColor YELLOW = registerElemental("yellow", new ElementalColor(DyeColor.YELLOW, ColorHelper.colorIntToVec(0xffe736), BASE_ADVANCEMENT_ID));
+	public static final ElementalColor YELLOW = registerElemental("yellow", new ElementalColor(DyeColor.YELLOW, ColorHelper.colorIntToVec(0xeded00), BASE_ADVANCEMENT_ID));
 	public static final ElementalColor BLACK = registerElemental("black", new ElementalColor(DyeColor.BLACK, ColorHelper.colorIntToVec(0x020106), BLACK_ADVANCEMENT_ID, true));
 	public static final ElementalColor WHITE = registerElemental("white", new ElementalColor(DyeColor.WHITE, ColorHelper.colorIntToVec(0xFFFFFF), WHITE_ADVANCEMENT_ID));
 
@@ -32,11 +31,11 @@ public class InkColors {
 		put(MAGENTA, 1F / 3F);
 		put(YELLOW, 2F / 3F);
 	}}));
-	public static final CompoundColor LIME = registerCompound("lime", new CompoundColor(DyeColor.LIME, ColorHelper.colorIntToVec(0x92ff2b), BASE_ADVANCEMENT_ID, new HashMap<>() {{
+	public static final CompoundColor LIME = registerCompound("lime", new CompoundColor(DyeColor.LIME, ColorHelper.colorIntToVec(0x98ff37), BASE_ADVANCEMENT_ID, new HashMap<>() {{
 		put(CYAN, 1F / 3F);
 		put(YELLOW, 2F / 3F);
 	}}));
-	public static final CompoundColor PINK = registerCompound("pink", new CompoundColor(DyeColor.PINK, ColorHelper.colorIntToVec(0xff94a6), BASE_ADVANCEMENT_ID, new HashMap<>() {{
+	public static final CompoundColor PINK = registerCompound("pink", new CompoundColor(DyeColor.PINK, ColorHelper.colorIntToVec(0xff9fc6), BASE_ADVANCEMENT_ID, new HashMap<>() {{
 		put(MAGENTA, 2F / 3F);
 		put(YELLOW, 1F / 3F);
 	}}));
@@ -48,7 +47,7 @@ public class InkColors {
 		put(CYAN, 3F / 4F);
 		put(MAGENTA, 1F / 4F);
 	}}));
-	public static final CompoundColor GREEN = registerCompound("green", new CompoundColor(DyeColor.GREEN, ColorHelper.colorIntToVec(0x00b76b), BASE_ADVANCEMENT_ID, new HashMap<>() {{
+	public static final CompoundColor GREEN = registerCompound("green", new CompoundColor(DyeColor.GREEN, ColorHelper.colorIntToVec(0x526b0f), BASE_ADVANCEMENT_ID, new HashMap<>() {{
 		put(CYAN, 2F / 5F);
 		put(MAGENTA, 1F / 5F);
 		put(YELLOW, 2F / 5F);
@@ -58,17 +57,17 @@ public class InkColors {
 		put(MAGENTA, 2F / 6F);
 		put(YELLOW, 1F / 6F);
 	}}));
-	public static final CompoundColor PURPLE = registerCompound("purple", new CompoundColor(DyeColor.PURPLE, ColorHelper.colorIntToVec(0x952bff), BASE_ADVANCEMENT_ID, new HashMap<>() {{
+	public static final CompoundColor PURPLE = registerCompound("purple", new CompoundColor(DyeColor.PURPLE, ColorHelper.colorIntToVec(0x802bc4), BASE_ADVANCEMENT_ID, new HashMap<>() {{
 		put(CYAN, 2F / 6F);
 		put(MAGENTA, 3F / 6F);
 		put(YELLOW, 1F / 6F);
 	}}));
-	public static final CompoundColor BROWN = registerCompound("brown", new CompoundColor(DyeColor.BROWN, ColorHelper.colorIntToVec(0x6f322a), BLACK_ADVANCEMENT_ID, new HashMap<>() {{
+	public static final CompoundColor BROWN = registerCompound("brown", new CompoundColor(DyeColor.BROWN, ColorHelper.colorIntToVec(0x70400d), BLACK_ADVANCEMENT_ID, new HashMap<>() {{
 		put(MAGENTA, 2F / 5F);
 		put(YELLOW, 1F / 5F);
 		put(BLACK, 2F / 5F);
 	}}, true));
-	public static final CompoundColor LIGHT_GRAY = registerCompound("light_gray", new CompoundColor(DyeColor.LIGHT_GRAY, ColorHelper.colorIntToVec(0xc2c2c2), WHITE_ADVANCEMENT_ID, new HashMap<>() {{
+	public static final CompoundColor LIGHT_GRAY = registerCompound("light_gray", new CompoundColor(DyeColor.LIGHT_GRAY, ColorHelper.colorIntToVec(0xadadad), WHITE_ADVANCEMENT_ID, new HashMap<>() {{
 		put(CYAN, 2F / 6F);
 		put(MAGENTA, 2F / 6F);
 		put(YELLOW, 2F / 6F);

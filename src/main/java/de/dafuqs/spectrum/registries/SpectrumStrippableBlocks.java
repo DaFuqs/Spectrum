560295bab9d202e609aecd7a9940b1199aede091
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-07-27 19:14:39 +0200

    noxcap hyphae can be stripped

--- src/main/java/de/dafuqs/spectrum/registries/SpectrumStrippableBlocks.java
+++ src/main/java/de/dafuqs/spectrum/registries/SpectrumStrippableBlocks.java
@@ -12,7 +12,13 @@ public class SpectrumStrippableBlocks {
 		StrippableBlockRegistry.register(SpectrumBlocks.EBONY_NOXCAP_STEM, SpectrumBlocks.STRIPPED_EBONY_NOXCAP_STEM);
 		StrippableBlockRegistry.register(SpectrumBlocks.IVORY_NOXCAP_STEM, SpectrumBlocks.STRIPPED_IVORY_NOXCAP_STEM);
 		StrippableBlockRegistry.register(SpectrumBlocks.CHESTNUT_NOXCAP_STEM, SpectrumBlocks.STRIPPED_CHESTNUT_NOXCAP_STEM);
-		StrippableBlockRegistry.register(SpectrumBlocks.GALA_LOG, SpectrumBlocks.STRIPPED_GALA_LOG);
+		StrippableBlockRegistry.register(SpectrumBlocks.SLATE_NOXCAP_HYPHAE, SpectrumBlocks.STRIPPED_SLATE_NOXCAP_HYPHAE);
+		StrippableBlockRegistry.register(SpectrumBlocks.EBONY_NOXCAP_HYPHAE, SpectrumBlocks.STRIPPED_EBONY_NOXCAP_HYPHAE);
+		StrippableBlockRegistry.register(SpectrumBlocks.IVORY_NOXCAP_HYPHAE, SpectrumBlocks.STRIPPED_IVORY_NOXCAP_HYPHAE);
+		StrippableBlockRegistry.register(SpectrumBlocks.CHESTNUT_NOXCAP_HYPHAE, SpectrumBlocks.STRIPPED_CHESTNUT_NOXCAP_HYPHAE);
+		
+		StrippableBlockRegistry.register(SpectrumBlocks.WEEPING_GALA_LOG, SpectrumBlocks.STRIPPED_WEEPING_GALA_LOG);
+		StrippableBlockRegistry.register(SpectrumBlocks.WEEPING_GALA_WOOD, SpectrumBlocks.STRIPPED_WEEPING_GALA_WOOD);
 		
 		for (DyeColor dyeColor : ColorHelper.VANILLA_DYE_COLORS) {
 			StrippableBlockRegistry.register(ColoredLogBlock.byColor(dyeColor), ColoredStrippedLogBlock.byColor(dyeColor));

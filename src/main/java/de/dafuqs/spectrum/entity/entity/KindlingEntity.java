23c5654ce2f3f74ec758c3e7c7f66679a97a4a9c
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-09-05 16:11:09 +0200

    improved dragonbone sequence breaking (Kindling feathers would not reveal before endgame before)

--- src/main/java/de/dafuqs/spectrum/entity/entity/KindlingEntity.java
+++ src/main/java/de/dafuqs/spectrum/entity/entity/KindlingEntity.java
@@ -1,15 +1,14 @@
 package de.dafuqs.spectrum.entity.entity;
 
-import de.dafuqs.additionalentityattributes.AdditionalEntityAttributes;
-import de.dafuqs.spectrum.compat.claims.GenericClaimModsCompat;
+import de.dafuqs.additionalentityattributes.*;
+import de.dafuqs.spectrum.compat.claims.*;
 import de.dafuqs.spectrum.entity.*;
 import de.dafuqs.spectrum.entity.variants.*;
 import de.dafuqs.spectrum.helpers.Support;
 import de.dafuqs.spectrum.mixin.accessors.*;
 import de.dafuqs.spectrum.registries.*;
 import net.fabricmc.fabric.api.tag.convention.v1.*;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.ai.goal.*;
@@ -28,17 +27,16 @@ import net.minecraft.loot.context.*;
 import net.minecraft.nbt.*;
 import net.minecraft.particle.*;
 import net.minecraft.recipe.*;
-import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.registry.tag.FluidTags;
+import net.minecraft.registry.tag.*;
 import net.minecraft.server.world.*;
 import net.minecraft.sound.*;
-import net.minecraft.text.Text;
+import net.minecraft.util.TimeHelper;
 import net.minecraft.util.*;
 import net.minecraft.util.math.*;
 import net.minecraft.util.math.intprovider.*;
 import net.minecraft.world.*;
 import net.minecraft.world.event.*;
-import net.minecraft.world.explosion.Explosion;
+import net.minecraft.world.explosion.*;
 import org.jetbrains.annotations.*;
 
 import java.util.*;
@@ -382,7 +380,7 @@ public class KindlingEntity extends AbstractHorseEntity implements RangedAttackM
 		playSound(SoundEvents.ENTITY_ENDER_DRAGON_AMBIENT, 1F, 2F);
 
 		((ServerWorld) world).getPlayers(p -> p.distanceTo(this) < 64).forEach( p -> {
-			Support.grantAdvancementCriterion(p, "lategame/ascend_kindling", "he_explarded");
+			Support.grantAdvancementCriterion(p, "ascend_kindling", "he_explarded");
 		});
 
 		for (int i = 0; i < 5; i++) {

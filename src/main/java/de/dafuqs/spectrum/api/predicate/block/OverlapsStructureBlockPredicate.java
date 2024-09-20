797f5e494a121c75a650a9e0af4b8badefbaf216
f-raZ0R <MonkeyCityOwner@gmail.com>
2024-09-20 14:22:10 -0400

    pointless machinations

--- src/main/java/de/dafuqs/spectrum/api/predicate/block/OverlapsStructureBlockPredicate.java
+++ src/main/java/de/dafuqs/spectrum/api/predicate/block/OverlapsStructureBlockPredicate.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.StructurePiece;
 import net.minecraft.structure.StructureStart;
 import net.minecraft.util.math.BlockBox;
@@ -11,6 +12,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.StructureWorldAccess;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.gen.blockpredicate.BlockPredicate;
 import net.minecraft.world.gen.blockpredicate.BlockPredicateType;
@@ -19,6 +21,7 @@ import de.dafuqs.spectrum.registries.SpectrumBlockPredicates;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -41,16 +44,63 @@ public class OverlapsStructureBlockPredicate implements BlockPredicate {
     }
 
     public boolean test(StructureWorldAccess structureWorldAccess, BlockPos blockPos) {
-        BlockBox exclusionZone = new BlockBox(blockPos.add(this.offset).getX() - this.range, blockPos.add(this.offset).getY() - this.range, blockPos.add(this.offset).getZ() - this.range,blockPos.add(this.offset).getX() + this.range, blockPos.add(this.offset).getY() + this.range, blockPos.add(this.offset).getZ() + this.range);
-        Map<Structure, StructureStart> structureMap = structureWorldAccess.toServerWorld().getChunk(ChunkSectionPos.getSectionCoord(blockPos.add(this.offset).getX()), ChunkSectionPos.getSectionCoord(blockPos.add(this.offset).getZ()), ChunkStatus.STRUCTURE_STARTS).getStructureStarts();
-        Structure targetStruct = null;
-        if(this.structure.isPresent())
+        ServerWorld serverWorld = structureWorldAccess.toServerWorld();
+        BlockPos blockPosOffset = blockPos.add(this.offset);
+        Map<Structure, StructureStart> structureMap = new HashMap<>();
+        Chunk centerChunk = serverWorld.getChunk(ChunkSectionPos.getSectionCoord(blockPosOffset.getX()), ChunkSectionPos.getSectionCoord(blockPosOffset.getZ()), ChunkStatus.EMPTY, false);
+        if(centerChunk!=null && centerChunk.getStatus().isAtLeast(ChunkStatus.STRUCTURE_STARTS))
         {
-            targetStruct = structureWorldAccess.toServerWorld().getStructureAccessor().getRegistryManager().get(RegistryKeys.STRUCTURE).get(this.structure.get());
+            structureMap.putAll(centerChunk.getStructureStarts());
         }
+        Chunk westChunk = serverWorld.getChunk(ChunkSectionPos.getSectionCoord(blockPosOffset.getX() - 16), ChunkSectionPos.getSectionCoord(blockPosOffset.getZ()), ChunkStatus.EMPTY, false);
+        if(westChunk!=null && westChunk.getStatus().isAtLeast(ChunkStatus.STRUCTURE_STARTS))
+        {
+            structureMap.putAll(westChunk.getStructureStarts());
+        }
+        Chunk northWestChunk = serverWorld.getChunk(ChunkSectionPos.getSectionCoord(blockPosOffset.getX() - 16), ChunkSectionPos.getSectionCoord(blockPosOffset.getZ() - 16), ChunkStatus.EMPTY, false);
+        if(northWestChunk!=null && northWestChunk.getStatus().isAtLeast(ChunkStatus.STRUCTURE_STARTS))
+        {
+            structureMap.putAll(northWestChunk.getStructureStarts());
+        }
+        Chunk northChunk = serverWorld.getChunk(ChunkSectionPos.getSectionCoord(blockPosOffset.getX()), ChunkSectionPos.getSectionCoord(blockPosOffset.getZ() - 16), ChunkStatus.EMPTY, false);
+        if(northChunk!=null && northChunk.getStatus().isAtLeast(ChunkStatus.STRUCTURE_STARTS))
+        {
+            structureMap.putAll(northChunk.getStructureStarts());
+        }
+        Chunk northEastChunk = serverWorld.getChunk(ChunkSectionPos.getSectionCoord(blockPosOffset.getX() + 16), ChunkSectionPos.getSectionCoord(blockPosOffset.getZ() - 16), ChunkStatus.EMPTY, false);
+        if(northEastChunk!=null && northEastChunk.getStatus().isAtLeast(ChunkStatus.STRUCTURE_STARTS))
+        {
+            structureMap.putAll(northEastChunk.getStructureStarts());
+        }
+        Chunk eastChunk = serverWorld.getChunk(ChunkSectionPos.getSectionCoord(blockPosOffset.getX() + 16), ChunkSectionPos.getSectionCoord(blockPosOffset.getZ()), ChunkStatus.EMPTY, false);
+        if(eastChunk!=null && eastChunk.getStatus().isAtLeast(ChunkStatus.STRUCTURE_STARTS))
+        {
+            structureMap.putAll(eastChunk.getStructureStarts());
+        }
+        Chunk southEastChunk = serverWorld.getChunk(ChunkSectionPos.getSectionCoord(blockPosOffset.getX() + 16), ChunkSectionPos.getSectionCoord(blockPosOffset.getZ() + 16), ChunkStatus.EMPTY, false);
+        if(southEastChunk!=null && southEastChunk.getStatus().isAtLeast(ChunkStatus.STRUCTURE_STARTS))
+        {
+            structureMap.putAll(southEastChunk.getStructureStarts());
+        }
+        Chunk southChunk = serverWorld.getChunk(ChunkSectionPos.getSectionCoord(blockPosOffset.getX()), ChunkSectionPos.getSectionCoord(blockPosOffset.getZ() + 16), ChunkStatus.EMPTY, false);
+        if(southChunk!=null && southChunk.getStatus().isAtLeast(ChunkStatus.STRUCTURE_STARTS))
+        {
+            structureMap.putAll(southChunk.getStructureStarts());
+        }
+        Chunk southWestChunk = serverWorld.getChunk(ChunkSectionPos.getSectionCoord(blockPosOffset.getX() - 16), ChunkSectionPos.getSectionCoord(blockPosOffset.getZ() + 16), ChunkStatus.EMPTY, false);
+        if(southWestChunk!=null && southWestChunk.getStatus().isAtLeast(ChunkStatus.STRUCTURE_STARTS))
+        {
+            structureMap.putAll(southWestChunk.getStructureStarts());
+        }
+        Structure targetStruct = null;
         if(structureMap.isEmpty())
         {
             return false;
+        }
+        BlockBox exclusionZone = new BlockBox(blockPosOffset.getX() - this.range, blockPosOffset.getY() - this.range, blockPosOffset.getZ() - this.range,blockPosOffset.getX() + this.range, blockPosOffset.getY() + this.range, blockPosOffset.getZ() + this.range);
+        if(this.structure.isPresent())
+        {
+            targetStruct = serverWorld.getStructureAccessor().getRegistryManager().get(RegistryKeys.STRUCTURE).get(this.structure.get());
         }
         for(var struct : structureMap.entrySet())
         {
@@ -61,12 +111,9 @@ public class OverlapsStructureBlockPredicate implements BlockPredicate {
                     continue;
                 }
             }
-            if(!struct.getValue().hasChildren())
+            if(struct.getValue().getBoundingBox().intersects(exclusionZone))
             {
-                if(struct.getValue().getBoundingBox().intersects(exclusionZone))
-                {
-                    return true;
-                }
+                return true;
             }
             else
             {

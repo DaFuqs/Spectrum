6f1787a6f85b8c590dd9a2b590d67dd2d3bfe5af
Noaaan <noaaan@hotmail.com>
2024-09-21 15:19:36 +0200

    Final adjustments

--- src/main/java/de/dafuqs/spectrum/render/armor/BedrockArmorModel.java
+++ src/main/java/de/dafuqs/spectrum/render/armor/BedrockArmorModel.java
@@ -25,21 +25,21 @@ public class BedrockArmorModel extends BipedEntityModel<LivingEntity> {
         root.addChild("hat", ModelPartBuilder.create(), ModelTransform.NONE);
 
         var head = root.addChild("head", ModelPartBuilder.create()
-                .uv(0, 0).cuboid(-4.5F, -8.5F, -4.5F, 9.0F, 9.0F, 9.0F, new Dilation(.5f)), ModelTransform.NONE);
+                .uv(0, 0).cuboid(-4.5F, -8.625F, -4.5F, 9.0F, 9.0F, 9.0F, new Dilation(.3f)), ModelTransform.NONE);
 
         var head_plume = head.addChild("head_plume", ModelPartBuilder.create()
                 .uv(38, 61).cuboid(0.0F, -0.0806F, 0.1517F, 0.0F, 11.0F, 7.0F, Dilation.NONE), ModelTransform.of(0.0F, -11.25F, 2.0F, 0.4363F, 0.0F, 0.0F));
 
         var visor = head.addChild("visor", ModelPartBuilder.create()
                 .uv(30, 11)
-            .cuboid(-5.0F, -3.0F, -6.8F, 10.0F, 5.0F, 7.0F, Dilation.NONE),
+            .cuboid(-5.0F, -3.0F, -6.8F, 10.0F, 5.0F, 7.0F, new Dilation(0.125f)),
             ModelTransform.of(0.0F, -6.0F, 1.5F, -0.1745F, 0.0F, 0.0F));
 
         var visor_frill_left = visor.addChild("visor_frill_left", ModelPartBuilder.create()
-                .uv(38, 14).cuboid(1.25F, -4.0F, -4.5F, 0.0F, 8.0F, 9.0F, Dilation.NONE), ModelTransform.of(5.1F, -2.0F, 1.2F, 0.5796F, 0.2344F, 0.1939F));
+                .uv(38, 14).cuboid(1.25F, -4.0F, -4.5F, 0.0F, 8.0F, 9.0F, new Dilation(0, 0.125f, 0.125f)), ModelTransform.of(5.1F, -2.0F, 1.2F, 0.5796F, 0.2344F, 0.1939F));
 
         var visor_frill_right = visor.addChild("visor_frill_right", ModelPartBuilder.create()
-                .uv(56, 14).cuboid(-1.25F, -4.0F, -4.5F, 0.0F, 8.0F, 9.0F, Dilation.NONE), ModelTransform.of(-5.1F, -2.0F, 1.2F, 0.5796F, -0.2344F, -0.1939F));
+                .uv(56, 14).cuboid(-1.25F, -4.0F, -4.5F, 0.0F, 8.0F, 9.0F, new Dilation(0, 0.125f, 0.125f)), ModelTransform.of(-5.1F, -2.0F, 1.2F, 0.5796F, -0.2344F, -0.1939F));
 
         var body = root.addChild("body", ModelPartBuilder.create()
                 .uv(0, 31)
@@ -53,8 +53,8 @@ public class BedrockArmorModel extends BipedEntityModel<LivingEntity> {
             .cuboid(-5.5F, -3.5F, -1.25F, 12.0F, 5.0F, 5.0F, Dilation.NONE), ModelTransform.of(-0.5F, 1.5F, 2.5F, -0.7854F, 0.0F, 0.0F));
 
         var body_collar = body.addChild("body_collar", ModelPartBuilder.create()
-                .uv(0, 83).cuboid(-4.5F, -1.5F, -4.25F, 9.0F, 5.0F, 7.0F, Dilation.NONE)
-                .uv(0, 18).cuboid(-5.5F, -1.5F, -5.25F, 11.0F, 5.0F, 8.0F, Dilation.NONE), ModelTransform.of(0.0F, -1.0F, 0.0F, 0.1745F, 0.0F, 0.0F));
+                .uv(0, 83).cuboid(-4.5F, -1.5F, -4.25F, 9.0F, 5.0F, 7.0F, new Dilation(0.25f))
+                .uv(0, 18).cuboid(-5.5F, -1.5F, -5.25F, 11.0F, 5.0F, 8.0F, new Dilation(0.25f)), ModelTransform.of(0.0F, -1.0F, 0.0F, 0.1745F, 0.0F, 0.0F));
 
         var right_arm = root.addChild("right_arm", ModelPartBuilder.create()
                 .uv(18, 56).cuboid(-4.0F, -1.5F, -2.5F, 5.0F, 12.0F, 5.0F, new Dilation(0.175f))
@@ -75,20 +75,20 @@ public class BedrockArmorModel extends BipedEntityModel<LivingEntity> {
         var left_leg = root.addChild("left_leg", ModelPartBuilder.create(), ModelTransform.pivot(2.0F, 12.0F, 0.0F));
 
         var left_leg_armor = left_leg.addChild("left_leg_armor", ModelPartBuilder.create()
-                .uv(67, 47).cuboid(-2.5F, -0.5F, -2.5F, 5.0F, 10.0F, 5.0F, Dilation.NONE), ModelTransform.NONE);
+                .uv(67, 47).cuboid(-2.5F, -0.5F, -2.5F, 5.0F, 10.0F, 5.0F, new Dilation(0.1f)), ModelTransform.NONE);
 
         var left_boot = left_leg.addChild("left_boot", ModelPartBuilder.create()
-                .uv(82, 42).cuboid(-2.5F, 9.5F, -2.5F, 5.0F, 3.0F, 5.0F, new Dilation(0.01f))
-                .uv(32, 80).mirrored().cuboid(-2.5F, 5.0F, -1.25F, 5.0F, 6.0F, 4.0F, new Dilation(0.01f)).mirrored(false), ModelTransform.NONE);
+                .uv(82, 42).cuboid(-2.5F, 9.25f, -2.5F, 5.0F, 3.0F, 5.0F, new Dilation(0.1f))
+                .uv(32, 80).mirrored().cuboid(-2.25F, 5.0F, -1.25F, 5.0F, 6.0F, 4.0F, new Dilation(0.125f)).mirrored(false), ModelTransform.NONE);
 
         var right_leg = root.addChild("right_leg", ModelPartBuilder.create(), ModelTransform.pivot(2.0F, 12.0F, 0.0F));
 
         var right_leg_armor = right_leg.addChild("right_leg_armor", ModelPartBuilder.create()
-                .uv(62, 63).cuboid(-2.5F, -0.5F, -2.5F, 5.0F, 10.0F, 5.0F, Dilation.NONE), ModelTransform.NONE);
+                .uv(62, 63).cuboid(-2.5F, -0.5F, -2.5F, 5.0F, 10.0F, 5.0F, new Dilation(0.1f)), ModelTransform.NONE);
 
         var right_boot = right_leg.addChild("right_boot", ModelPartBuilder.create()
-                .uv(32, 80).cuboid(-2.5F, 5.0F, -1.25F, 5.0F, 6.0F, 4.0F, new Dilation(0.01f))
-                .uv(80, 34).cuboid(-2.5F, 9.5F, -2.5F, 5.0F, 3.0F, 5.0F, new Dilation(0.01f)), ModelTransform.NONE);
+                .uv(32, 80).cuboid(-2.75F, 5.0F, -1.25F, 5.0F, 6.0F, 4.0F, new Dilation(0.1f))
+                .uv(80, 34).cuboid(-2.5F, 9.25f, -2.5F, 5.0F, 3.0F, 5.0F, new Dilation(0.125f)), ModelTransform.NONE);
         return data;
     }
 

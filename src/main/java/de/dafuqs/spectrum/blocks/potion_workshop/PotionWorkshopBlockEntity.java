774b904af267bf337330c8d0fbb90d0e56cd5036
Robotgiggle <88736742+Robotgiggle@users.noreply.github.com>
2025-04-08 15:14:16 -0400

    Potion unlocking rework (v2) (#671)
    * Remove vanilla potion requirement from spectrum potion unlocks
    
    * Lock non-vanilla reagents until a potion is brewed
    
    * Rework reagent intro pages
    
    * Make potion workshop reagents only work if they're unlocked
    
    * Apply reagent locking to mod compat recipes

--- src/main/java/de/dafuqs/spectrum/blocks/potion_workshop/PotionWorkshopBlockEntity.java
+++ src/main/java/de/dafuqs/spectrum/blocks/potion_workshop/PotionWorkshopBlockEntity.java
@@ -210,8 +210,10 @@ public class PotionWorkshopBlockEntity extends BlockEntity implements NamedScree
 	
 	private static boolean isBrewingRecipeApplicable(PotionWorkshopBrewingRecipe recipe, ItemStack baseIngredient, PotionWorkshopBlockEntity potionWorkshopBlockEntity) {
 		PotionMod potionMod = getPotionModFromReagents(potionWorkshopBlockEntity);
-		return hasUniqueReagents(potionWorkshopBlockEntity) && recipe.recipeData.isApplicableTo(baseIngredient, potionMod)
-		       && !(potionMod.incurable && SpectrumStatusEffectTags.cannotBeIncurable(recipe.recipeData.statusEffect()));
+		return hasUniqueReagents(potionWorkshopBlockEntity) 
+		    && canPlayerUseReagents(potionWorkshopBlockEntity)
+		    && recipe.recipeData.isApplicableTo(baseIngredient, potionMod)
+			&& !(potionMod.incurable && SpectrumStatusEffectTags.cannotBeIncurable(recipe.recipeData.statusEffect()));
 	}
 	
 	private static void craftRecipe(PotionWorkshopBlockEntity potionWorkshopBlockEntity, PotionWorkshopCraftingRecipe recipe) {
@@ -324,6 +326,18 @@ public class PotionWorkshopBlockEntity extends BlockEntity implements NamedScree
 			
 			potionWorkshopBlockEntity.lastBrewedRecipe = brewingRecipe;
 		}
+	}
+
+	private static boolean canPlayerUseReagents(PotionWorkshopBlockEntity potionWorkshopBlockEntity) {
+		World world = potionWorkshopBlockEntity.getWorld();
+		List<PotionWorkshopReactingRecipe> reagentRecipes = world.getRecipeManager().getAllMatches(SpectrumRecipeTypes.POTION_WORKSHOP_REACTING, potionWorkshopBlockEntity, world);
+		SpectrumCommon.logError("Found "+reagentRecipes.size()+" matching reagent recipes");
+		for (PotionWorkshopReactingRecipe recipe : reagentRecipes) {
+			if (!recipe.canPlayerCraft(potionWorkshopBlockEntity.getOwnerIfOnline())) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	private static PotionMod getPotionModFromReagents(PotionWorkshopBlockEntity potionWorkshopBlockEntity) {

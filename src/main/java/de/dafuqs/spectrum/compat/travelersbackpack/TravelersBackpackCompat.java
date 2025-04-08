f4763599157a500819328aecfa4e84ae3531c5dc
DaFuqs <dominik.m.schmidt@googlemail.com>
2025-04-08 21:47:56 +0200

    Updated Travelers Backpack compat. Fixes #1224

--- src/main/java/de/dafuqs/spectrum/compat/travelersbackpack/TravelersBackpackCompat.java
+++ src/main/java/de/dafuqs/spectrum/compat/travelersbackpack/TravelersBackpackCompat.java
@@ -1,12 +1,12 @@
 package de.dafuqs.spectrum.compat.travelersbackpack;
 
+import com.tiviacz.travelersbackpack.api.fluids.*;
 import com.tiviacz.travelersbackpack.fluids.*;
+import com.tiviacz.travelersbackpack.inventory.*;
 import de.dafuqs.spectrum.compat.*;
 import de.dafuqs.spectrum.helpers.*;
 import de.dafuqs.spectrum.registries.*;
 import net.fabricmc.api.*;
-import net.fabricmc.fabric.api.transfer.v1.fluid.*;
-import net.fabricmc.fabric.api.transfer.v1.storage.*;
 import net.minecraft.enchantment.*;
 import net.minecraft.entity.*;
 import net.minecraft.entity.effect.*;
@@ -22,12 +22,10 @@ public class TravelersBackpackCompat extends SpectrumIntegrationPacks.ModIntegra
 	public abstract static class SpectrumEffectFluid extends EffectFluid {
 		
 		public SpectrumEffectFluid(String id, Fluid fluid) {
-			super(id, fluid, 81000L);
+			super(id, fluid, 81000);
 		}
 		
-		public abstract void affectDrinker(StorageView<FluidVariant> fluidStack, World world, Entity entity);
-		
-		public boolean canExecuteEffect(StorageView<FluidVariant> stack, World world, Entity entity) {
+		public boolean canExecuteEffect(FluidVariantWrapper stack, World world, Entity entity) {
 			return stack.getAmount() >= this.amountRequired;
 		}
 		
@@ -36,19 +34,21 @@ public class TravelersBackpackCompat extends SpectrumIntegrationPacks.ModIntegra
 	@Override
 	public void register() {
 		EffectFluidRegistry.registerFluidEffect(new SpectrumEffectFluid("spectrum:mud", SpectrumFluids.MUD.getStill()) {
+			
 			@Override
-			public void affectDrinker(StorageView<FluidVariant> fluidStack, World world, Entity entity) {
+			public void affectDrinker(FluidVariantWrapper fluidVariantWrapper, World world, Entity entity) {
 				if (entity instanceof LivingEntity livingEntity) {
 					livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200));
 					livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 400, 2));
 					livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 600, 3));
 				}
 			}
+			
 		});
 		
 		EffectFluidRegistry.registerFluidEffect(new SpectrumEffectFluid("spectrum:liquid_crystal", SpectrumFluids.LIQUID_CRYSTAL.getStill()) {
 			@Override
-			public void affectDrinker(StorageView<FluidVariant> fluidStack, World world, Entity entity) {
+			public void affectDrinker(FluidVariantWrapper fluidStack, World world, Entity entity) {
 				if (entity instanceof PlayerEntity player) {
 					player.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 400, 1));
 				}
@@ -57,7 +57,7 @@ public class TravelersBackpackCompat extends SpectrumIntegrationPacks.ModIntegra
 		
 		EffectFluidRegistry.registerFluidEffect(new SpectrumEffectFluid("spectrum:midnight_solution", SpectrumFluids.MIDNIGHT_SOLUTION.getStill()) {
 			@Override
-			public void affectDrinker(StorageView<FluidVariant> fluidStack, World world, Entity entity) {
+			public void affectDrinker(FluidVariantWrapper fluidStack, World world, Entity entity) {
 				if (entity instanceof PlayerEntity player) {
 					player.addExperience(-20);
 					
@@ -80,7 +80,7 @@ public class TravelersBackpackCompat extends SpectrumIntegrationPacks.ModIntegra
 		
 		EffectFluidRegistry.registerFluidEffect(new SpectrumEffectFluid("spectrum:dragonrot", SpectrumFluids.DRAGONROT.getStill()) {
 			@Override
-			public void affectDrinker(StorageView<FluidVariant> fluidStack, World world, Entity entity) {
+			public void affectDrinker(FluidVariantWrapper fluidStack, World world, Entity entity) {
 				if (entity instanceof LivingEntity livingEntity) {
 					livingEntity.addStatusEffect(new StatusEffectInstance(SpectrumStatusEffects.LIFE_DRAIN, 600, 3));
 					livingEntity.damage(SpectrumDamageTypes.dragonrot(world), 1000); // 💀

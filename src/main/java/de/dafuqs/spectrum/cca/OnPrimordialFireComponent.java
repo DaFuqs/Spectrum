90a3d6c0a037a22bac794a5ddedc9c33f94034dc
DaFuqs <dominik.m.schmidt@googlemail.com>
2025-05-28 21:24:06 +0200

    primordial fire does not use set_health anymore (since that plays damage tilt every tick). Now considered fire damage

--- src/main/java/de/dafuqs/spectrum/cca/OnPrimordialFireComponent.java
+++ src/main/java/de/dafuqs/spectrum/cca/OnPrimordialFireComponent.java
@@ -1,7 +1,6 @@
 package de.dafuqs.spectrum.cca;
 
 import de.dafuqs.spectrum.*;
-import de.dafuqs.spectrum.cca.azure_dike.*;
 import de.dafuqs.spectrum.helpers.*;
 import de.dafuqs.spectrum.registries.*;
 import de.dafuqs.spectrum.sound.*;
@@ -15,7 +14,6 @@ import net.minecraft.nbt.*;
 import net.minecraft.sounds.*;
 import net.minecraft.tags.*;
 import net.minecraft.util.*;
-import net.minecraft.world.effect.*;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.player.*;
 import net.minecraft.world.item.enchantment.*;
@@ -29,14 +27,8 @@ import org.ladysnake.cca.api.v3.component.tick.*;
 import java.util.*;
 
 public class OnPrimordialFireComponent implements AutoSyncedComponent, ServerTickingComponent, ClientTickingComponent {
-
-	// 1% of max health as damage every tick as a base.
-	public static final float BASE_PERCENT_DAMAGE = 0.01F;
-
-	// Base damage reduction applied by fire resistance
-	public static final float FIRE_RESISTANCE_DAMAGE_RESISTANCE = 0.25F;
-	// Per-level damage reduction added by fire prot. Caps at 50%
-	public static final float FIRE_PROT_DAMAGE_RESISTANCE = 0.05F;
+	
+	public static final float BASE_PERCENT_DAMAGE = 0.1F;
 	
 	@Environment(EnvType.CLIENT)
 	private static Optional<OnPrimordialFireSoundInstance> soundInstance;
@@ -122,16 +114,17 @@ public class OnPrimordialFireComponent implements AutoSyncedComponent, ServerTic
 
 		if (this.primordialFireTicks > 0) {
 			if (!isAffectingConstruct()) {
-				var damageScaling = getDamageHealthScaling(provider);
-				provider.hurt(SpectrumDamageTypes.primordialFire(this.provider.level()), AzureDikeProvider.absorbDamage(provider, damageScaling * provider.getMaxHealth()));
+				var damageScaling = getDamage(provider);
+				provider.hurt(SpectrumDamageTypes.primordialFire(this.provider.level()), damageScaling);
 			}
 			//Primordial fire is so strong because it rends the soul. No soul = just slightly spicier fire
 			//Constructs have no soul, thus you get 2 dps and no more
-			else if (provider.tickCount % 10 == 0) {
-				provider.hurt(SpectrumDamageTypes.primordialFire(this.provider.level()), 1);
+			else if (provider.tickCount % 20 == 0) {
+				provider.hurt(SpectrumDamageTypes.primordialFire(this.provider.level()), 2.0F);
 			}
 
 			this.primordialFireTicks -= this.provider.getFluidHeight(FluidTags.WATER) > 0 ? 3 : 1;
+			
 			// was on fire, but is not any longer
 			if (this.primordialFireTicks <= 0) {
 				ON_PRIMORDIAL_FIRE_COMPONENT.sync(this.provider);
@@ -142,52 +135,20 @@ public class OnPrimordialFireComponent implements AutoSyncedComponent, ServerTic
 	public boolean isAffectingConstruct() {
 		return provider.getType().is(SpectrumEntityTypeTags.SOULLESS);
 	}
-
-	/**
-	 * Primordial fire's base DPS is 1/t, for a kill time of 5 seconds on a base hp player.
-	 */
-	public float getDamageHealthScaling(LivingEntity entity) {
+	
+	public float getDamage(LivingEntity entity) {
 		float baseDamage = BASE_PERCENT_DAMAGE;
 
 		//Bosses have great and exceptional souls that can resist a lot more.
-		//95% less damage to them before reductions and caps
+		//95% less damage to them
 		if (entity.getType().is(ConventionalEntityTypeTags.BOSSES))
 			baseDamage /= 20F;
-
-        return baseDamage * getDamagePenalties(entity) * getDamageBonuses(entity);
-	}
-
-	public float getDamagePenalties(LivingEntity entity) {
-		//fire prot has a cap of 50% DR, requiring fire protection 10 on an armor piece
-		float fireProt = Math.min(FIRE_PROT_DAMAGE_RESISTANCE * SpectrumEnchantmentHelper.getEquipmentLevel(entity.level().registryAccess(), Enchantments.FIRE_PROTECTION, provider), 0.5F);
-		int fireResLevel = Optional.ofNullable(provider.getEffect(MobEffects.FIRE_RESISTANCE)).map(MobEffectInstance::getAmplifier).orElse(-1) + 1;
-		float fireRes = 0;
-
-		// flat 25% for a start on fire res
-		if (fireResLevel > 0)
-			fireRes = FIRE_RESISTANCE_DAMAGE_RESISTANCE;
-
-		//Fire resistance has diminishing returns
-		for (int i = 1; i < fireResLevel; i++) {
-			fireRes += (float) (0.05 * (i) + (0.25F * Math.pow(0.5F, i)));
-		}
-
+		
 		//Fire immune entities can have a lil res, as a treat
-		float immunityReduction = entity.fireImmune() ? 0.25F : 0;
-
-		//Primordial fire has an overall cap of 90% DR
-		return Math.max(1 - (fireRes + fireProt + immunityReduction), 0.10F);
+		float fireImmunityMultiplier = entity.fireImmune() ? 0.25F : 1;
+		return baseDamage * fireImmunityMultiplier * provider.getMaxHealth();
 	}
-
-	/**
-	 * Here for completeness.
-	 * <p>
-	 * Unused... for now...
-	 */
-	public float getDamageBonuses(LivingEntity entity) {
-		return 1F;
-	}
-
+	
 	@Override
 	@Environment(EnvType.CLIENT)
 	public void clientTick() {

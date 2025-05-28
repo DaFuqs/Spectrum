6a11952f78d1142833c85e072b0d52679354508b
DaFuqs <dominik.m.schmidt@googlemail.com>
2025-05-28 19:48:11 +0200

    immunity makes the player immune to *all* other effects (positive, as well as negative)

--- src/main/java/de/dafuqs/spectrum/status_effects/ImmunityStatusEffect.java
+++ src/main/java/de/dafuqs/spectrum/status_effects/ImmunityStatusEffect.java
@@ -1,15 +1,19 @@
 package de.dafuqs.spectrum.status_effects;
 
-import de.dafuqs.spectrum.items.trinkets.*;
+import de.dafuqs.spectrum.registries.*;
+import net.minecraft.core.*;
 import net.minecraft.world.effect.*;
 import net.minecraft.world.entity.*;
+import org.jetbrains.annotations.*;
+
+import java.util.*;
 
 public class ImmunityStatusEffect extends MobEffect {
 	
 	public ImmunityStatusEffect(MobEffectCategory statusEffectCategory, int color) {
 		super(statusEffectCategory, color);
 	}
-
+	
 	@Override
 	public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
 		return duration % 20 == 0;
@@ -17,14 +21,27 @@ public class ImmunityStatusEffect extends MobEffect {
 	
 	@Override
 	public boolean applyEffectTick(LivingEntity entity, int amplifier) {
-		WhispyCircletItem.removeNegativeStatusEffects(entity);
+		removeOtherStatusEffects(entity);
 		return super.applyEffectTick(entity, amplifier);
 	}
 	
 	@Override
 	public void onEffectStarted(LivingEntity entity, int amplifier) {
 		super.onEffectStarted(entity, amplifier);
-		WhispyCircletItem.removeNegativeStatusEffects(entity);
+		removeOtherStatusEffects(entity);
+	}
+	
+	public static void removeOtherStatusEffects(@NotNull LivingEntity entity) {
+		Set<Holder<MobEffect>> effectsToRemove = new HashSet<>();
+		for (MobEffectInstance instance : entity.getActiveEffects()) {
+			if (!instance.getEffect().is(SpectrumStatusEffectTags.BYPASSES_IMMUNITY)) {
+				effectsToRemove.add(instance.getEffect());
+			}
+		}
+		
+		for (Holder<MobEffect> effect : effectsToRemove) {
+			entity.removeEffect(effect);
+		}
 	}
 	
 }

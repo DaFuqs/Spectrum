2d57094a273d27b7be84090d5f28e7b3e07b007f
DaFuqs <dominik.m.schmidt@googlemail.com>
2025-06-26 17:09:21 +0200

    seems to work

--- src/main/java/de/dafuqs/spectrum/progression/advancement/InkContainerInteractionCriterion.java
+++ src/main/java/de/dafuqs/spectrum/progression/advancement/InkContainerInteractionCriterion.java
@@ -31,7 +31,7 @@ public class InkContainerInteractionCriterion extends SimpleCriterionTrigger<Ink
 			Optional<ContextAwarePredicate> player,
 			ItemPredicate itemPredicate,
 			Map<InkColor, LongRange> colorRanges,
-			ColorPredicate changeColorPredicate,
+			InkColorPredicate changeInkColorPredicate,
 			LongRange changeRange
 	) implements SimpleCriterionTrigger.SimpleInstance {
 		
@@ -39,14 +39,14 @@ public class InkContainerInteractionCriterion extends SimpleCriterionTrigger<Ink
 				ContextAwarePredicate.CODEC.optionalFieldOf("player").forGetter(Conditions::player),
 				ItemPredicate.CODEC.optionalFieldOf("item", ItemPredicate.Builder.item().build()).forGetter(Conditions::itemPredicate),
 				CodecHelper.registryMap(SpectrumRegistries.INK_COLOR, LongRange.CODEC).forGetter(Conditions::colorRanges),
-				ColorPredicate.CODEC.optionalFieldOf("change_color", ColorPredicate.ANY).forGetter(Conditions::changeColorPredicate),
+				InkColorPredicate.CODEC.optionalFieldOf("change_color", InkColorPredicate.ANY).forGetter(Conditions::changeInkColorPredicate),
 				LongRange.CODEC.optionalFieldOf("change_amount", LongRange.ANY).forGetter(Conditions::changeRange)
 		).apply(instance, Conditions::new));
 		
 		public boolean matches(ItemStack stack, Map<InkColor, Long> colors, InkColor changeColor, long change) {
 			return itemPredicate.test(stack)
 					&& changeRange.test(change)
-					&& changeColorPredicate.test(changeColor)
+					&& changeInkColorPredicate.test(changeColor)
 					&& colorRanges.entrySet().stream().allMatch(entry -> colorRanges.get(entry.getKey()).test(colors.get(entry.getKey())));
 			
 		}

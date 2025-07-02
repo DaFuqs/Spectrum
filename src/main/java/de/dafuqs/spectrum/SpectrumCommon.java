7ce24c5371c05d07855f09fc488c439843cdca34
Technici4n <13494793+Technici4n@users.noreply.github.com>
2025-07-03 01:35:41 +0200

    Starting bringing SpectrumCommon over

---
+++ src/main/java/de/dafuqs/spectrum/SpectrumCommon.java
@@ -0,0 +1,74 @@
+package de.dafuqs.spectrum;
+
+import net.minecraft.resources.*;
+import net.minecraft.server.*;
+import net.minecraft.tags.*;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.*;
+import net.neoforged.bus.api.*;
+import net.neoforged.fml.common.*;
+import org.jetbrains.annotations.*;
+import org.slf4j.*;
+
+import java.util.*;
+
+@Mod(SpectrumCommon.MOD_ID)
+public class SpectrumCommon {
+	
+	public static final String MOD_ID = "spectrum";
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger("Spectrum");
+	public static final Map<ResourceLocation, TagKey<Item>> CACHED_ITEM_TAG_MAP = new HashMap<>();
+	// TODO PORT
+//	public static SpectrumConfig CONFIG;
+
+	public static void logInfo(String message) {
+		LOGGER.info("[Spectrum] {}", message);
+	}
+	
+	public static void logWarning(String message) {
+		LOGGER.warn("[Spectrum] {}", message);
+	}
+	
+	public static void logError(String message) {
+		LOGGER.error("[Spectrum] {}", message);
+	}
+	
+	public static ResourceLocation locate(String name) {
+		return ResourceLocation.fromNamespaceAndPath(MOD_ID, name);
+	}
+	
+	/**
+	 * This is the Spectrum analogue of Identifier.of, but instead of defaulting to the namespace 'minecraft', it defaults to 'spectrum'.
+	 *
+	 * @param id The stringified identifier to parse
+	 * @return The parsed identifier
+	 */
+	public static ResourceLocation ofSpectrumDefaulted(String id) {
+		int i = id.indexOf(':');
+		String path = id.substring(i + 1);
+		String namespace = i > 0 ? id.substring(0, i) : SpectrumCommon.MOD_ID;
+		return ResourceLocation.fromNamespaceAndPath(namespace, path);
+	}
+	
+	// Will be null when playing on a dedicated server!
+	@Nullable
+	public static MinecraftServer minecraftServer;
+	
+	public SpectrumCommon(IEventBus modBus) {
+		logInfo("Starting Common Startup");
+		
+	}
+	
+	/**
+	 * When initializing a block entity, world can still be null
+	 * Therefore we use the RecipeManager reference from MinecraftServer
+	 * This in turn does not work on clients connected to dedicated servers, though
+	 * since SpectrumCommon.minecraftServer is null
+	 */
+	public static Optional<RecipeManager> getRecipeManager(@Nullable Level world) {
+		return world == null ? minecraftServer == null ? Optional.empty() : Optional.of(minecraftServer.getRecipeManager()) : Optional.of(world.getRecipeManager());
+	}
+	
+}
--- src/main_disabled/java/de/dafuqs/spectrum/SpectrumCommon.java
+++ src/main_disabled/java/de/dafuqs/spectrum/SpectrumCommon.java
@@ -42,45 +42,6 @@ import java.util.*;
 
 public class SpectrumCommon implements ModInitializer {
 	
-	public static final String MOD_ID = "spectrum";
-	
-	public static final Logger LOGGER = LoggerFactory.getLogger("Spectrum");
-	public static final Map<ResourceLocation, TagKey<Item>> CACHED_ITEM_TAG_MAP = new HashMap<>();
-	public static SpectrumConfig CONFIG;
-	
-	public static void logInfo(String message) {
-		LOGGER.info("[Spectrum] {}", message);
-	}
-	
-	public static void logWarning(String message) {
-		LOGGER.warn("[Spectrum] {}", message);
-	}
-	
-	public static void logError(String message) {
-		LOGGER.error("[Spectrum] {}", message);
-	}
-	
-	public static ResourceLocation locate(String name) {
-		return ResourceLocation.fromNamespaceAndPath(MOD_ID, name);
-	}
-	
-	/**
-	 * This is the Spectrum analogue of Identifier.of, but instead of defaulting to the namespace 'minecraft', it defaults to 'spectrum'.
-	 *
-	 * @param id The stringified identifier to parse
-	 * @return The parsed identifier
-	 */
-	public static ResourceLocation ofSpectrumDefaulted(String id) {
-		int i = id.indexOf(':');
-		String path = id.substring(i + 1);
-		String namespace = i > 0 ? id.substring(0, i) : SpectrumCommon.MOD_ID;
-		return ResourceLocation.fromNamespaceAndPath(namespace, path);
-	}
-	
-	// Will be null when playing on a dedicated server!
-	@Nullable
-	public static MinecraftServer minecraftServer;
-	
 	// Miscellaneous registrars
 	public static final DeferredRegistrar FUEL_REGISTRAR = new DeferredRegistrar();
 	
@@ -94,7 +55,6 @@ public class SpectrumCommon implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
-		logInfo("Starting Common Startup");
 		
 		// Register internals
 		SpectrumRegistries.register();
@@ -274,16 +234,6 @@ public class SpectrumCommon implements ModInitializer {
 		}
 		
 		logInfo("Common startup completed!");
-	}
-	
-	/**
-	 * When initializing a block entity, world can still be null
-	 * Therefore we use the RecipeManager reference from MinecraftServer
-	 * This in turn does not work on clients connected to dedicated servers, though
-	 * since SpectrumCommon.minecraftServer is null
-	 */
-	public static Optional<RecipeManager> getRecipeManager(@Nullable Level world) {
-		return world == null ? minecraftServer == null ? Optional.empty() : Optional.of(minecraftServer.getRecipeManager()) : Optional.of(world.getRecipeManager());
 	}
 	
 }

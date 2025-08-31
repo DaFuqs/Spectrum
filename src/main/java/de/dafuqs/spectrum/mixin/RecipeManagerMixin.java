1b8da3cdea9366ae780b285010fe4019c9434aaf
unilock <katherines1337@outlook.com>
2025-08-31 07:39:14 -0400

    RecipeManagerMixin: inject after recipe data loading

--- src/main/java/de/dafuqs/spectrum/mixin/RecipeManagerMixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/RecipeManagerMixin.java
@@ -1,7 +1,9 @@
 package de.dafuqs.spectrum.mixin;
 
 import com.google.common.collect.*;
+import com.llamalad7.mixinextras.injector.*;
 import com.llamalad7.mixinextras.sugar.*;
+import com.llamalad7.mixinextras.sugar.ref.*;
 import de.dafuqs.spectrum.recipe.enchantment_upgrade.*;
 import de.dafuqs.spectrum.registries.*;
 import net.minecraft.recipe.*;
@@ -14,11 +16,20 @@ import java.util.*;
 
 @Mixin(RecipeManager.class)
 public class RecipeManagerMixin {
-	@Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE_ASSIGN", target = "Lcom/google/common/collect/ImmutableMap;builder()Lcom/google/common/collect/ImmutableMap$Builder;", remap = false))
-	public void injectEnchantmentUpgradeRecipes(CallbackInfo info, @Local(ordinal = 1) Map<RecipeType<?>, ImmutableMap.Builder<Identifier, Recipe<?>>> recipes, @Local(ordinal = 0) ImmutableMap.Builder<Identifier, Recipe<?>> recipesById) {
-		ImmutableMap<Identifier, Recipe<?>> collectedRecipes = EnchantmentUpgradeRecipeSerializer.enchantmentUpgradeRecipesToInject.stream().collect(ImmutableMap.toImmutableMap(EnchantmentUpgradeRecipe::getId, enchantmentUpgradeRecipe -> enchantmentUpgradeRecipe));
-		
-		recipes.computeIfAbsent(SpectrumRecipeTypes.ENCHANTMENT_UPGRADE, (recipeType) -> ImmutableMap.builder()).putAll(collectedRecipes);
-		recipesById.putAll(collectedRecipes);
+	@Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE", target = "Ljava/util/Map;entrySet()Ljava/util/Set;", ordinal = 1, remap = false))
+	private void shareEnchantmentUpgradeRecipes(CallbackInfo ci, @Share("collectedRecipes") LocalRef<ImmutableMap<Identifier, Recipe<?>>> collectedRecipes) {
+		collectedRecipes.set(EnchantmentUpgradeRecipeSerializer.enchantmentUpgradeRecipesToInject.stream().collect(ImmutableMap.toImmutableMap(EnchantmentUpgradeRecipe::getId, enchantmentUpgradeRecipe -> enchantmentUpgradeRecipe)));
+	}
+	
+	@ModifyReceiver(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE", target = "Ljava/util/Map;entrySet()Ljava/util/Set;", ordinal = 1, remap = false))
+	private Map<RecipeType<?>, ImmutableMap.Builder<Identifier, Recipe<?>>> injectRecipes(Map<RecipeType<?>, ImmutableMap.Builder<Identifier, Recipe<?>>> recipes, @Share("collectedRecipes") LocalRef<ImmutableMap<Identifier, Recipe<?>>> collectedRecipes) {
+		recipes.computeIfAbsent(SpectrumRecipeTypes.ENCHANTMENT_UPGRADE, (recipeType) -> ImmutableMap.builder()).putAll(collectedRecipes.get());
+		return recipes;
+	}
+	
+	@ModifyReceiver(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap$Builder;build()Lcom/google/common/collect/ImmutableMap;", remap = false))
+	private ImmutableMap.Builder<Identifier, Recipe<?>> injectRecipesById(ImmutableMap.Builder<Identifier, Recipe<?>> recipesById, @Share("collectedRecipes") LocalRef<ImmutableMap<Identifier, Recipe<?>>> collectedRecipes) {
+		recipesById.putAll(collectedRecipes.get());
+		return recipesById;
 	}
 }

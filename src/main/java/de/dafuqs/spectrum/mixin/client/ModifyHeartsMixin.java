a16a2381a49901250a54d8f77b231d9ccc5dd3a9
DaFuqs <dominik.m.schmidt@googlemail.com>
2025-09-16 11:39:37 +0200

    deadly poison renders green hearts (mixes and matches with divinity)

--- src/main/java/de/dafuqs/spectrum/mixin/client/ModifyHeartsMixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/client/ModifyHeartsMixin.java
@@ -1,7 +1,9 @@
 package de.dafuqs.spectrum.mixin.client;
 
 import de.dafuqs.spectrum.registries.*;
+import net.minecraft.client.*;
 import net.minecraft.client.gui.*;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.player.*;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
@@ -10,13 +12,19 @@ import org.spongepowered.asm.mixin.injection.callback.*;
 @Mixin(Gui.HeartType.class)
 public abstract class ModifyHeartsMixin {
 	
-	@Inject(method = "Lnet/minecraft/client/gui/Gui$HeartType;forPlayer(Lnet/minecraft/world/entity/player/Player;)Lnet/minecraft/client/gui/Gui$HeartType;", at = @At(value = "HEAD"), cancellable = true)
-	private void spectrum$showDivinityHardcoreHearts(Player player, CallbackInfoReturnable<Gui.HeartType> cir) {
-		if (player.hasEffect(SpectrumStatusEffects.DIVINITY)) {
-			// cir.setReturnValue(Gui.HeartType.);
+	@ModifyVariable(method = "getSprite(ZZZ)Lnet/minecraft/resources/ResourceLocation;", at = @At(value = "HEAD"), ordinal = 0, argsOnly = true)
+	private static boolean spectrum$hardcoreHearts(boolean hardcore) {
+		Entity cameraEntity = Minecraft.getInstance().getCameraEntity();
+		if (cameraEntity instanceof LivingEntity livingEntity && livingEntity.hasEffect(SpectrumStatusEffects.DIVINITY)) {
+			return true;
 		}
+		return hardcore;
+	}
+	
+	@Inject(method = "forPlayer(Lnet/minecraft/world/entity/player/Player;)Lnet/minecraft/client/gui/Gui$HeartType;", at = @At(value = "HEAD"), cancellable = true)
+	private static void spectrum$deadlyPoisonHearts(Player player, CallbackInfoReturnable<Gui.HeartType> cir) {
 		if (player.hasEffect(SpectrumStatusEffects.DEADLY_POISON)) {
-			// cir.setReturnValue(Gui.HeartType.);
+			cir.setReturnValue(Gui.HeartType.POISIONED);
 		}
 	}
 	

9fa5be236a0f8c4d0516100f9eeacdefb2a7df3d
DaFuqs <dominik.m.schmidt@googlemail.com>
2025-11-20 18:03:36 +0100

    going sane

--- src/main/java/de/dafuqs/spectrum/commands/SanityCommand.java
+++ src/main/java/de/dafuqs/spectrum/commands/SanityCommand.java
@@ -2,6 +2,7 @@ package de.dafuqs.spectrum.commands;
 
 import com.mojang.brigadier.arguments.*;
 import com.mojang.brigadier.tree.*;
+import de.dafuqs.fractal.api.*;
 import de.dafuqs.revelationary.*;
 import de.dafuqs.revelationary.advancement_criteria.*;
 import de.dafuqs.spectrum.*;
@@ -34,6 +35,7 @@ import net.minecraft.tags.*;
 import net.minecraft.world.effect.*;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.*;
+import net.minecraft.world.flag.*;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.item.enchantment.*;
@@ -44,6 +46,7 @@ import org.apache.commons.lang3.*;
 import org.jetbrains.annotations.*;
 
 import java.util.*;
+import java.util.stream.*;
 
 public class SanityCommand {
 	
@@ -249,7 +252,7 @@ public class SanityCommand {
 				SpectrumCommon.logWarning("[SANITY: Recipe Group Lang] Recipe group " + recipeGroup + " is not localized.");
 			}
 		}
-		
+		source.getLevel().getRecipeManager().getAllRecipesFor(SpectrumRecipeTypes.PEDESTAL).stream().filter(pedestalRecipeRecipeHolder -> pedestalRecipeRecipeHolder.id().getPath().contains("lucky_roll")).collect(Collectors.toUnmodifiableList());
 		// Impossible to unlock recipes
 		for (RecipeHolder<?> recipeEntry : recipeManager.getRecipes()) {
 			if (recipeEntry.value() instanceof GatedRecipe<?> gatedRecipe) {
@@ -441,8 +444,34 @@ public class SanityCommand {
 			}
 		}
 		
+		// Recipes with invalid ingredients
+		for (RecipeHolder<?> recipe : source.getLevel().getRecipeManager().getRecipes()) {
+			ResourceLocation id = recipe.id();
+			Recipe<?> r = recipe.value();
+			for (Ingredient ingredient : r.getIngredients()) {
+				if (ingredient.getItems().length == 0 && !ingredient.isEmpty() && ingredient != Ingredient.EMPTY) {
+					SpectrumCommon.logWarning("[SANITY: Recipe Ingredients] Recipe '" + id + "' has an empty ingredient");
+				}
+			}
+		}
+		BuiltInRegistries.ITEM.getTags().toList().forEach(tagKeyNamedPair -> {
+			TagKey<Item> tag = tagKeyNamedPair.getSecond().key();
+			int size = tagKeyNamedPair.getSecond().size();
+			
+			if (size == 0) {
+				SpectrumCommon.logWarning("[SANITY: Item Tags] Tag '" + tag + "' seems to be empty");
+			}
+		});
+		
 		// items / blocks missing in the creative tab (this will also omit them from most recipe viewers)
 		Collection<ItemStack> itemGroupStacks = SpectrumItemGroups.MAIN.getSearchTabDisplayItems();
+		if (itemGroupStacks.isEmpty()) {
+			// since the creative group will only have content when queried, we build the contents if it is still empty
+			for (ItemSubGroup subGroup : SpectrumItemGroups.MAIN.fractal$getChildren()) {
+				subGroup.buildContents(new CreativeModeTab.ItemDisplayParameters(FeatureFlagSet.of(), false, registryManager));
+			}
+			itemGroupStacks = SpectrumItemGroups.MAIN.getSearchTabDisplayItems();
+		}
 		for (Map.Entry<ResourceKey<Item>, Item> item : BuiltInRegistries.ITEM.entrySet()) {
 			if (item.getKey().location().getNamespace().equals(modId) && !item.getValue().builtInRegistryHolder().is(SpectrumItemTags.COMING_SOON_TOOLTIP)) {
 				boolean found = false;

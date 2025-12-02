5165f4a6a8bd7ae247a4bb0e21d0c9fd04c55161
DaFuqs <dominik.m.schmidt@googlemail.com>
2025-12-02 10:14:38 +0100

    improve the way, items are converted via primordial fire

--- src/main/java/de/dafuqs/spectrum/blocks/PrimordialFireBlock.java
+++ src/main/java/de/dafuqs/spectrum/blocks/PrimordialFireBlock.java
@@ -147,7 +147,7 @@ public class PrimordialFireBlock extends BaseFireBlock {
 			entity.hurt(SpectrumDamageTypes.primordialFire(world, null), DAMAGE);
 			OnPrimordialFireComponent.addPrimordialFireTicks(livingEntity, 5);
 		}
-		if (world.getGameTime() % 20 == 0 && entity instanceof ItemEntity itemEntity) {
+		if (entity instanceof ItemEntity itemEntity && !itemEntity.hasPickUpDelay()) {
 			PrimordialFireBurningRecipe.processItemEntity(world, itemEntity);
 		}
 	}

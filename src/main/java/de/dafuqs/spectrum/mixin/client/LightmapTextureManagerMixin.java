a8445c11d8bd1fbbf2b92d34aa9d97c62fa1006a
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-01-13 16:21:44 +0100

    fixed the screen getting immensely bright when entering black langast

--- src/main/java/de/dafuqs/spectrum/mixin/client/LightmapTextureManagerMixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/client/LightmapTextureManagerMixin.java
@@ -13,7 +13,6 @@ import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(value = LightTexture.class, priority = 9999)
-
 public class LightmapTextureManagerMixin {
 	
 	@Shadow
@@ -22,13 +21,12 @@ public class LightmapTextureManagerMixin {
 	
 	@ModifyReturnValue(method = "calculateDarknessScale", at = @At("RETURN"))
 	private float spectrum$getDarkness(float original) {
-		var lightMod = SpectrumCommon.CONFIG.DimensionBrightnessMod * 0.25F;
-
 		if (isInDim()) {
-			var darkening = Mth.lerp(DimensionRenderEffects.getDarknessInterpolation(), 0.11F - lightMod, 0.2125F - lightMod);
+			var lightMod = SpectrumCommon.CONFIG.DimensionBrightnessMod * 0.25F;
+			float darkening = Mth.lerp(DimensionRenderEffects.getDarknessInterpolation(), 0.11F - lightMod, 0.2125F - lightMod);
 			return Math.max(darkening, original);
-
 		}
+		
 		return original;
 	}
 	
@@ -37,10 +35,6 @@ public class LightmapTextureManagerMixin {
 		if (isInDim()) {
 			if (minecraft.getCameraEntity() instanceof LivingEntity living) {
 				gamma -= living.hasEffect(MobEffects.NIGHT_VISION) ? 0.275F : 0F;
-			}
-
-			if (DimensionRenderEffects.darkenTicks > 0) {
-				gamma = Mth.lerp(DimensionRenderEffects.getDarknessInterpolation(), gamma, gamma - 25F + SpectrumCommon.CONFIG.DimensionBrightnessMod);
 			}
 		}
 

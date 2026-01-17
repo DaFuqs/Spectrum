1d6f62cd5f2fcee2e56385eb679bc1453af5d2d3
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-01-17 12:41:25 +0100

    update travelers backpack; fix compat

--- src/main/java/de/dafuqs/spectrum/compat/travelersbackpack/TravelersBackpackCompat.java
+++ src/main/java/de/dafuqs/spectrum/compat/travelersbackpack/TravelersBackpackCompat.java
@@ -12,6 +12,7 @@ import net.minecraft.world.item.*;
 import net.minecraft.world.item.enchantment.*;
 import net.minecraft.world.level.*;
 import net.minecraft.world.level.material.*;
+import net.neoforged.neoforge.fluids.*;
 
 import java.util.*;
 
@@ -23,7 +24,7 @@ public class TravelersBackpackCompat extends SpectrumIntegrationPacks.ModIntegra
 			super(id, fluid, 1000);
 		}
 		
-		public boolean canExecuteEffect(FluidIngredientWrapper stack, Level world, Entity entity) {
+		public boolean canExecuteEffect(FluidStack stack, Level world, Entity entity) {
 			return stack.getAmount() >= this.amountRequired;
 		}
 		
@@ -31,9 +32,10 @@ public class TravelersBackpackCompat extends SpectrumIntegrationPacks.ModIntegra
 	
 	@Override
 	public void register() {
+		
 		EffectFluidRegistry.registerFluidEffect(new SpectrumEffectFluid("spectrum:sludge", SpectrumFluids.SLUDGE.get().getSource()) {
 			@Override
-			public void affectDrinker(FluidIngredientWrapper FluidIngredientWrapper, Level world, Entity entity) {
+			public void affectDrinker(FluidStack fluidStack, Level world, Entity entity) {
 				if (entity instanceof LivingEntity livingEntity) {
 					livingEntity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 200));
 					livingEntity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 400, 2));
@@ -45,7 +47,7 @@ public class TravelersBackpackCompat extends SpectrumIntegrationPacks.ModIntegra
 		
 		EffectFluidRegistry.registerFluidEffect(new SpectrumEffectFluid("spectrum:liquid_crystal", SpectrumFluids.LIQUID_CRYSTAL.get().getSource()) {
 			@Override
-			public void affectDrinker(FluidIngredientWrapper fluidStack, Level world, Entity entity) {
+			public void affectDrinker(FluidStack fluidStack, Level world, Entity entity) {
 				if (entity instanceof Player player) {
 					player.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 400, 1));
 				}
@@ -54,7 +56,7 @@ public class TravelersBackpackCompat extends SpectrumIntegrationPacks.ModIntegra
 		
 		EffectFluidRegistry.registerFluidEffect(new SpectrumEffectFluid("spectrum:midnight_solution", SpectrumFluids.MIDNIGHT_SOLUTION.get().getSource()) {
 			@Override
-			public void affectDrinker(FluidIngredientWrapper fluidStack, Level world, Entity entity) {
+			public void affectDrinker(FluidStack fluidStack, Level world, Entity entity) {
 				if (entity instanceof Player player) {
 					player.giveExperiencePoints(-20);
 					
@@ -77,7 +79,7 @@ public class TravelersBackpackCompat extends SpectrumIntegrationPacks.ModIntegra
 		
 		EffectFluidRegistry.registerFluidEffect(new SpectrumEffectFluid("spectrum:dragonrot", SpectrumFluids.DRAGONROT.get().getSource()) {
 			@Override
-			public void affectDrinker(FluidIngredientWrapper fluidStack, Level world, Entity entity) {
+			public void affectDrinker(FluidStack fluidStack, Level world, Entity entity) {
 				if (entity instanceof LivingEntity livingEntity) {
 					livingEntity.addEffect(new MobEffectInstance(SpectrumStatusEffects.LIFE_DRAIN, 600, 3));
 					livingEntity.hurt(SpectrumDamageTypes.dragonrot(world), 1000); // 💀

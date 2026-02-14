b46e955dc3c0ce88c700abf8d76395b00ed2004e
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-02-14 13:31:08 +0100

    bedrock armor front cloth

--- src/main/java/de/dafuqs/spectrum/render/armor/BedrockArmorCapeModel.java
+++ src/main/java/de/dafuqs/spectrum/render/armor/BedrockArmorCapeModel.java
@@ -4,36 +4,32 @@ import net.minecraft.client.model.geom.*;
 import net.minecraft.client.model.geom.builders.*;
 
 public class BedrockArmorCapeModel {
+	
 	public static final ModelPart CAPE_MODEL = createCape();
 	public static final ModelPart FRONT_CLOTH = createFrontCloth();
 	
 	private static ModelPart createCape() {
-		MeshDefinition data = new MeshDefinition();
-		var root = data.getRoot();
+		MeshDefinition definition = new MeshDefinition();
 		
-		root.addOrReplaceChild(
+		definition.getRoot().addOrReplaceChild(
 				"cape",
 				CubeListBuilder.create()
 						.texOffs(0, 80)
 						.addBox(-5.5F, 0.0F, -0.05F, 11.0F, 23.0F, 1.0F),
-				PartPose.offset(0.0F, 0.5F, 2.9F)
-		);
+				PartPose.offset(0.0F, 0.5F, 2.9F));
 		
-		return data.getRoot().bake(128, 128);
+		return definition.getRoot().bake(128, 128);
 	}
 	
 	private static ModelPart createFrontCloth() {
-		MeshDefinition data = new MeshDefinition();
-		var root = data.getRoot();
+		MeshDefinition definition = new MeshDefinition();
 		
-		root.addOrReplaceChild(
-				"loincloth",
-				CubeListBuilder.create()
+		definition.getRoot().addOrReplaceChild("loincloth", CubeListBuilder.create()
 						.texOffs(62, 55)
-						.addBox(-3.5F, 0.0F, 0.0F, 7.0F, 14.0F, 1.0F),
-				PartPose.offset(0.0F, 14.0F, 0F)
-		);
-		return data.getRoot().bake(128, 128);
+						.addBox(-3.5F, -8.35F, -2.5F, 7.0F, 14.0F, 1.0F),
+				PartPose.offset(0.0F, 18.0F, 0.0F));
+		
+		return definition.getRoot().bake(128, 128);
 	}
 	
 }

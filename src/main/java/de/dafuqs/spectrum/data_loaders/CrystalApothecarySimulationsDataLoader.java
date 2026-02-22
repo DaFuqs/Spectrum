351c60011141a2f2eb6e9ea04400b8443ff506ac
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-02-22 14:49:19 +0100

    make CrystalApothecarySimulationsDataLoader respect load conditions

--- src/main/java/de/dafuqs/spectrum/data_loaders/CrystalApothecarySimulationsDataLoader.java
+++ src/main/java/de/dafuqs/spectrum/data_loaders/CrystalApothecarySimulationsDataLoader.java
@@ -5,12 +5,14 @@ import com.mojang.datafixers.util.*;
 import com.mojang.serialization.*;
 import com.mojang.serialization.codecs.*;
 import de.dafuqs.spectrum.*;
+import de.dafuqs.spectrum.registries.*;
 import net.minecraft.core.registries.*;
 import net.minecraft.resources.*;
 import net.minecraft.server.packs.resources.*;
 import net.minecraft.util.profiling.*;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.*;
+import net.neoforged.neoforge.common.conditions.*;
 
 import java.util.*;
 
@@ -21,9 +23,10 @@ public class CrystalApothecarySimulationsDataLoader extends SimpleJsonResourceRe
 	
 	public static final HashMap<Block, SimulatedBlockGrowthEntry> COMPENSATIONS = new HashMap<>();
 	
-	public record SimulatedBlockGrowthEntry(Collection<Block> validNeighbors, int ticksForCompensationLootPerValidNeighbor, ItemStack compensatedStack) {
+	public record SimulatedBlockGrowthEntry(Block buddingBlock, Collection<Block> validNeighbors, int ticksForCompensationLootPerValidNeighbor, ItemStack compensatedStack) {
 		
 		public static final Codec<SimulatedBlockGrowthEntry> CODEC = RecordCodecBuilder.create(i -> i.group(
+				BuiltInRegistries.BLOCK.byNameCodec().fieldOf("budding_block").forGetter(c -> c.buddingBlock),
 				BuiltInRegistries.BLOCK.byNameCodec().listOf().xmap(list -> (Collection<Block>) list, set -> set.stream().toList()).fieldOf("valid_neighbor_blocks").forGetter(c -> c.validNeighbors),
 				Codec.INT.optionalFieldOf("ticks_for_compensation_loot_per_valid_neighbor", 10000).forGetter(c -> c.ticksForCompensationLootPerValidNeighbor),
 				ItemStack.CODEC.fieldOf("compensated_loot").forGetter(c -> c.compensatedStack)
@@ -39,21 +42,19 @@ public class CrystalApothecarySimulationsDataLoader extends SimpleJsonResourceRe
 	protected void apply(Map<ResourceLocation, JsonElement> prepared, ResourceManager manager, ProfilerFiller profiler) {
 		COMPENSATIONS.clear();
 		prepared.forEach((identifier, jsonElement) -> {
-			JsonObject object = jsonElement.getAsJsonObject();
-			
-			DataResult<Block> buddingBlock = BuiltInRegistries.BLOCK.byNameCodec().decode(JsonOps.INSTANCE, object.get("budding_block")).map(Pair::getFirst);
-			if (buddingBlock.error().isPresent() || buddingBlock.result().isEmpty()) {
-				SpectrumCommon.logError("Crystal apothecary simulation error for " + identifier + ": " + buddingBlock.error().get() + ". Ignoring that one.");
-				return;
+			DataResult<Pair<Optional<SimulatedBlockGrowthEntry>, JsonElement>> result = ConditionalOps.createConditionalCodec(SimulatedBlockGrowthEntry.CODEC).decode(JsonOps.INSTANCE, jsonElement.getAsJsonObject());
+			// ConditionalOps.createConditionalCodec makes the data loader respect load conditions. Hooooray!
+			if (result.isSuccess() && result.getOrThrow().getFirst().isPresent()) {
+				Block buddingBlock = result.getOrThrow().getFirst().get().buddingBlock;
+				DataResult<SimulatedBlockGrowthEntry> entry = SimulatedBlockGrowthEntry.CODEC.decode(JsonOps.INSTANCE, jsonElement).map(Pair::getFirst);
+				if (entry.error().isPresent() || entry.result().isEmpty()) {
+					SpectrumCommon.logError("Crystal Apothecary Simulation error for " + identifier + ": " + entry.error().get() + ". Ignoring that one.");
+					return;
+				}
+				
+				COMPENSATIONS.put(buddingBlock, entry.result().get());
 			}
 			
-			DataResult<SimulatedBlockGrowthEntry> entry = SimulatedBlockGrowthEntry.CODEC.decode(JsonOps.INSTANCE, jsonElement).map(Pair::getFirst);
-			if (entry.error().isPresent() || entry.result().isEmpty()) {
-				SpectrumCommon.logError("Crystal Apothecary Simulation error for " + identifier + ": " + entry.error().get() + ". Ignoring that one.");
-				return;
-			}
-			
-			COMPENSATIONS.put(buddingBlock.result().get(), entry.result().get());
 		});
 	}
 	

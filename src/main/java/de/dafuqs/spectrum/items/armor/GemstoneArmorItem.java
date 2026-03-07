3c791610f4995dd08ab72393abe628d6de40a26f
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-03-07 12:18:55 +0100

    refactor gemstone armor code

--- src/main/java/de/dafuqs/spectrum/items/armor/GemstoneArmorItem.java
+++ src/main/java/de/dafuqs/spectrum/items/armor/GemstoneArmorItem.java
@@ -1,6 +1,5 @@
 package de.dafuqs.spectrum.items.armor;
 
-import de.dafuqs.spectrum.*;
 import de.dafuqs.spectrum.api.item.*;
 import de.dafuqs.spectrum.config.*;
 import net.minecraft.*;
@@ -11,7 +10,6 @@ import net.minecraft.world.damagesource.*;
 import net.minecraft.world.effect.*;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.item.*;
-import org.jetbrains.annotations.*;
 
 import java.util.*;
 
@@ -25,7 +23,26 @@ public class GemstoneArmorItem extends ArmorItem implements ArmorWithHitEffect {
 	public void onHit(ItemStack itemStack, DamageSource source, LivingEntity targetEntity, float amount) {
 		// While mostly useful against mobs, being able to trigger this effect for all kinds of damage
 		// like fall damage seems like an awesome mechanic
-		process(type, source, targetEntity);
+		switch (type) {
+			case HELMET -> {
+				if (source.getEntity() instanceof LivingEntity livingSource) {
+					livingSource.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 5 * 20, SpectrumConfig.CONFIG.GemstoneArmorWeaknessAmplifier.get()));
+					livingSource.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 5 * 20, SpectrumConfig.CONFIG.GemstoneArmorSlownessAmplifier.get()));
+				}
+			}
+			case CHESTPLATE -> {
+				targetEntity.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 5 * 20, SpectrumConfig.CONFIG.GemstoneArmorAbsorptionAmplifier.get()));
+				targetEntity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 5 * 20, SpectrumConfig.CONFIG.GemstoneArmorResistanceAmplifier.get()));
+			}
+			case LEGGINGS -> {
+				targetEntity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 5 * 20, SpectrumConfig.CONFIG.GemstoneArmorRegenerationAmplifier.get()));
+			}
+			case BOOTS -> {
+				targetEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 5 * 20, SpectrumConfig.CONFIG.GemstoneArmorSpeedAmplifier.get()));
+				targetEntity.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY, 5 * 20, 0));
+			}
+		}
+		
 		targetEntity.level().playSound(null, targetEntity.blockPosition(), SoundEvents.AMETHYST_BLOCK_HIT, SoundSource.PLAYERS, 1.0F, 1.0F);
 		targetEntity.level().playSound(null, targetEntity.blockPosition(), SoundEvents.SPLASH_POTION_BREAK, SoundSource.PLAYERS, 1.0F, 1.0F);
 		
@@ -35,40 +52,8 @@ public class GemstoneArmorItem extends ArmorItem implements ArmorWithHitEffect {
 	@Override
 	public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltip, TooltipFlag tooltipType) {
 		super.appendHoverText(stack, context, tooltip, tooltipType);
-		addTooltip(tooltip, type);
-	}
-	
-	private void process(@NotNull ArmorItem.Type type, DamageSource source, LivingEntity targetEntity) {
+		
 		switch (type) {
-			case HELMET -> {
-				if (source.getEntity() instanceof LivingEntity) {
-					MobEffectInstance statusEffectInstance = new MobEffectInstance(MobEffects.WEAKNESS, 5 * 20, SpectrumConfig.CONFIG.GemstoneArmorWeaknessAmplifier.get());
-					((LivingEntity) source.getEntity()).addEffect(statusEffectInstance);
-					statusEffectInstance = new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 5 * 20, SpectrumConfig.CONFIG.GemstoneArmorSlownessAmplifier.get());
-					((LivingEntity) source.getEntity()).addEffect(statusEffectInstance);
-				}
-			}
-			case CHESTPLATE -> {
-				MobEffectInstance statusEffectInstance = new MobEffectInstance(MobEffects.ABSORPTION, 5 * 20, SpectrumConfig.CONFIG.GemstoneArmorAbsorptionAmplifier.get());
-				targetEntity.addEffect(statusEffectInstance);
-				statusEffectInstance = new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 5 * 20, SpectrumConfig.CONFIG.GemstoneArmorResistanceAmplifier.get());
-				targetEntity.addEffect(statusEffectInstance);
-			}
-			case LEGGINGS -> {
-				MobEffectInstance statusEffectInstance = new MobEffectInstance(MobEffects.REGENERATION, 5 * 20, SpectrumConfig.CONFIG.GemstoneArmorRegenerationAmplifier.get());
-				targetEntity.addEffect(statusEffectInstance);
-			}
-			case BOOTS -> {
-				MobEffectInstance statusEffectInstance = new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 5 * 20, SpectrumConfig.CONFIG.GemstoneArmorSpeedAmplifier.get());
-				targetEntity.addEffect(statusEffectInstance);
-				statusEffectInstance = new MobEffectInstance(MobEffects.INVISIBILITY, 5 * 20, 0);
-				targetEntity.addEffect(statusEffectInstance);
-			}
-		}
-	}
-	
-	public void addTooltip(List<Component> tooltip, @NotNull ArmorItem.Type equipmentSlot) {
-		switch (equipmentSlot) {
 			case HELMET -> tooltip.add(Component.translatable("item.spectrum.fetchling_helmet.tooltip").withStyle(ChatFormatting.GRAY));
 			case CHESTPLATE -> tooltip.add(Component.translatable("item.spectrum.ferocious_chestplate.tooltip").withStyle(ChatFormatting.GRAY));
 			case LEGGINGS -> tooltip.add(Component.translatable("item.spectrum.sylph_leggings.tooltip").withStyle(ChatFormatting.GRAY));

33915be67548f3cb93ba359b991eb02755df7d6c
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-03-09 18:17:50 +0100

    remove ComponentHelper

--- src/main/java/de/dafuqs/spectrum/api/item/ActivatableItem.java
+++ src/main/java/de/dafuqs/spectrum/api/item/ActivatableItem.java
@@ -1,13 +1,16 @@
 package de.dafuqs.spectrum.api.item;
 
-import de.dafuqs.spectrum.helpers.*;
 import de.dafuqs.spectrum.registries.*;
+import net.minecraft.util.*;
 import net.minecraft.world.item.*;
 
 public interface ActivatableItem {
 	
 	static void setActivated(ItemStack stack, boolean activated) {
-		ComponentHelper.setOrRemove(stack, SpectrumDataComponentTypes.ACTIVATED, activated);
+		if (activated)
+			stack.set(SpectrumDataComponentTypes.ACTIVATED, Unit.INSTANCE);
+		else
+			stack.remove(SpectrumDataComponentTypes.ACTIVATED);
 	}
 	
 	static boolean isActivated(ItemStack stack) {

e5f190b96a59b32efd219c4a412fff91171f93c1
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-03-10 16:48:53 +0100

    heartbound chest can be opened by team members

--- src/main/java/de/dafuqs/spectrum/api/block/PlayerOwnedWithName.java
+++ src/main/java/de/dafuqs/spectrum/api/block/PlayerOwnedWithName.java
@@ -1,7 +1,42 @@
 package de.dafuqs.spectrum.api.block;
 
+import net.minecraft.nbt.*;
+import net.minecraft.world.entity.player.*;
+import net.minecraft.world.scores.*;
+
+import java.util.*;
+
 public interface PlayerOwnedWithName extends PlayerOwned {
 	
+	static UUID readOwnerUUID(CompoundTag nbt) {
+		if (nbt.contains("OwnerUUID")) {
+			return nbt.getUUID("OwnerUUID");
+		}
+		return null;
+	}
+	
+	static void writeOwnerName(CompoundTag nbt, String ownerName) {
+		if (ownerName != null) {
+			nbt.putString("OwnerName", ownerName);
+		}
+	}
+	
+	static String readOwnerName(CompoundTag nbt) {
+		if (nbt.contains("OwnerName")) {
+			return nbt.getString("OwnerName");
+		}
+		return "???";
+	}
+	
 	String getOwnerName();
+	
+	default boolean isOwnerOrSameTeamAsOwner(Player player) {
+		if(getOwnerUUID().equals(player.getUUID())) {
+			return true;
+		}
+		
+		PlayerTeam team = player.getTeam();
+		return  team != null && team.getPlayers().contains(getOwnerName());
+	}
 	
 }

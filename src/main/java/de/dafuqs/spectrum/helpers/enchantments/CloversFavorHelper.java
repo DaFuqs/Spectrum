46f1e87ec2c758773acd05dc11bc86e3cdac71e5
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-03-12 17:30:14 +0100

    update aea => fixes clovers favor

--- src/main/java/de/dafuqs/spectrum/helpers/enchantments/CloversFavorHelper.java
+++ src/main/java/de/dafuqs/spectrum/helpers/enchantments/CloversFavorHelper.java
@@ -7,7 +7,6 @@ import org.jetbrains.annotations.*;
 
 public class CloversFavorHelper {
 	
-	// TODO: give this enchantment AdditionalEntityAttributes.BONUS_RARE_LOOT_ROLLS 1 for level 1, 4 for level 2, ...
 	public static float rollChance(float baseChance, @Nullable Entity entity) {
 		if (entity instanceof Player player) {
 			float rareLootLevel = (float) player.getAttributeValue(AdditionalEntityAttributes.BONUS_RARE_LOOT_ROLLS);

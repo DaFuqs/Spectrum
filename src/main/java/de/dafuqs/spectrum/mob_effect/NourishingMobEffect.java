7eefe47b33b0858f53e2fbdcdc2d8eaa1bc4e16a
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-04-01 17:46:56 +0200

    Nourishing only feeds the player when they are hungry / saturation is low. Implements #848

--- src/main/java/de/dafuqs/spectrum/mob_effect/NourishingMobEffect.java
+++ src/main/java/de/dafuqs/spectrum/mob_effect/NourishingMobEffect.java
@@ -3,6 +3,7 @@ package de.dafuqs.spectrum.mob_effect;
 import net.minecraft.world.effect.*;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.player.*;
+import net.minecraft.world.food.*;
 import net.minecraft.world.level.*;
 import org.jetbrains.annotations.*;
 
@@ -16,7 +17,10 @@ public class NourishingMobEffect extends MobEffect {
 	public boolean applyEffectTick(LivingEntity entity, int amplifier) {
 		Level world = entity.level();
 		if (!world.isClientSide && entity instanceof Player playerEntity) {
-			playerEntity.getFoodData().eat(1, 0.25F);
+			FoodData foodData = playerEntity.getFoodData();
+			if(foodData.needsFood() || foodData.getSaturationLevel() < 20.0F) {
+				foodData.eat(1, 0.25F);
+			}
 		}
 		return super.applyEffectTick(entity, amplifier);
 	}

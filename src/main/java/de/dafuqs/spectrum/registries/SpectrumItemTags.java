2a5070d5d0d87e0309b254ce4247d4fd835ab7a8
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-04-07 22:22:07 +0200

    remove indestructible_blacklisted item tag. Use tag exclusion instead

--- src/main/java/de/dafuqs/spectrum/registries/SpectrumItemTags.java
+++ src/main/java/de/dafuqs/spectrum/registries/SpectrumItemTags.java
@@ -19,7 +19,6 @@ public class SpectrumItemTags {
 	public static final TagKey<Item> COMING_SOON_TOOLTIP = of("coming_soon_tooltip");
 	public static final TagKey<Item> ENCHANTABLE_BOOKS = of("enchantable_books");
 	public static final TagKey<Item> MEMORY_BONDING_AGENTS_CONCEALABLE = of("memory_bonding_agents_concealable");
-	public static final TagKey<Item> INDESTRUCTIBLE_BLACKLISTED = of("indestructible_blacklisted");
 	public static final TagKey<Item> NO_CINDERHEARTH_DOUBLING = of("no_cinderhearth_doubling");
 	public static final TagKey<Item> SHOOTING_STARS = of("shooting_stars");
 	public static final TagKey<Item> GLASS_ARROWS = of("glass_arrows");

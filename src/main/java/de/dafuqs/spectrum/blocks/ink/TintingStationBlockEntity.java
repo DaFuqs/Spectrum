aa0583dfcd20c16fcc7b44533486ba360c9fd5ca
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-05-24 16:18:35 +0200

    potion tinting

--- src/main/java/de/dafuqs/spectrum/blocks/ink/TintingStationBlockEntity.java
+++ src/main/java/de/dafuqs/spectrum/blocks/ink/TintingStationBlockEntity.java
@@ -22,6 +22,7 @@ import net.minecraft.world.*;
 import net.minecraft.world.entity.player.*;
 import net.minecraft.world.inventory.*;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.alchemy.*;
 import net.minecraft.world.item.component.*;
 import net.minecraft.world.level.*;
 import net.minecraft.world.level.block.state.*;
@@ -49,7 +50,7 @@ public class TintingStationBlockEntity extends BaseInkTransferBlockEntity<Indivi
 			return;
 		}
 		
-		boolean didSomething = false;
+		boolean didSomething = true;
 		
 		Optional<Holder<InkColor>> inkColorHolder = blockEntity.getSelectedColor();
 		InkStorage inkStorage = blockEntity.getInkStorage();
@@ -65,16 +66,17 @@ public class TintingStationBlockEntity extends BaseInkTransferBlockEntity<Indivi
 						if (resultStack.isEmpty()) {
 							resultStack = tintStack(input, selectedInkColor);
 						}
+						if (resultStack.isEmpty()) {
+							resultStack = tintPotion(input, selectedInkColor);
+						}
 						
 						if (!resultStack.isEmpty()) {
 							if (output.isEmpty()) {
 								input.shrink(1);
 								blockEntity.setItem(OUTPUT_SLOT_ID, resultStack);
-								didSomething = true;
 							} else if (ItemStack.isSameItemSameComponents(output, resultStack)) {
 								input.shrink(1);
 								output.grow(1);
-								didSomething = true;
 							}
 							
 							if (didSomething) {
@@ -92,8 +94,12 @@ public class TintingStationBlockEntity extends BaseInkTransferBlockEntity<Indivi
 									);
 								}
 							}
+						} else {
+							didSomething = false;
 						}
 					}
+				} else {
+					didSomething = false;
 				}
 			}
 		}
@@ -114,48 +120,64 @@ public class TintingStationBlockEntity extends BaseInkTransferBlockEntity<Indivi
 	protected static ItemStack tintStack(ItemStack stack, InkColor inkColor) {
 		if (!stack.is(ItemTags.DYEABLE)) {
 			return ItemStack.EMPTY;
-		} else {
-			ItemStack result = stack.copyWithCount(1);
-			int i = 0;
-			int j = 0;
-			int k = 0;
-			int prevMax = 0;
-			int i1 = 0;
-			DyedItemColor dyeditemcolor = result.get(DataComponents.DYED_COLOR);
-			if (dyeditemcolor != null) {
-				int prevRed = FastColor.ARGB32.red(dyeditemcolor.rgb());
-				int prevGreen = FastColor.ARGB32.green(dyeditemcolor.rgb());
-				int prevBlue = FastColor.ARGB32.blue(dyeditemcolor.rgb());
-				prevMax += Math.max(prevRed, Math.max(prevGreen, prevBlue));
-				i += prevRed;
-				j += prevGreen;
-				k += prevBlue;
-				i1++;
-			}
-			
-			int j3 = inkColor.getColorInt();
-			int i2 = FastColor.ARGB32.red(j3);
-			int j2 = FastColor.ARGB32.green(j3);
-			int k2 = FastColor.ARGB32.blue(j3);
-			prevMax += Math.max(i2, Math.max(j2, k2));
-			i += i2;
-			j += j2;
-			k += k2;
-			i1++;
-			
-			int finalRed = i / i1;
-			int finalGreen = j / i1;
-			int finalBlue = k / i1;
-			float f = (float)prevMax / (float)i1;
-			float f1 = (float)Math.max(finalRed, Math.max(finalGreen, finalBlue));
-			finalRed = (int)((float)finalRed * f / f1);
-			finalGreen = (int)((float)finalGreen * f / f1);
-			finalBlue = (int)((float)finalBlue * f / f1);
-			int finalColor = FastColor.ARGB32.color(0, finalRed, finalGreen, finalBlue);
-			boolean flag = dyeditemcolor == null || dyeditemcolor.showInTooltip();
-			result.set(DataComponents.DYED_COLOR, new DyedItemColor(finalColor, flag));
-			return result;
 		}
+		
+		ItemStack result = stack.copyWithCount(1);
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		int prevMax = 0;
+		int i1 = 0;
+		DyedItemColor dyeditemcolor = result.get(DataComponents.DYED_COLOR);
+		if (dyeditemcolor != null) {
+			int prevRed = FastColor.ARGB32.red(dyeditemcolor.rgb());
+			int prevGreen = FastColor.ARGB32.green(dyeditemcolor.rgb());
+			int prevBlue = FastColor.ARGB32.blue(dyeditemcolor.rgb());
+			prevMax += Math.max(prevRed, Math.max(prevGreen, prevBlue));
+			i += prevRed;
+			j += prevGreen;
+			k += prevBlue;
+			i1++;
+		}
+		
+		int j3 = inkColor.getColorInt();
+		int i2 = FastColor.ARGB32.red(j3);
+		int j2 = FastColor.ARGB32.green(j3);
+		int k2 = FastColor.ARGB32.blue(j3);
+		prevMax += Math.max(i2, Math.max(j2, k2));
+		i += i2;
+		j += j2;
+		k += k2;
+		i1++;
+		
+		int finalRed = i / i1;
+		int finalGreen = j / i1;
+		int finalBlue = k / i1;
+		float f = (float)prevMax / (float)i1;
+		float f1 = (float)Math.max(finalRed, Math.max(finalGreen, finalBlue));
+		finalRed = (int)((float)finalRed * f / f1);
+		finalGreen = (int)((float)finalGreen * f / f1);
+		finalBlue = (int)((float)finalBlue * f / f1);
+		int finalColor = FastColor.ARGB32.color(0, finalRed, finalGreen, finalBlue);
+		boolean flag = dyeditemcolor == null || dyeditemcolor.showInTooltip();
+		result.set(DataComponents.DYED_COLOR, new DyedItemColor(finalColor, flag));
+		return result;
+	}
+	
+	protected static ItemStack tintPotion(ItemStack stack, InkColor inkColor) {
+		PotionContents pc = stack.get(DataComponents.POTION_CONTENTS);
+		if(pc == null) {
+			return ItemStack.EMPTY;
+		}
+		int newColor = inkColor.getColorInt();
+		if(pc.customColor().isPresent() && pc.customColor().get() == newColor) {
+			return ItemStack.EMPTY;
+		}
+		
+		ItemStack result = stack.copyWithCount(1);
+		PotionContents newContents = new PotionContents(pc.potion(), Optional.of(newColor), pc.customEffects());
+		result.set(DataComponents.POTION_CONTENTS, newContents);
+		return result;
 	}
 	
 	public ItemStack colorStack(ItemStack stack, InkColor inkColor) {

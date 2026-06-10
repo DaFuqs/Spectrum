5bcbc413ebc863bfbe2a5acd7d6a9c0af0bf9527
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-06-10 20:56:35 +0200

    fireproof item entity bounding box

--- src/main/java/de/dafuqs/spectrum/entity/entity/FireproofItemEntity.java
+++ src/main/java/de/dafuqs/spectrum/entity/entity/FireproofItemEntity.java
@@ -31,7 +31,7 @@ public class FireproofItemEntity extends ItemEntity {
 	}
 	
 	public static void scatter(Level world, double x, double y, double z, ItemStack stack) {
-		double d = EntityType.ITEM.getWidth();
+		double d = SpectrumEntityTypes.FIREPROOF_ITEM.getWidth();
 		double e = 1.0 - d;
 		double f = d / 2.0;
 		double g = Math.floor(x) + world.getRandom().nextDouble() * e + f;

12e05a49ce2b8432bf692f8fae26db82c7cfb388
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-09-06 21:56:49 +0200

    ensure mutability

--- src/main/java/de/dafuqs/spectrum/compat/emi/recipes/EnchantmentUpgradeEmiRecipeGated.java
+++ src/main/java/de/dafuqs/spectrum/compat/emi/recipes/EnchantmentUpgradeEmiRecipeGated.java
@@ -48,7 +48,7 @@ public class EnchantmentUpgradeEmiRecipeGated extends GatedSpectrumEmiRecipe<Enc
 		
 		this.inputs = recipe.getIngredientStacks().stream()
 				.map(s -> EmiIngredient.of(s.getItems().map(EmiStack::of).toList()))
-				.toList();
+				.collect(Collectors.toCollection(ArrayList::new));
 		
 		// Then the xp
 		inputs.add(EmiStack.of(KnowledgeGemItem.getKnowledgeDropStackWithXP(recipe.getLevelData().getFirst().experience(), true)));

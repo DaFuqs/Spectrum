bba58a19978a006fd2afea190b886695116b3a7e
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-09-08 14:47:54 +0200

    enchant upgrade tweaks

--- src/main/java/de/dafuqs/spectrum/compat/emi/recipes/EnchantmentUpgradeEmiRecipeGated.java
+++ src/main/java/de/dafuqs/spectrum/compat/emi/recipes/EnchantmentUpgradeEmiRecipeGated.java
@@ -105,7 +105,7 @@ public class EnchantmentUpgradeEmiRecipeGated extends GatedSpectrumEmiRecipe<Enc
 		widgets.add(new DynamicStackWidget(c -> inputs.get(selectedSourceLevel), 0, 0, 18));
 		
 		// Center Slot
-		widgets.add(new DynamicStackWidget(c -> inputs.get(cap + selectedSourceLevel), 0, 31, 31));
+		widgets.add(new DynamicStackWidget(c -> inputs.get(levelCap + selectedSourceLevel), 0, 31, 31));
 		
 		// Output
 		widgets.add(new DynamicStackWidget(c -> outputs.get(selectedSourceLevel - 1), 0, 106, 26).large(true).recipeContext(this));
